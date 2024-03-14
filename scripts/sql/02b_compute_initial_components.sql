4b00f3ce397088a5a1eb5fec93fe3f5edf163960
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-03-14 15:12:07 +0100

    working on comp gaps

--- scripts/sql/02b_compute_initial_components.sql
+++ scripts/sql/02b_compute_initial_components.sql
@@ -1,5 +1,5 @@
 ALTER TABLE
-    edges
+    kbh
 ADD
     COLUMN IF NOT EXISTS component_all BIGINT DEFAULT NULL,
 ADD
@@ -34,7 +34,7 @@ SELECT
     *
 FROM
     pgr_connectedComponents(
-        'SELECT id, source, target, cost, reverse_cost FROM edges'
+        'SELECT id, source, target, cost, reverse_cost FROM kbh'
     );
 
 CREATE TABLE components_1 AS
@@ -42,7 +42,7 @@ SELECT
     *
 FROM
     pgr_connectedComponents(
-        'SELECT id, source, target, cost, reverse_cost FROM edges WHERE lts_access = 1'
+        'SELECT id, source, target, cost, reverse_cost FROM kbh WHERE lts_access = 1'
     );
 
 CREATE TABLE components_2 AS
@@ -50,7 +50,7 @@ SELECT
     *
 FROM
     pgr_connectedComponents(
-        'SELECT id, source, target, cost, reverse_cost FROM edges WHERE lts_access IN (1,2)'
+        'SELECT id, source, target, cost, reverse_cost FROM kbh WHERE lts_access IN (1,2)'
     );
 
 CREATE TABLE components_3 AS
@@ -58,7 +58,7 @@ SELECT
     *
 FROM
     pgr_connectedComponents(
-        'SELECT id, source, target, cost, reverse_cost FROM edges WHERE lts_access IN (1,2,3)'
+        'SELECT id, source, target, cost, reverse_cost FROM kbh WHERE lts_access IN (1,2,3)'
     );
 
 CREATE TABLE components_4 AS
@@ -66,11 +66,11 @@ SELECT
     *
 FROM
     pgr_connectedComponents(
-        'SELECT id, source, target, cost, reverse_cost FROM edges WHERE lts_access IN (1,2,3,4)'
+        'SELECT id, source, target, cost, reverse_cost FROM kbh WHERE lts_access IN (1,2,3,4)'
     );
 
 UPDATE
-    edges e
+    kbh e
 SET
     component_all = component
 FROM
@@ -79,47 +79,53 @@ WHERE
     e.source = co.node;
 
 UPDATE
-    edges e
+    kbh e
 SET
     component_1 = component
 FROM
     components_1 co
 WHERE
-    e.source = co.node;
+    e.source = co.node
+    AND lts_access = 1;
 
 UPDATE
-    edges e
+    kbh e
 SET
     component_2 = component
 FROM
     components_2 co
 WHERE
-    e.source = co.node;
+    e.source = co.node
+    AND lts_access IN (1, 2);
+
+;
 
 UPDATE
-    edges e
+    kbh e
 SET
     component_3 = component
 FROM
     components_3 co
 WHERE
-    e.source = co.node;
+    e.source = co.node
+    AND lts_access IN (1, 2, 3);
 
 UPDATE
-    edges e
+    kbh e
 SET
     component_4 = component
 FROM
     components_4 co
 WHERE
-    e.source = co.node;
+    e.source = co.node
+    AND lts_access IN (1, 2, 3, 4);
 
 -- CREATE TABLE component_5 AS
 -- SELECT
 --     *
 -- FROM
 --     pgr_connectedComponents(
---         'SELECT id, source, target, cost, reverse_cost FROM edges WHERE lts_access = 5'
+--         'SELECT id, source, target, cost, reverse_cost FROM kbh WHERE lts_access = 5'
 --     );
 -- ---- **** -----
 -- CREATE TABLE component_6 AS
@@ -127,21 +133,21 @@ WHERE
 --     *
 -- FROM
 --     pgr_connectedComponents(
---         'SELECT id, source, target, cost, reverse_cost FROM edges WHERE lts_access = 6'
+--         'SELECT id, source, target, cost, reverse_cost FROM kbh WHERE lts_access = 6'
 --     );
 -- CREATE TABLE component_7 AS
 -- SELECT
 --     *
 -- FROM
 --     pgr_connectedComponents(
---         'SELECT id, source, target, cost, reverse_cost FROM edges WHERE lts_access = 7'
+--         'SELECT id, source, target, cost, reverse_cost FROM kbh WHERE lts_access = 7'
 --     );
 -- CREATE TABLE component_0 AS
 -- SELECT
 --     *
 -- FROM
 --     pgr_connectedComponents(
---         'SELECT id, source, target, cost, reverse_cost FROM edges WHERE lts_access = 0'
+--         'SELECT id, source, target, cost, reverse_cost FROM kbh WHERE lts_access = 0'
 --     );
 DROP TABLE IF EXISTS components;
 
--- scripts/sql/02c_close_lts_gaps.sql
+++ scripts/sql/02c_close_lts_gaps.sql
@@ -1,6 +1,5 @@
--- NEEDS COMPONENT INFO
 ALTER TABLE
-    edges
+    kbh
 ADD
     COLUMN IF NOT EXISTS lts_1_gap BOOLEAN,
 ADD
@@ -10,18 +9,26 @@ ADD
 ADD
     COLUMN IF NOT EXISTS lts_4_gap BOOLEAN;
 
+DROP TABLE IF EXISTS lts_1_gaps;
+
+DROP TABLE IF EXISTS lts_2_gaps;
+
+DROP TABLE IF EXISTS lts_3_gaps;
+
+DROP TABLE IF EXISTS lts_4_gaps;
+
 CREATE VIEW nodes_lts_1 AS WITH nodes AS (
     SELECT
         source AS node
     FROM
-        edges
+        kbh
     WHERE
         lts = 1
     UNION
     SELECT
         target AS node
     FROM
-        edges
+        kbh
     WHERE
         lts = 1
 )
@@ -34,14 +41,14 @@ CREATE VIEW nodes_lts_2 AS WITH nodes AS (
     SELECT
         source AS node
     FROM
-        edges
+        kbh
     WHERE
         lts = 2
     UNION
     SELECT
         target AS node
     FROM
-        edges
+        kbh
     WHERE
         lts = 2
 )
@@ -54,14 +61,14 @@ CREATE VIEW nodes_lts_3 AS WITH nodes AS (
     SELECT
         source AS node
     FROM
-        edges
+        kbh
     WHERE
         lts = 3
     UNION
     SELECT
         target AS node
     FROM
-        edges
+        kbh
     WHERE
         lts = 3
 )
@@ -74,14 +81,14 @@ CREATE VIEW nodes_lts_4 AS WITH nodes AS (
     SELECT
         source AS node
     FROM
-        edges
+        kbh
     WHERE
         lts = 4
     UNION
     SELECT
         target AS node
     FROM
-        edges
+        kbh
     WHERE
         lts = 4
 )
@@ -90,11 +97,8 @@ SELECT
 FROM
     nodes;
 
--- for each node view:
--- select all edges where both start and end node is in view
--- mark them as that gap
--- TODO: SEE HOW MANY GAPS HAVE CYCLING NOT ALLOWED OR NO ACCESS
-WITH node_sel AS (
+-- LTS 1 GAPS
+CREATE TABLE lts_1_gaps AS WITH node_sel AS (
     SELECT
         node
     FROM
@@ -104,13 +108,21 @@ WITH node_sel AS (
 ),
 lts_1_gaps AS (
     SELECT
-        *
+        id,
+        lts,
+        lts_access,
+        all_access,
+        cycling_allowed,
+        km,
+        source,
+        target,
+        geometry
     FROM
-        edges
+        kbh
     WHERE
         lts <> 1
-        AND km <= 30
-        AND all_access = TRUE -- AND cycling_allowed = TRUE
+        AND km <= 0.030
+        AND all_access = TRUE
         AND source IN (
             SELECT
                 *
@@ -124,8 +136,191 @@ lts_1_gaps AS (
                 node_sel
         )
 )
+SELECT
+    l.id,
+    l.lts,
+    l.lts_access,
+    l.all_access,
+    l.cycling_allowed,
+    l.source,
+    l.target,
+    l.geometry
+FROM
+    lts_1_gaps l
+    LEFT JOIN components_1 co1 ON l.source = co1.node
+    LEFT JOIN components_1 co2 ON l.target = co2.node
+WHERE
+    co1.component <> co2.component;
+
+--- LTS 2 GAPS
+CREATE TABLE lts_2_gaps AS WITH node_sel AS (
+    SELECT
+        node
+    FROM
+        nodes_lts_2
+    ORDER BY
+        node
+),
+lts_2_gaps AS (
+    SELECT
+        id,
+        lts,
+        lts_access,
+        all_access,
+        cycling_allowed,
+        km,
+        source,
+        target,
+        geometry
+    FROM
+        kbh
+    WHERE
+        lts NOT IN (1, 2)
+        AND km <= 0.030
+        AND all_access = TRUE
+        AND source IN (
+            SELECT
+                *
+            FROM
+                node_sel
+        )
+        AND target IN (
+            SELECT
+                *
+            FROM
+                node_sel
+        )
+)
+SELECT
+    l.id,
+    l.lts,
+    l.lts_access,
+    l.all_access,
+    l.cycling_allowed,
+    l.source,
+    l.target,
+    l.geometry
+FROM
+    lts_2_gaps l
+    LEFT JOIN components_2 co1 ON l.source = co1.node
+    LEFT JOIN components_2 co2 ON l.target = co2.node
+WHERE
+    co1.component <> co2.component;
+
+-- LTS 3 GAPS
+CREATE TABLE lts_3_gaps AS WITH node_sel AS (
+    SELECT
+        node
+    FROM
+        nodes_lts_3
+    ORDER BY
+        node
+),
+lts_3_gaps AS (
+    SELECT
+        id,
+        lts,
+        lts_access,
+        all_access,
+        cycling_allowed,
+        km,
+        source,
+        target,
+        geometry
+    FROM
+        kbh
+    WHERE
+        lts NOT IN (1, 2, 3)
+        AND km <= 0.030
+        AND all_access = TRUE
+        AND source IN (
+            SELECT
+                *
+            FROM
+                node_sel
+        )
+        AND target IN (
+            SELECT
+                *
+            FROM
+                node_sel
+        )
+)
+SELECT
+    l.id,
+    l.lts,
+    l.lts_access,
+    l.all_access,
+    l.cycling_allowed,
+    l.source,
+    l.target,
+    l.geometry
+FROM
+    lts_3_gaps l
+    LEFT JOIN components_3 co1 ON l.source = co1.node
+    LEFT JOIN components_3 co2 ON l.target = co2.node
+WHERE
+    co1.component <> co2.component;
+
+-- LTS 4 GAPS
+CREATE TABLE lts_4_gaps AS WITH node_sel AS (
+    SELECT
+        node
+    FROM
+        nodes_lts_4
+    ORDER BY
+        node
+),
+lts_4_gaps AS (
+    SELECT
+        id,
+        lts,
+        lts_access,
+        all_access,
+        cycling_allowed,
+        km,
+        source,
+        target,
+        geometry
+    FROM
+        kbh
+    WHERE
+        lts NOT IN (1, 2, 3, 4)
+        AND km <= 0.030
+        AND all_access = TRUE
+        AND source IN (
+            SELECT
+                *
+            FROM
+                node_sel
+        )
+        AND target IN (
+            SELECT
+                *
+            FROM
+                node_sel
+        )
+)
+SELECT
+    l.id,
+    l.lts,
+    l.lts_access,
+    l.all_access,
+    l.cycling_allowed,
+    l.source,
+    l.target,
+    l.geometry
+FROM
+    lts_4_gaps l
+    LEFT JOIN components_1 co1 ON l.source = co1.node
+    LEFT JOIN components_1 co2 ON l.target = co2.node
+WHERE
+    co1.component <> co2.component;
+
+-- TODO: CONSIDER NODE SELECTION FOR 2,3,4
+-- GET DISTINCT COMPONENT FOR EACH gap source and target - if its the same, it is not a gap
 UPDATE
-    edges
+    kbh
 SET
     lts_1_gap = TRUE
 WHERE
@@ -136,37 +331,20 @@ WHERE
             lts_1_gaps
     );
 
-----
-WITH lts_1_gaps AS (
-    SELECT
-        *
-    FROM
-        edges
-    WHERE
-        source IN (
-            SELECT
-                node
-            FROM
-                nodes_lts_1
-        )
-        AND target IN (
-            SELECT
-                node
-            FROM
-                nodes_lts_1
-        )
-        AND lts <> 1
-        AND km <= 30
-        AND all_access = TRUE -- AND cycling_allowed = TRUE
-)
-UPDATE
-    edges
-SET
-    lts_1_gap = TRUE
-WHERE
-    id IN (
-        SELECT
-            id
-        FROM
-            lts_1_gaps
-    );
+DROP TABLE IF EXISTS lts_1_gaps;
+
+DROP TABLE IF EXISTS lts_2_gaps;
+
+DROP TABLE IF EXISTS lts_3_gaps;
+
+DROP TABLE IF EXISTS lts_4_gaps;
+
+DROP VIEW IF EXISTS nodes_lts_1;
+
+DROP VIEW IF EXISTS nodes_lts_2;
+
+DROP VIEW IF EXISTS nodes_lts_3;
+
+DROP VIEW IF EXISTS nodes_lts_4;
+
+```
