12c8bc4a80f2289a59d1cd8a96b47445525a19f4
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-03-15 15:37:17 +0100

    gaps script done

--- scripts/sql/02c_close_lts_gaps.sql
+++ scripts/sql/02c_close_lts_gaps.sql
@@ -138,245 +138,84 @@ FROM
     LEFT JOIN components_1 AS co2 ON e.target = co2.node
 WHERE
     e.lts <> 1
-    AND e.km <= 0.030
+    AND e.km <= 0.020
     AND e.all_access = TRUE
     AND co1.component <> co2.component;
 
--- LTS 1 GAPS
-CREATE TABLE lts_1_gaps AS WITH node_sel AS (
-    SELECT
-        node
-    FROM
-        nodes_lts_1
-    ORDER BY
-        node
-),
-lts_1_gaps AS (
-    SELECT
-        "name",
-        municipality,
-        id,
-        lts,
-        lts_access,
-        all_access,
-        cycling_allowed,
-        km,
-        source,
-        target,
-        geometry
-    FROM
-        edges
-    WHERE
-        lts <> 1
-        AND km <= 0.030
-        AND all_access = TRUE
-        AND source IN (
-            SELECT
-                *
-            FROM
-                node_sel
-        )
-        AND target IN (
-            SELECT
-                *
-            FROM
-                node_sel
-        )
-)
+-- LTS GAPS 2
+CREATE TABLE lts_2_gaps AS
 SELECT
-    l.name,
-    l.municipality,
-    l.id,
-    l.lts,
-    l.lts_access,
-    l.all_access,
-    l.cycling_allowed,
-    l.source,
-    l.target,
-    l.geometry
+    e.name,
+    e.municipality,
+    e.id,
+    e.lts,
+    e.lts_access,
+    e.all_access,
+    e.cycling_allowed,
+    e.source,
+    e.target,
+    e.geometry
 FROM
-    lts_1_gaps l
-    LEFT JOIN components_1 co1 ON l.source = co1.node
-    LEFT JOIN components_1 co2 ON l.target = co2.node
+    edges AS e
+    JOIN nodes_lts_2 AS ns ON e.source = ns.node
+    JOIN nodes_lts_2 AS nt ON e.target = nt.node
+    LEFT JOIN components_2 AS co1 ON e.source = co1.node
+    LEFT JOIN components_2 AS co2 ON e.target = co2.node
 WHERE
-    co1.component <> co2.component;
+    e.lts <> 2
+    AND e.km <= 0.020
+    AND e.all_access = TRUE
+    AND co1.component <> co2.component;
 
---- LTS 2 GAPS
-CREATE TABLE lts_2_gaps AS WITH node_sel AS (
-    SELECT
-        node
-    FROM
-        nodes_lts_2
-    ORDER BY
-        node
-),
-lts_2_gaps AS (
-    SELECT
-        "name",
-        municipality,
-        id,
-        lts,
-        lts_access,
-        all_access,
-        cycling_allowed,
-        km,
-        source,
-        target,
-        geometry
-    FROM
-        edges
-    WHERE
-        lts NOT IN (1, 2)
-        AND km <= 0.030
-        AND all_access = TRUE
-        AND source IN (
-            SELECT
-                *
-            FROM
-                node_sel
-        )
-        AND target IN (
-            SELECT
-                *
-            FROM
-                node_sel
-        )
-)
+-- LTS GAPS 3
+CREATE TABLE lts_3_gaps AS
 SELECT
-    l.name,
-    l.municipality,
-    l.id,
-    l.lts,
-    l.lts_access,
-    l.all_access,
-    l.cycling_allowed,
-    l.source,
-    l.target,
-    l.geometry
+    e.name,
+    e.municipality,
+    e.id,
+    e.lts,
+    e.lts_access,
+    e.all_access,
+    e.cycling_allowed,
+    e.source,
+    e.target,
+    e.geometry
 FROM
-    lts_2_gaps l
-    LEFT JOIN components_2 co1 ON l.source = co1.node
-    LEFT JOIN components_2 co2 ON l.target = co2.node
+    edges AS e
+    JOIN nodes_lts_3 AS ns ON e.source = ns.node
+    JOIN nodes_lts_3 AS nt ON e.target = nt.node
+    LEFT JOIN components_3 AS co1 ON e.source = co1.node
+    LEFT JOIN components_3 AS co2 ON e.target = co2.node
 WHERE
-    co1.component <> co2.component;
+    e.lts <> 3
+    AND e.km <= 0.020
+    AND e.all_access = TRUE
+    AND co1.component <> co2.component;
 
--- LTS 3 GAPS
-CREATE TABLE lts_3_gaps AS WITH node_sel AS (
-    SELECT
-        node
-    FROM
-        nodes_lts_3
-    ORDER BY
-        node
-),
-lts_3_gaps AS (
-    SELECT
-        "name",
-        municipality,
-        id,
-        lts,
-        lts_access,
-        all_access,
-        cycling_allowed,
-        km,
-        source,
-        target,
-        geometry
-    FROM
-        edges
-    WHERE
-        lts NOT IN (1, 2, 3)
-        AND km <= 0.030
-        AND all_access = TRUE
-        AND source IN (
-            SELECT
-                *
-            FROM
-                node_sel
-        )
-        AND target IN (
-            SELECT
-                *
-            FROM
-                node_sel
-        )
-)
+-- LTS GAPS 4
+CREATE TABLE lts_4_gaps AS
 SELECT
-    l.name,
-    l.municipality,
-    l.id,
-    l.lts,
-    l.lts_access,
-    l.all_access,
-    l.cycling_allowed,
-    l.source,
-    l.target,
-    l.geometry
+    e.name,
+    e.municipality,
+    e.id,
+    e.lts,
+    e.lts_access,
+    e.all_access,
+    e.cycling_allowed,
+    e.source,
+    e.target,
+    e.geometry
 FROM
-    lts_3_gaps l
-    LEFT JOIN components_3 co1 ON l.source = co1.node
-    LEFT JOIN components_3 co2 ON l.target = co2.node
+    edges AS e
+    JOIN nodes_lts_4 AS ns ON e.source = ns.node
+    JOIN nodes_lts_4 AS nt ON e.target = nt.node
+    LEFT JOIN components_4 AS co1 ON e.source = co1.node
+    LEFT JOIN components_4 AS co2 ON e.target = co2.node
 WHERE
-    co1.component <> co2.component;
-
--- LTS 4 GAPS
-CREATE TABLE lts_4_gaps AS WITH node_sel AS (
-    SELECT
-        node
-    FROM
-        nodes_lts_4
-    ORDER BY
-        node
-),
-lts_4_gaps AS (
-    SELECT
-        "name",
-        municipality,
-        id,
-        lts,
-        lts_access,
-        all_access,
-        cycling_allowed,
-        km,
-        source,
-        target,
-        geometry
-    FROM
-        edges
-    WHERE
-        lts NOT IN (1, 2, 3, 4)
-        AND km <= 0.030
-        AND all_access = TRUE
-        AND source IN (
-            SELECT
-                *
-            FROM
-                node_sel
-        )
-        AND target IN (
-            SELECT
-                *
-            FROM
-                node_sel
-        )
-)
-SELECT
-    l.name,
-    l.municipality,
-    l.id,
-    l.lts,
-    l.lts_access,
-    l.all_access,
-    l.cycling_allowed,
-    l.source,
-    l.target,
-    l.geometry
-FROM
-    lts_4_gaps l
-    LEFT JOIN components_1 co1 ON l.source = co1.node
-    LEFT JOIN components_1 co2 ON l.target = co2.node
-WHERE
-    co1.component <> co2.component;
+    e.lts <> 4
+    AND e.km <= 0.020
+    AND e.all_access = TRUE
+    AND co1.component <> co2.component;
 
 CREATE INDEX IF NOT EXISTS lts1_gap_geom_ix ON lts_1_gaps USING GIST (geometry);
 
@@ -402,7 +241,7 @@ too_long_gaps AS (
         merged m
         INNER JOIN lts_1_gaps l ON ST_Intersects(l.geometry, m.geometry)
     WHERE
-        ST_Length(m.geometry) > 40
+        ST_Length(m.geometry) > 30
 )
 DELETE FROM
     lts_1_gaps
@@ -429,7 +268,7 @@ too_long_gaps AS (
         merged m
         INNER JOIN lts_2_gaps l ON ST_Intersects(l.geometry, m.geometry)
     WHERE
-        ST_Length(m.geometry) > 40
+        ST_Length(m.geometry) > 30
 )
 DELETE FROM
     lts_2_gaps
@@ -456,7 +295,7 @@ too_long_gaps AS (
         merged m
         INNER JOIN lts_3_gaps l ON ST_Intersects(l.geometry, m.geometry)
     WHERE
-        ST_Length(m.geometry) > 40
+        ST_Length(m.geometry) > 30
 )
 DELETE FROM
     lts_3_gaps
@@ -483,7 +322,7 @@ too_long_gaps AS (
         merged m
         INNER JOIN lts_4_gaps l ON ST_Intersects(l.geometry, m.geometry)
     WHERE
-        ST_Length(m.geometry) > 40
+        ST_Length(m.geometry) > 30
 )
 DELETE FROM
     lts_4_gaps
@@ -496,6 +335,26 @@ WHERE
     );
 
 -- CONSIDER - close gaps for pedestrian, no cycling, etc?
+DELETE FROM
+    lts_1_gaps
+WHERE
+    lts_access IN (0, 7);
+
+DELETE FROM
+    lts_2_gaps
+WHERE
+    lts_access IN (0, 7);
+
+DELETE FROM
+    lts_3_gaps
+WHERE
+    lts_access IN (0, 7);
+
+DELETE FROM
+    lts_4_gaps
+WHERE
+    lts_access IN (0, 7);
+
 UPDATE
     edges
 SET
@@ -544,16 +403,35 @@ WHERE
             lts_4_gaps
     );
 
--- DROP TABLE IF EXISTS lts_1_gaps;
--- DROP TABLE IF EXISTS lts_2_gaps;
--- DROP TABLE IF EXISTS lts_3_gaps;
--- DROP TABLE IF EXISTS lts_4_gaps;
--- DROP MATERIALIZED VIEW IF EXISTS nodes_lts_1;
--- DROP MATERIALIZED VIEW IF EXISTS nodes_lts_2;
--- DROP MATERIALIZED VIEW IF EXISTS nodes_lts_3;
--- DROP MATERIALIZED VIEW IF EXISTS nodes_lts_4;
--- DROP TABLE IF EXISTS components;
--- DROP TABLE IF EXISTS components_1;
--- DROP TABLE IF EXISTS components_2;
--- DROP TABLE IF EXISTS components_3;
--- DROP TABLE IF EXISTS components_4;
+DROP TABLE IF EXISTS lts_1_gaps;
+
+DROP TABLE IF EXISTS lts_2_gaps;
+
+DROP TABLE IF EXISTS lts_3_gaps;
+
+DROP TABLE IF EXISTS lts_4_gaps;
+
+DROP MATERIALIZED VIEW IF EXISTS nodes_lts_1;
+
+DROP MATERIALIZED VIEW IF EXISTS nodes_lts_2;
+
+DROP MATERIALIZED VIEW IF EXISTS nodes_lts_3;
+
+DROP MATERIALIZED VIEW IF EXISTS nodes_lts_4;
+
+DROP TABLE IF EXISTS components;
+
+DROP TABLE IF EXISTS components_1;
+
+DROP TABLE IF EXISTS components_2;
+
+DROP TABLE IF EXISTS components_3;
+
+DROP TABLE IF EXISTS components_4;
+
+ALTER TABLE
+    edges DROP COLUMN component_all,
+    DROP COLUMN component_1,
+    DROP COLUMN component_2,
+    DROP COLUMN component_3,
+    DROP COLUMN component_4;
