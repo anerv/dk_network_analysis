88d7ec3063c4e4f91f7105a18c9a3b7f184d682e
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-04-09 18:09:18 +0200

    new cells updated

--- scripts/03_compute_network_density.py
+++ scripts/03_compute_network_density.py
@@ -29,6 +29,10 @@ print("Settings loaded!")
 
 engine = dbf.connect_alc(db_name, db_user, db_password, db_port=db_port)
 
+connection = dbf.connect_pg(db_name, db_user, db_password, db_port=db_port)
+
+
+# %%
 q = f"SELECT ST_Union(geometry) as geometry FROM adm_boundaries;"
 
 study_area_poly = gpd.GeoDataFrame.from_postgis(
@@ -44,8 +48,6 @@ h3_grid.columns = h3_grid.columns.str.lower()
 dbf.to_postgis(geodataframe=h3_grid, table_name="h3_grid", engine=engine)
 
 print("H3 grid created and saved to database!")
-
-connection = dbf.connect_pg(db_name, db_user, db_password, db_port=db_port)
 
 q = "SELECT hex_id FROM h3_grid LIMIT 10;"
 
