c7addd25a08baccf0b751d237c8ce222fc0d24d8
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-04-10 16:13:48 +0200

    drop tables

--- scripts/sql/02b_compute_initial_components.sql
+++ scripts/sql/02b_compute_initial_components.sql
@@ -112,8 +112,12 @@ WHERE
     e.source = co.node
     AND lts_access IN (1, 2, 3, 4);
 
--- DROP TABLE IF EXISTS components;
--- DROP TABLE IF EXISTS components_1;
--- DROP TABLE IF EXISTS components_2;
--- DROP TABLE IF EXISTS components_3;
--- DROP TABLE IF EXISTS components_4;
+DROP TABLE IF EXISTS components;
+
+DROP TABLE IF EXISTS components_1;
+
+DROP TABLE IF EXISTS components_2;
+
+DROP TABLE IF EXISTS components_3;
+
+DROP TABLE IF EXISTS components_4;
