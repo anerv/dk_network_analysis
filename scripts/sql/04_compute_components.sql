51192354db7d29cd2366717a7f359b3d86675efe
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-04-15 08:44:30 +0200

    components done

--- scripts/sql/04_compute_components.sql
+++ scripts/sql/04_compute_components.sql
@@ -1,3 +1,23 @@
+DROP TABLE IF EXISTS components;
+
+DROP TABLE IF EXISTS components_1;
+
+DROP TABLE IF EXISTS components_2;
+
+DROP TABLE IF EXISTS components_3;
+
+DROP TABLE IF EXISTS components_4;
+
+DROP TABLE IF EXISTS components_car;
+
+ALTER TABLE
+    edges DROP COLUMN IF EXISTS component_all,
+    DROP COLUMN IF EXISTS component_1,
+    DROP COLUMN IF EXISTS component_1_2,
+    DROP COLUMN IF EXISTS component_1_3,
+    DROP COLUMN IF EXISTS component_1_4,
+    DROP COLUMN IF EXISTS component_car;
+
 ALTER TABLE
     edges
 ADD
@@ -70,10 +90,11 @@ CREATE TABLE components_car AS (
         *
     FROM
         pgr_connectedComponents(
-            'SELECT id, source, target, cost, reverse_cost FROM edges WHERE car_traffic IS TRUE'
+            'SELECT id, source, target, cost, reverse_cost FROM edges WHERE car_traffic IS TRUE AND lts_access IN (1,2,3,4,7)' -- Should we include gaps here?
         )
 );
 
+-- TODO - FIX!!
 UPDATE
     edges e
 SET
@@ -81,7 +102,14 @@ SET
 FROM
     components co
 WHERE
-    e.source = co.node;
+    e.source = co.node
+    AND (
+        lts_access IN (1, 2, 3, 4, 7)
+        OR lts_1_gap IS TRUE
+        OR lts_2_gap IS TRUE
+        OR lts_3_gap IS TRUE
+        OR lts_4_gap IS TRUE
+    );
 
 UPDATE
     edges e
@@ -109,8 +137,6 @@ WHERE
         OR lts_1_gap IS TRUE
         OR lts_2_gap IS TRUE
     );
-
-;
 
 UPDATE
     edges e
@@ -151,16 +177,278 @@ FROM
     components_car co
 WHERE
     e.source = co.node
-    AND car_traffic IS TRUE;
+    AND car_traffic IS TRUE
+    AND lts_access IN (1, 2, 3, 4, 7);
+
+--- CHECK COMPONENT COMPUTATION -- 
+-- CHECK FOR TOO MANY COMP VALUES
+DO $$
+DECLARE
+    comp_check INT;
+
+BEGIN
+    SELECT
+        COUNT(*) INTO comp_check
+    FROM
+        edges
+    WHERE
+        lts_access <> 1
+        AND lts_1_gap IS NULL
+        AND component_1 IS NOT NULL;
+
+ASSERT comp_check = 0,
+'Problem with component calculation for LTS 1';
+
+END $$;
+
+DO $$
+DECLARE
+    comp_check INT;
+
+BEGIN
+    SELECT
+        COUNT(*) INTO comp_check
+    FROM
+        edges
+    WHERE
+        lts_access NOT IN (1, 2)
+        AND lts_1_gap IS NULL
+        AND lts_2_gap IS NULL
+        AND component_1_2 IS NOT NULL;
+
+ASSERT comp_check = 0,
+'Problem with component calculation for LTS 1_2';
+
+END $$;
+
+DO $$
+DECLARE
+    comp_check INT;
+
+BEGIN
+    SELECT
+        COUNT(*) INTO comp_check
+    FROM
+        edges
+    WHERE
+        lts_access NOT IN (1, 2, 3)
+        AND lts_1_gap IS NULL
+        AND lts_2_gap IS NULL
+        AND lts_3_gap IS NULL
+        AND component_1_3 IS NOT NULL;
+
+ASSERT comp_check = 0,
+'Problem with component calculation for LTS 1_3';
+
+END $$;
+
+DO $$
+DECLARE
+    comp_check INT;
+
+BEGIN
+    SELECT
+        COUNT(*) INTO comp_check
+    FROM
+        edges
+    WHERE
+        lts_access NOT IN (1, 2, 3, 4)
+        AND lts_1_gap IS NULL
+        AND lts_2_gap IS NULL
+        AND lts_3_gap IS NULL
+        AND lts_4_gap IS NULL
+        AND component_1_4 IS NOT NULL;
+
+ASSERT comp_check = 0,
+'Problem with component calculation for LTS 1_4';
+
+END $$;
+
+DO $$
+DECLARE
+    comp_check INT;
+
+BEGIN
+    SELECT
+        COUNT(*) INTO comp_check
+    FROM
+        edges
+    WHERE
+        lts_access NOT IN (1, 2, 3, 4, 7)
+        AND lts_1_gap IS NULL
+        AND lts_2_gap IS NULL
+        AND lts_3_gap IS NULL
+        AND lts_4_gap IS NULL
+        AND component_all IS NOT NULL;
+
+ASSERT comp_check = 0,
+'Problem with component calculation for component all';
+
+END $$;
+
+DO $$
+DECLARE
+    comp_check INT;
+
+BEGIN
+    SELECT
+        COUNT(*) INTO comp_check
+    FROM
+        edges
+    WHERE
+        (
+            car_traffic IS FALSE
+            OR lts_access NOT IN (1, 2, 3, 4, 7)
+        )
+        AND component_car IS NOT NULL;
+
+ASSERT comp_check = 0,
+'Problem with component calculation for car components';
+
+END $$;
+
+----- CHECK FOR MISSING COMP VALUES
+DO $$
+DECLARE
+    comp_check INT;
+
+BEGIN
+    SELECT
+        COUNT(*) INTO comp_check
+    FROM
+        edges
+    WHERE
+        (
+            lts_access = 1
+            OR lts_1_gap IS TRUE
+        )
+        AND component_1 IS NULL;
+
+ASSERT comp_check = 0,
+'Problem with component calculation for LTS 1';
+
+END $$;
+
+DO $$
+DECLARE
+    comp_check INT;
+
+BEGIN
+    SELECT
+        COUNT(*) INTO comp_check
+    FROM
+        edges
+    WHERE
+        (
+            lts_access IN (1, 2)
+            OR lts_1_gap IS TRUE
+            OR lts_2_gap IS TRUE
+        )
+        AND component_1_2 IS NULL;
+
+ASSERT comp_check = 0,
+'Problem with component calculation for LTS 1_2';
+
+END $$;
+
+DO $$
+DECLARE
+    comp_check INT;
+
+BEGIN
+    SELECT
+        COUNT(*) INTO comp_check
+    FROM
+        edges
+    WHERE
+        (
+            lts_access IN (1, 2, 3)
+            OR lts_1_gap IS TRUE
+            OR lts_2_gap IS TRUE
+            OR lts_3_gap IS TRUE
+        )
+        AND component_1_3 IS NULL;
+
+ASSERT comp_check = 0,
+'Problem with component calculation for LTS 1_3';
+
+END $$;
+
+DO $$
+DECLARE
+    comp_check INT;
+
+BEGIN
+    SELECT
+        COUNT(*) INTO comp_check
+    FROM
+        edges
+    WHERE
+        (
+            lts_access IN (1, 2, 3, 4)
+            OR lts_1_gap IS TRUE
+            OR lts_2_gap IS TRUE
+            OR lts_3_gap IS TRUE
+            OR lts_4_gap IS TRUE
+        )
+        AND component_1_4 IS NULL;
+
+ASSERT comp_check = 0,
+'Problem with component calculation for LTS 1_4';
+
+END $$;
+
+DO $$
+DECLARE
+    comp_check INT;
+
+BEGIN
+    SELECT
+        COUNT(*) INTO comp_check
+    FROM
+        edges
+    WHERE
+        (
+            lts_access IN (1, 2, 3, 4, 7)
+            OR lts_1_gap IS TRUE
+            OR lts_2_gap IS TRUE
+            OR lts_3_gap IS TRUE
+            OR lts_4_gap IS TRUE
+        )
+        AND component_all IS NULL;
+
+ASSERT comp_check = 0,
+'Problem with component calculation for component all';
+
+END $$;
+
+DO $$
+DECLARE
+    comp_check INT;
+
+BEGIN
+    SELECT
+        COUNT(*) INTO comp_check
+    FROM
+        edges
+    WHERE
+        car_traffic IS TRUE
+        AND lts_access IN (1, 2, 3, 4, 7)
+        AND component_car IS NULL;
+
+ASSERT comp_check = 0,
+'Problem with component calculation for car components';
+
+END $$;
 
 DROP TABLE IF EXISTS components;
 
 DROP TABLE IF EXISTS components_1;
 
-DROP TABLE IF EXISTS components_1_2;
+DROP TABLE IF EXISTS components_2;
 
-DROP TABLE IF EXISTS components_1_3;
+DROP TABLE IF EXISTS components_3;
 
-DROP TABLE IF EXISTS components_1_4;
+DROP TABLE IF EXISTS components_4;
 
 DROP TABLE IF EXISTS components_car;
