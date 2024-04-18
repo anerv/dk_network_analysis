d11fcab1e1fa7e5780ceffea8572006b5d9ead26
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-04-18 16:36:18 +0200

    moved settings

---
+++ scripts/settings/yaml_variables.py
@@ -0,0 +1,23 @@
+import yaml
+from src import db_functions as dbf
+
+with open(r"../../config.yml") as file:
+    parsed_yaml_file = yaml.load(file, Loader=yaml.FullLoader)
+
+    db_name = parsed_yaml_file["db_name"]
+    input_db_name = parsed_yaml_file["input_db_name"]
+    db_user = parsed_yaml_file["db_user"]
+    db_password = parsed_yaml_file["db_password"]
+    db_host = parsed_yaml_file["db_host"]
+    db_port = parsed_yaml_file["db_port"]
+    network_edges = parsed_yaml_file["network_edges"]
+    network_nodes = parsed_yaml_file["network_nodes"]
+
+    adm_fp = parsed_yaml_file["adm_boundaries_fp"]
+    socio_fp = parsed_yaml_file["socio_fp"]
+
+    crs = parsed_yaml_file["CRS"]
+
+    h3_resolution = parsed_yaml_file["h3_resolution"]
+
+print("Settings loaded!")
