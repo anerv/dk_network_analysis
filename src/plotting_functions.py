fc753886b9f7edd474bd5b0f7e7cafb9c6f5b34c
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-04-18 16:36:33 +0200

    new func

--- src/plotting_functions.py
+++ src/plotting_functions.py
@@ -1,8 +1,152 @@
 import matplotlib as mpl
 from matplotlib import cm, colors
-
+import matplotlib.pyplot as plt
 import matplotlib_inline.backend_inline
+import contextily as cx
+
+exec(open("../settings/yaml_variables.py").read())
+exec(open("../settings/plotting.py").read())
 
 
 def set_renderer(f="svg"):
     matplotlib_inline.backend_inline.set_matplotlib_formats(f)
+
+
+def plot_polygon_results(
+    poly_gdf,
+    plot_cols,
+    plot_titles,
+    filepaths,
+    cmaps,
+    alpha,
+    cx_tile,
+    no_data_cols,
+    na_facecolor=pdict["nodata_face"],
+    na_edgecolor=pdict["nodata_edge"],
+    na_linewidth=pdict["line_nodata"],
+    na_hatch=pdict["nodata_hatch"],
+    na_alpha=pdict["alpha_nodata"],
+    na_legend=nodata_patch,
+    figsize=pdict["fsmap"],
+    dpi=pdict["dpi"],
+    crs=crs,
+    legend=True,
+    set_axis_off=True,
+    legend_loc="upper left",
+    use_norm=False,
+    norm_min=None,
+    norm_max=None,
+    plot_res=plot_res,
+    attr=None,
+):
+    """
+    Make multiple choropleth maps of e.g. poly_gdf with analysis results based on a list of geodataframe columns to be plotted
+    and save them in separate files
+    Arguments:
+        poly_gdf (gdf): geodataframe with polygons to be plotted
+        plot_cols (list): list of column names (strings) to be plotted
+        plot_titles (list): list of strings to be used as plot titles
+        cmaps (list): list of color maps
+        alpha(numeric): value between 0-1 for setting the transparency of the plots
+        cx_tile(cx tileprovider): name of contextily tile to be used for base map
+        no_data_cols(list): list of column names used for generating no data layer in each plot
+        na_facecolor(string): name of color used for the no data layer fill
+        na_edegcolor(string): name of color used for the no data layer outline
+        na_hatch: hatch pattern used for no data layer
+        na_alpha (numeric): value between 0-1 for setting the transparency of the plots
+        na_linewidth (numeric): width of edge lines of no data poly_gdf cells
+        na_legend(matplotlib Patch): patch to be used for the no data layer in the legend
+        figsize(tuple): size of each plot
+        dpi(numeric): resolution of saved plots
+        crs (string): name of crs used for the poly_gdf (to set correct crs of basemap)
+        legend (bool): True if a legend/colorbar should be plotted
+        set_axis_off (bool): True if axis ticks and values should be omitted
+        legend_loc (string): Position of map legend (see matplotlib doc for valid entries)
+        use_norm (bool): True if colormap should be defined based on provided min and max values
+        norm_min(numeric): min value to use for norming color map
+        norm_max(numeric): max value to use for norming color map
+        attr (string): optional attribution
+
+    Returns:
+        None
+    """
+
+    if use_norm is True:
+        assert norm_min is not None, print("Please provide a value for norm_min")
+        assert norm_max is not None, print("Please provide a value for norm_max")
+
+    for i, c in enumerate(plot_cols):
+
+        fig, ax = plt.subplots(1, figsize=figsize)
+
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="3.5%", pad="1%")
+
+        if use_norm is True:
+
+            cbnorm = colors.Normalize(vmin=norm_min[i], vmax=norm_max[i])
+
+            poly_gdf.plot(
+                cax=cax,
+                ax=ax,
+                column=c,
+                legend=legend,
+                alpha=alpha,
+                norm=cbnorm,
+                cmap=cmaps[i],
+            )
+
+        else:
+            poly_gdf.plot(
+                cax=cax,
+                ax=ax,
+                column=c,
+                legend=legend,
+                alpha=alpha,
+                cmap=cmaps[i],
+            )
+        cx.add_basemap(ax=ax, crs=crs, source=cx_tile)
+
+        if attr is not None:
+            cx.add_attribution(ax=ax, text="(C) " + attr)
+            txt = ax.texts[-1]
+            txt.set_position([1, 0.00])
+            txt.set_ha("right")
+            txt.set_va("bottom")
+
+        ax.set_title(plot_titles[i])
+
+        if set_axis_off:
+            ax.set_axis_off()
+
+        # add patches in poly_gdf cells with no data on edges
+        if type(no_data_cols[i]) == tuple:
+
+            poly_gdf[
+                (poly_gdf[no_data_cols[i][0]].isnull())
+                & (poly_gdf[no_data_cols[i][1]].isnull())
+            ].plot(
+                ax=ax,
+                facecolor=na_facecolor,
+                edgecolor=na_edgecolor,
+                linewidth=na_linewidth,
+                hatch=na_hatch,
+                alpha=na_alpha,
+            )
+
+        else:
+            poly_gdf[poly_gdf[no_data_cols[i]].isnull()].plot(
+                ax=ax,
+                facecolor=na_facecolor,
+                edgecolor=na_edgecolor,
+                linewidth=na_linewidth,
+                hatch=na_hatch,
+                alpha=na_alpha,
+            )
+
+        ax.legend(handles=[na_legend], loc=legend_loc)
+
+        if plot_res == "high":
+            fig.savefig(filepaths[i] + ".svg", dpi=dpi)
+        else:
+            fig.savefig(filepaths[i] + ".png", dpi=dpi)
