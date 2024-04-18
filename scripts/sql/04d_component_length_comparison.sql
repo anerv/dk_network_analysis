f9511b44a24bc84eeb4f16a1c202d1beedd5e2a9
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-04-18 15:59:38 +0200

    specified cols

--- scripts/sql/04d_component_length_comparison.sql
+++ scripts/sql/04d_component_length_comparison.sql
@@ -1,23 +1,80 @@
 CREATE VIEW component_length_muni AS (
     SELECT
-        * -- only include necesssary columns (e.g. lts length, municipality, geometry)
+        dens.municipality,
+        dens.lts_1_length,
+        dens.lts_2_length,
+        dens.lts_3_length,
+        dens.lts_4_length,
+        dens.total_car_length,
+        dens.total_network_length,
+        dens.lts_1_dens,
+        dens.lts_1_2_dens,
+        dens.lts_1_3_dens,
+        dens.lts_1_4_dens,
+        dens.total_car_dens,
+        dens.total_network_dens,
+        den.geometry,
+        comp.comp_all_count,
+        comp.comp_1_count,
+        comp.comp_2_count,
+        comp.comp_3_count,
+        comp.comp_4_count,
+        comp.comp_car_count
     FROM
-        density_municipality
-        JOIN comp_count_muni USING (municipality)
+        density_municipality dens
+        JOIN comp_count_muni comp USING (municipality)
 );
 
 CREATE VIEW component_length_socio AS (
     SELECT
-        *
+        dens.municipality,
+        dens.lts_1_length,
+        dens.lts_2_length,
+        dens.lts_3_length,
+        dens.lts_4_length,
+        dens.total_car_length,
+        dens.total_network_length,
+        dens.lts_1_dens,
+        dens.lts_1_2_dens,
+        dens.lts_1_3_dens,
+        dens.lts_1_4_dens,
+        dens.total_car_dens,
+        dens.total_network_dens,
+        den.geometry,
+        comp.comp_all_count,
+        comp.comp_1_count,
+        comp.comp_2_count,
+        comp.comp_3_count,
+        comp.comp_4_count,
+        comp.comp_car_count
     FROM
-        density_municipality
-        JOIN comp_count_muni USING (municipality)
+        density_municipality dens
+        JOIN comp_count_muni comp USING (municipality)
 );
 
 CREATE VIEW component_length_h3 AS (
     SELECT
-        *
+        dens.municipality,
+        dens.lts_1_length,
+        dens.lts_2_length,
+        dens.lts_3_length,
+        dens.lts_4_length,
+        dens.total_car_length,
+        dens.total_network_length,
+        dens.lts_1_dens,
+        dens.lts_1_2_dens,
+        dens.lts_1_3_dens,
+        dens.lts_1_4_dens,
+        dens.total_car_dens,
+        dens.total_network_dens,
+        den.geometry,
+        comp.comp_all_count,
+        comp.comp_1_count,
+        comp.comp_2_count,
+        comp.comp_3_count,
+        comp.comp_4_count,
+        comp.comp_car_count
     FROM
-        density_municipality
-        JOIN comp_count_muni USING (municipality)
+        density_municipality dens
+        JOIN comp_count_muni comp USING (municipality)
 );
