0419bacf56b30aa890f9abf05838220f7a9ae4a2
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-04-19 16:35:06 +0200

    debugged

--- scripts/sql/04c_compute_local_component_count.sql
+++ scripts/sql/04c_compute_local_component_count.sql
@@ -1,16 +1,35 @@
-CREATE TABLE comp_count_muni AS (
+DROP TABLE IF EXISTS comp_count_muni;
+
+DROP TABLE IF EXISTS comp_count_socio;
+
+DROP TABLE IF EXISTS comp_count_h3;
+
+CREATE TABLE comp_count_muni AS WITH comp_count AS (
     SELECT
         COUNT(DISTINCT component_all) AS comp_all_count,
         COUNT(DISTINCT component_1) AS comp_1_count,
         COUNT(DISTINCT component_1_2) AS comp_2_count,
         COUNT(DISTINCT component_1_3) AS comp_3_count,
         COUNT(DISTINCT component_1_4) AS comp_4_count,
-        COUNT(DISTINCT component_car) AS comp_car_count
+        COUNT(DISTINCT component_car) AS comp_car_count,
+        municipality
     FROM
         component_edges
     GROUP BY
         municipality
-);
+)
+SELECT
+    co.municipality,
+    co.comp_all_count,
+    co.comp_1_count,
+    co.comp_2_count,
+    co.comp_3_count,
+    co.comp_4_count,
+    co.comp_car_count,
+    mu.geometry
+FROM
+    comp_count co
+    JOIN adm_boundaries mu ON co.municipality = mu.navn;
 
 -- JOIN SPLIT EDGES TO COMPONENT EDGES
 CREATE TABLE socio_component_edges AS
@@ -43,8 +62,9 @@ FROM
     h3_edges h
     JOIN component_edges co ON h.id = co.id;
 
-CREATE TABLE comp_count_socio AS (
+CREATE TABLE comp_count_socio AS WITH comp_count AS (
     SELECT
+        socio_id,
         COUNT(DISTINCT component_all) AS comp_all_count,
         COUNT(DISTINCT component_1) AS comp_1_count,
         COUNT(DISTINCT component_1_2) AS comp_2_count,
@@ -55,10 +75,23 @@ CREATE TABLE comp_count_socio AS (
         socio_component_edges
     GROUP BY
         socio_id
-);
+)
+SELECT
+    co.socio_id,
+    co.comp_all_count,
+    co.comp_1_count,
+    co.comp_2_count,
+    co.comp_3_count,
+    co.comp_4_count,
+    co.comp_car_count,
+    so.geometry
+FROM
+    comp_count co
+    JOIN socio so ON co.socio_id = so.id;
 
-CREATE TABLE comp_count_h3 AS (
+CREATE TABLE comp_count_h3 AS WITH comp_count AS (
     SELECT
+        h3_id,
         COUNT(DISTINCT component_all) AS comp_all_count,
         COUNT(DISTINCT component_1) AS comp_1_count,
         COUNT(DISTINCT component_1_2) AS comp_2_count,
@@ -69,4 +102,16 @@ CREATE TABLE comp_count_h3 AS (
         h3_component_edges
     GROUP BY
         h3_id
-);
+)
+SELECT
+    co.h3_id,
+    co.comp_all_count,
+    co.comp_1_count,
+    co.comp_2_count,
+    co.comp_3_count,
+    co.comp_4_count,
+    co.comp_car_count,
+    h.geometry
+FROM
+    comp_count co
+    JOIN h3_grid h ON co.h3_id = h.hex_id;
