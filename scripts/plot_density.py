3b4b4ce92ade83cd428d4ccfc4e217b30bf5558b
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-04-24 17:36:22 +0200

    cleanup

--- scripts/plot_density.py
+++ scripts/plot_density.py
@@ -4,8 +4,11 @@ from src import plotting_functions as plot_func
 import geopandas as gpd
 import numpy as np
 import seaborn as sns
+import pandas as pd
+from matplotlib import pyplot as plt
+import plotly.express as px
 
-# sns.set_theme("paper")
+sns.set_theme("paper")
 
 exec(open("../settings/yaml_variables.py").read())
 exec(open("../settings/plotting.py").read())
@@ -15,8 +18,7 @@ engine = dbf.connect_alc(db_name, db_user, db_password, db_port=db_port)
 connection = dbf.connect_pg(db_name, db_user, db_password, db_port=db_port)
 
 # %%
-# PLOT MUNI DENSITY
-
+### READ DATA ###
 density_muni = gpd.GeoDataFrame.from_postgis(
     "SELECT * FROM density_municipality;",
     engine,
@@ -25,105 +27,6 @@ density_muni = gpd.GeoDataFrame.from_postgis(
 )
 density_muni.replace(0, np.nan, inplace=True)
 
-# %%
-# Individual LTS density
-
-plot_cols = [
-    "lts_1_dens",
-    "lts_2_dens",
-    "lts_3_dens",
-    "lts_4_dens",
-    "total_car_dens",
-    "total_network_dens",
-]
-labels = ["LTS 1", "LTS 2", "LTS 3", "LTS 4", "Total car", "Total network"]
-plot_titles = [f"Municipal network density for: {l}" for l in labels]
-filepaths = [f"../results/network_density/administrative/{l}" for l in labels]
-
-for i, p in enumerate(plot_cols):
-
-    plot_func.plot_classified_poly(
-        gdf=density_muni,
-        plot_col=p,
-        scheme="quantiles",
-        cx_tile=cx_tile_2,
-        plot_na=True,
-        cmap=pdict["pos"],
-        edgecolor="none",
-        title=plot_titles[i],
-        fp=filepaths[i],
-    )
-
-# %%
-# Stepwise density
-plot_cols = [
-    "lts_1_dens",
-    "lts_1_2_dens",
-    "lts_1_3_dens",
-    "lts_1_4_dens",
-    "total_car_dens",
-    "total_network_dens",
-]
-labels = ["LTS 1", "LTS 1-2", "LTS 1-3", "LTS 1-4", "Total car", "Total network"]
-plot_titles = [f"Municipal network density for: {l}" for l in labels]
-filepaths = [f"../results/network_density/administrative/{l}" for l in labels]
-
-for i, p in enumerate(plot_cols):
-
-    plot_func.plot_classified_poly(
-        gdf=density_muni,
-        plot_col=p,
-        scheme="quantiles",
-        cx_tile=cx_tile_2,
-        plot_na=True,
-        cmap=pdict["pos"],
-        edgecolor="none",
-        title=plot_titles[i],
-        fp=filepaths[i],
-    )
-
-# %%
-
-# Relative network length
-plot_cols = [
-    "lts_1_length_rel",
-    "lts_1_2_length_rel",
-    "lts_1_3_length_rel",
-    "lts_1_4_length_rel",
-    "lts_7_length_rel",
-    # "total_car_length_rel",
-]
-for p in plot_cols:
-    density_muni[p] = density_muni[p] * 100
-
-labels = [
-    "LTS 1 (%)",
-    "LTS 1-2 (%)",
-    "LTS 1-3 (%)",
-    "LTS 1-4 (%)",
-    "No biking (%)",
-    # "Total car (%)",
-]
-
-plot_titles = [f"Municipal network length for: {l}" for l in labels]
-filepaths = [f"../results/network_density/administrative/{l}" for l in labels]
-
-for i, p in enumerate(plot_cols):
-
-    plot_func.plot_classified_poly(
-        gdf=density_muni,
-        plot_col=p,
-        scheme="quantiles",
-        cx_tile=cx_tile_2,
-        plot_na=True,
-        cmap=pdict["pos"],
-        edgecolor="none",
-        title=plot_titles[i],
-        fp=filepaths[i],
-    )
-# %%
-# SOCIO
-# Individual LTS density
 density_socio = gpd.GeoDataFrame.from_postgis(
     "SELECT * FROM density_socio;",
     engine,
@@ -132,100 +35,6 @@ density_socio = gpd.GeoDataFrame.from_postgis(
 )
 
 density_socio.replace(0, np.nan, inplace=True)
-
-plot_cols = [
-    "lts_1_dens",
-    "lts_2_dens",
-    "lts_3_dens",
-    "lts_4_dens",
-    "total_car_dens",
-    "total_network_dens",
-]
-labels = ["LTS 1", "LTS 2", "LTS 3", "LTS 4", "Total car", "Total network"]
-plot_titles = [f"Socio network density for: {l}" for l in labels]
-filepaths = [f"../results/network_density/socio/{l}" for l in labels]
-
-for i, p in enumerate(plot_cols):
-
-    plot_func.plot_classified_poly(
-        gdf=density_socio,
-        plot_col=p,
-        scheme="quantiles",
-        cx_tile=cx_tile_2,
-        plot_na=True,
-        cmap=pdict["pos"],
-        edgecolor="none",
-        title=plot_titles[i],
-        fp=filepaths[i],
-    )
-
-# Stepwise density
-plot_cols = [
-    "lts_1_dens",
-    "lts_1_2_dens",
-    "lts_1_3_dens",
-    "lts_1_4_dens",
-    "total_car_dens",
-    "total_network_dens",
-]
-labels = ["LTS 1", "LTS 1-2", "LTS 1-3", "LTS 1-4", "Total car", "Total network"]
-plot_titles = [f"Socio network density for: {l}" for l in labels]
-filepaths = [f"../results/network_density/socio/{l}" for l in labels]
-
-for i, p in enumerate(plot_cols):
-
-    plot_func.plot_classified_poly(
-        gdf=density_socio,
-        plot_col=p,
-        scheme="quantiles",
-        cx_tile=cx_tile_2,
-        plot_na=True,
-        cmap=pdict["pos"],
-        edgecolor="none",
-        title=plot_titles[i],
-        fp=filepaths[i],
-    )
-
-# Relative network length
-plot_cols = [
-    "lts_1_length_rel",
-    "lts_1_2_length_rel",
-    "lts_1_3_length_rel",
-    "lts_1_4_length_rel",
-    "lts_7_length_rel",
-    # "total_car_length_rel",
-]
-for p in plot_cols:
-    density_socio[p] = density_socio[p] * 100
-
-labels = [
-    "LTS 1 (%)",
-    "LTS 1-2 (%)",
-    "LTS 1-3 (%)",
-    "LTS 1-4 (%)",
-    "No biking (%)",
-    # "Total car (%)",
-]
-
-plot_titles = [f"Socio network length for: {l}" for l in labels]
-filepaths = [f"../results/network_density/socio/{l}" for l in labels]
-
-for i, p in enumerate(plot_cols):
-
-    plot_func.plot_classified_poly(
-        gdf=density_socio,
-        plot_col=p,
-        scheme="quantiles",
-        cx_tile=cx_tile_2,
-        plot_na=True,
-        cmap=pdict["pos"],
-        edgecolor="none",
-        title=plot_titles[i],
-        fp=filepaths[i],
-    )
-
-# %%
-# H3
 
 density_h3 = gpd.GeoDataFrame.from_postgis(
     "SELECT * FROM density_h3;",
@@ -237,122 +46,279 @@ density_h3 = gpd.GeoDataFrame.from_postgis(
 density_h3.replace(0, np.nan, inplace=True)
 
 # %%
+### COMPARE MUNICIPAL NETWORK DISTRIBUTION ####
 
-plot_cols = [
-    "lts_1_dens",
-    "lts_2_dens",
-    "lts_3_dens",
-    "lts_4_dens",
-    "total_car_dens",
-    "total_network_dens",
+municipalities = density_muni.municipality.unique()
+
+for m in municipalities:
+    data = density_muni[density_muni.municipality == m]
+
+    if len(data) > 0:
+
+        dens_list = data[density_columns].values[0]
+        length_list = data[length_columns].values[0]
+        lts = ["1", "2", "3", "4", "car", "all"]
+
+        df = pd.DataFrame({"density": dens_list, "length": length_list, "lts": lts})
+
+        plt.figure()
+
+        fig = sns.barplot(df, x="lts", y="length").set(title=f"{m} network length")
+
+        plt.show()
+
+        plt.close()
+
+for m in municipalities:
+    data = density_muni[density_muni.municipality == m]
+
+    if len(data) > 0:
+
+        dens_list = data[density_steps_columns].values[0]
+        length_list = data[length_steps_columns].values[0]
+        lts = ["1", "1-2", "1-3", "1-4", "car", "all"]
+
+        df = pd.DataFrame({"density": dens_list, "length": length_list, "lts": lts})
+
+        plt.figure()
+
+        fig = sns.barplot(df, x="lts", y="length").set(
+            title=f"{m} network length (stepwise)"
+        )
+
+        plt.show()
+
+        plt.close()
+
+# %%
+####### MAPS ##############################
+###########################################
+gdfs = [density_muni, density_socio, density_h3]
+
+all_plot_titles = [
+    "Municipal network density for: ",
+    "Socio network density for: ",
+    "Local network density for: ",
 ]
-labels = ["LTS 1", "LTS 2", "LTS 3", "LTS 4", "Total car", "Total network"]
-plot_titles = [f"Local network density for: {l}" for l in labels]
-filepaths = [f"../results/network_density/administrative/{l}.png" for l in labels]
+all_file_paths = [
+    "../results/network_density/administrative/",
+    "../results/network_density/socio/",
+    "../results/network_density/h3/",
+]
 
+for e, gdf in enumerate(gdfs):
 
-for i, p in enumerate(plot_cols):
+    print("At iteration: ", e)
+    ###### Plot individual LTS densities #####
 
-    plot_func.plot_classified_poly(
-        gdf=density_h3,
-        plot_col=p,
-        scheme="quantiles",
-        cx_tile=cx_tile_2,
-        plot_na=False,
-        cmap=pdict["pos"],
-        edgecolor="none",
-        title=plot_titles[i],
-        fp=filepaths[i],
+    plot_cols = density_columns
+
+    labels = ["LTS 1", "LTS 2", "LTS 3", "LTS 4", "Total car", "Total network"]
+    plot_titles = [all_plot_titles[e] + l for l in labels]
+    filepaths = [all_file_paths[e] + l for l in labels]
+
+    for i, p in enumerate(plot_cols):
+
+        plot_func.plot_classified_poly(
+            gdf=gdf,
+            plot_col=p,
+            scheme="quantiles",
+            cx_tile=cx_tile_2,
+            plot_na=True,
+            cmap=pdict["pos"],
+            edgecolor="none",
+            title=plot_titles[i],
+            fp=filepaths[i],
+        )
+
+    ###### Plot stepwise LTS densities #####
+    plot_cols = density_steps_columns
+
+    labels = ["LTS 1", "LTS 1-2", "LTS 1-3", "LTS 1-4", "Total car", "Total network"]
+    plot_titles = [all_plot_titles[e] + l for l in labels]
+    filepaths = [all_file_paths[e] + l for l in labels]
+
+    for i, p in enumerate(plot_cols):
+
+        plot_func.plot_classified_poly(
+            gdf=gdf,
+            plot_col=p,
+            scheme="quantiles",
+            cx_tile=cx_tile_2,
+            plot_na=True,
+            cmap=pdict["pos"],
+            edgecolor="none",
+            title=plot_titles[i],
+            fp=filepaths[i],
+        )
+
+    ###### Plot relative network length #####
+    plot_cols = length_relative_columns
+
+    for p in plot_cols:
+        gdf[p] = gdf[p] * 100
+
+    labels = [
+        "LTS 1 (%)",
+        "LTS 1-2 (%)",
+        "LTS 1-3 (%)",
+        "LTS 1-4 (%)",
+        "No biking (%)",
+        # "Total car (%)",
+    ]
+    plot_titles = [all_plot_titles[e] + l for l in labels]
+    filepaths = [all_file_paths[e] + l for l in labels]
+
+    for i, p in enumerate(plot_cols):
+
+        plot_func.plot_classified_poly(
+            gdf=gdf,
+            plot_col=p,
+            scheme="quantiles",
+            cx_tile=cx_tile_2,
+            plot_na=True,
+            cmap=pdict["pos"],
+            edgecolor="none",
+            title=plot_titles[i],
+            fp=filepaths[i],
+        )
+
+# %%
+
+####### PLOTS ##############################
+###########################################
+length_titles = [
+    "Municipal network length (km)",
+    "Local network length (km)",
+    "Grid network length (km)",
+]
+density_titles = [
+    "Municipal network density (km/sqkm)",
+    "Local network density (km/sqkm)",
+    "Grid network density (km/sqkm)",
+]
+id_cols = ["municipality", "id", "hex_id"]
+
+type_cols = ["Municipal", "Local", "Grid"]
+
+municipalities = density_muni.municipality.unique()
+socio_ids = density_socio.id.unique()
+h3_ids = density_h3.hex_id.unique()
+
+id_lists = [municipalities, socio_ids, h3_ids]
+
+filepaths_bar = [
+    "../results/network_density/administrative/lts_stacked_bar.jpg",
+    "../results/network_density/socio/lts_stacked_bar.jpg",
+    "../results/network_density/h3/lts_stacked_bar.jpg",
+]
+
+filepaths_violin = [
+    "../results/network_density/administrative/violin_",
+    "../results/network_density/socio/violin_",
+    "../results/network_density/h3/violin_",
+]
+
+for e, df in enumerate(gdfs):
+
+    # **** BAR CHARTS ****
+
+    dens_all = []
+    length_all = []
+    lts_all = []
+    ids_all = []
+
+    for i in id_lists[e]:
+        data = df[df[id_cols[e]] == i]
+
+        if len(data) > 0:
+
+            dens_list = data[density_columns].values[0]
+            length_list = data[length_columns].values[0]
+            lts = ["1", "2", "3", "4", "car", "all"]
+            ids = [i] * 6
+
+            dens_all.extend(dens_list)
+            length_all.extend(length_list)
+            lts_all.extend(lts)
+            ids_all.extend(ids)
+
+    df = pd.DataFrame(
+        {
+            "density": dens_all,
+            "length": length_all,
+            "lts": lts_all,
+            "id": ids_all,
+        }
     )
 
-# %%
-# Stepwise density
-plot_cols = [
-    "lts_1_dens",
-    "lts_1_2_dens",
-    "lts_1_3_dens",
-    "lts_1_4_dens",
-    "total_car_dens",
-    "total_network_dens",
-]
-labels = ["LTS 1", "LTS 1-2", "LTS 1-3", "LTS 1-4", "Total car", "Total network"]
-plot_titles = [f"Local network density for: {l}" for l in labels]
-filepaths = [f"../results/network_density/administrative/{l}.png" for l in labels]
-
-for i, p in enumerate(plot_cols):
-
-    plot_func.plot_classified_poly(
-        gdf=density_h3,
-        plot_col=p,
-        scheme="quantiles",
-        cx_tile=cx_tile_2,
-        plot_na=True,
-        cmap=pdict["pos"],
-        edgecolor="none",
-        title=plot_titles[i],
-        fp=filepaths[i],
+    fig = px.bar(
+        df.sort_values("length", ascending=False),
+        x="id",
+        y="length",
+        color="lts",
+        title=length_titles[e],
+        labels=plotly_labels,
+        color_discrete_map=lts_color_dict,
     )
 
-# %%
-# Relative network length
-plot_cols = [
-    "lts_1_length_rel",
-    "lts_1_2_length_rel",
-    "lts_1_3_length_rel",
-    "lts_1_4_length_rel",
-    "lts_7_length_rel",
-    # "total_car_length_rel",
-]
-for p in plot_cols:
-    density_h3[p] = density_h3[p] * 100
+    fig.show()
 
-labels = [
-    "LTS 1 (%)",
-    "LTS 1-2 (%)",
-    "LTS 1-3 (%)",
-    "LTS 1-4 (%)",
-    "No biking (%)",
-    # "Total car (%)",
-]
-
-plot_titles = [f"Local network length for: {l}" for l in labels]
-filepaths = [f"../results/network_density/administrative/{l}.png" for l in labels]
-
-
-for i, p in enumerate(plot_cols):
-
-    plot_func.plot_classified_poly(
-        gdf=density_h3,
-        plot_col=p,
-        scheme="quantiles",
-        cx_tile=cx_tile_2,
-        plot_na=True,
-        cmap=pdict["pos"],
-        edgecolor="none",
-        title=plot_titles[i],
-        fp=filepaths[i],
+    fig.write_image(
+        filepaths_bar[e],
+        width=1000,
+        height=750,
     )
 
+    fig = px.bar(
+        df.sort_values("density", ascending=False),
+        x="id",
+        y="density",
+        color="lts",
+        title=density_titles[e],
+        labels=plotly_labels,
+        color_discrete_map=lts_color_dict,
+    )
+    fig.show()
+
+    # **** VIOLIN PLOTS ****
+    colors = [v for v in lts_color_dict.values()]
+
+    for i, d in enumerate(density_columns):
+        fig = px.violin(
+            density_muni,
+            y=d,
+            hover_name="municipality",
+            points="all",
+            box=False,
+            labels=plotly_labels,
+            color_discrete_sequence=[colors[i]],
+            title=type_cols[e],
+        )
+        fig.show()
+
+        fig.write_image(
+            filepaths_violin[e] + d + ".jpg",
+            width=1000,
+            height=750,
+        )
+
+    for i, l in enumerate(length_columns):
+        fig = px.violin(
+            density_muni,
+            y=l,
+            hover_name="municipality",
+            points="all",
+            box=False,
+            labels=plotly_labels,
+            color_discrete_sequence=[colors[i]],
+            title=type_cols[e],
+        )
+        fig.show()
+
+        fig.write_image(
+            filepaths_violin[e] + d + ".jpg",
+            width=1000,
+            height=750,
+        )
 # %%
-# TODO: Plot distribution of network densities for each lts
-
-# TODO: Make distribution plots of each LTS combined and for each aggregation level
-
-# TODO: make dataframe with lts in one col and network length in another
-
-# transpose?
-
-sns.displot(density_muni, x="network_length", hue="lts", kind="kde", multiple="stack")
-# %%
-sns.displot(density_muni, x="lts_1_dens", kde=True)
-
-# %%
-
-sns.histplot(density_muni, x="lts_1_dens", kde=True)
-
-import seaborn as sns
-import plotly.express as px
-
-# %%
-
-# TODO Make plotly express violin plot of density for each LTS
