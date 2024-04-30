926d75d2fe852a046bb21dda30bd2a08d25c6bb8
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-04-30 17:49:18 +0200

    ...

--- scripts/summary_stats.py
+++ scripts/summary_stats.py
@@ -6,4 +6,16 @@
 # Total length of network
 # Total length of each LTS level
 
-# MIN MAX, MEAN AND MEDIAN OF EACH LTS FOR EACH AGGREGATION LEVEL
+# MIN, MAX, MEAN, AND MEDIAN OF EACH LTS FOR EACH AGGREGATION LEVEL
+
+# Make municipal dataframe with LTS length in abs value and pct of total network
+
+# What are the patterns - always the same order of the different levels in all aggregations?
+# (e.g. less 1 than 2 than 3 than 4) (of course not using stepwise length)
+
+# do places tend to have more car than bike?
+
+
+# What are the interesting questions??
+# - How does the LTS distribution change with aggregation level?
+# - how does the value ranges differ between the different aggregation levels and different levels of LTS?
