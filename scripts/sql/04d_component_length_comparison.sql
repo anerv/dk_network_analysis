2d7daf1633d36784c1171fdd93aa2a64779fcf67
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-05-08 15:59:32 +0200

    compute components per length and dens

--- scripts/sql/04d_component_length_comparison.sql
+++ scripts/sql/04d_component_length_comparison.sql
@@ -94,3 +94,253 @@ CREATE TABLE component_length_h3 AS (
         density_h3 dens
         JOIN comp_count_h3 comp ON dens.hex_id = comp.h3_id
 );
+
+ALTER TABLE
+    component_length_muni
+ADD
+    COLUMN component_per_length_all DOUBLE PRECISION,
+ADD
+    COLUMN component_per_length_1 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_length_2 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_length_3 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_length_4 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_length_car DOUBLE PRECISION;
+
+ALTER TABLE
+    component_length_socio
+ADD
+    COLUMN component_per_length_all DOUBLE PRECISION,
+ADD
+    COLUMN component_per_length_1 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_length_2 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_length_3 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_length_4 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_length_car DOUBLE PRECISION;
+
+ALTER TABLE
+    component_length_h3
+ADD
+    COLUMN component_per_length_all DOUBLE PRECISION,
+ADD
+    COLUMN component_per_length_1 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_length_2 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_length_3 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_length_4 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_length_car DOUBLE PRECISION;
+
+ALTER TABLE
+    component_length_muni
+ADD
+    COLUMN component_per_dens_all DOUBLE PRECISION,
+ADD
+    COLUMN component_per_dens_1 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_dens_2 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_dens_3 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_dens_4 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_dens_car DOUBLE PRECISION;
+
+ALTER TABLE
+    component_length_socio
+ADD
+    COLUMN component_per_dens_all DOUBLE PRECISION,
+ADD
+    COLUMN component_per_dens_1 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_dens_2 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_dens_3 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_dens_4 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_dens_car DOUBLE PRECISION;
+
+ALTER TABLE
+    component_length_h3
+ADD
+    COLUMN component_per_dens_all DOUBLE PRECISION,
+ADD
+    COLUMN component_per_dens_1 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_dens_2 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_dens_3 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_dens_4 DOUBLE PRECISION,
+ADD
+    COLUMN component_per_dens_car DOUBLE PRECISION;
+
+UPDATE
+    component_length_muni
+SET
+    component_per_length_all = comp_all_count / total_network_length,
+    component_per_length_1 = comp_1_count / lts_1_length,
+    component_per_length_2 = comp_2_count / lts_1_2_length,
+    component_per_length_3 = comp_3_count / lts_1_3_length,
+    component_per_length_4 = comp_4_count / lts_1_4_length,
+    component_per_length_car = comp_car_count / total_car_length;
+
+UPDATE
+    component_length_muni
+SET
+    component_per_dens_all = comp_all_count / total_network_dens,
+    component_per_dens_1 = comp_1_count / lts_1_dens,
+    component_per_dens_2 = comp_2_count / lts_1_2_dens,
+    component_per_dens_3 = comp_3_count / lts_1_3_dens,
+    component_per_dens_4 = comp_4_count / lts_1_4_dens,
+    component_per_dens_car = comp_car_count / total_car_dens;
+
+UPDATE
+    component_length_socio
+SET
+    component_per_length_all = comp_all_count / total_network_length,
+    component_per_length_1 = comp_1_count / lts_1_length,
+    component_per_length_2 = comp_2_count / lts_1_2_length,
+    component_per_length_3 = comp_3_count / lts_1_3_length,
+    component_per_length_4 = comp_4_count / lts_1_4_length,
+    component_per_length_car = comp_car_count / total_car_length;
+
+UPDATE
+    component_length_socio
+SET
+    component_per_dens_all = comp_all_count / total_network_dens,
+    component_per_dens_1 = comp_1_count / lts_1_dens,
+    component_per_dens_2 = comp_2_count / lts_1_2_dens,
+    component_per_dens_3 = comp_3_count / lts_1_3_dens,
+    component_per_dens_4 = comp_4_count / lts_1_4_dens,
+    component_per_dens_car = comp_car_count / total_car_dens;
+
+UPDATE
+    component_length_h3
+SET
+    component_per_length_all = comp_all_count / total_network_length
+WHERE
+    (
+        total_network_length > 0
+        AND total_network_length IS NOT NULL
+    );
+
+UPDATE
+    component_length_h3
+SET
+    component_per_length_1 = comp_1_count / lts_1_length
+WHERE
+    (
+        lts_1_length > 0
+        AND lts_1_length IS NOT NULL
+    );
+
+UPDATE
+    component_length_h3
+SET
+    component_per_length_2 = comp_2_count / lts_1_2_length
+WHERE
+    (
+        lts_1_2_length > 0
+        AND lts_1_2_length IS NOT NULL
+    );
+
+UPDATE
+    component_length_h3
+SET
+    component_per_length_3 = comp_3_count / lts_1_3_length
+WHERE
+    (
+        lts_1_3_length > 0
+        AND lts_1_3_length IS NOT NULL
+    );
+
+UPDATE
+    component_length_h3
+SET
+    component_per_length_4 = comp_4_count / lts_1_4_length
+WHERE
+    (
+        lts_1_4_length > 0
+        AND lts_1_4_length IS NOT NULL
+    );
+
+UPDATE
+    component_length_h3
+SET
+    component_per_length_car = comp_car_count / total_car_length
+WHERE
+    (
+        total_car_length > 0
+        AND total_car_length IS NOT NULL
+    );
+
+UPDATE
+    component_length_h3
+SET
+    component_per_dens_all = comp_all_count / total_network_dens
+WHERE
+    (
+        total_network_length > 0
+        AND total_network_length IS NOT NULL
+    );
+
+UPDATE
+    component_length_h3
+SET
+    component_per_dens_1 = comp_1_count / lts_1_dens
+WHERE
+    (
+        lts_1_length > 0
+        AND lts_1_length IS NOT NULL
+    );
+
+UPDATE
+    component_length_h3
+SET
+    component_per_dens_2 = comp_2_count / lts_1_2_dens
+WHERE
+    (
+        lts_1_2_length > 0
+        AND lts_1_2_length IS NOT NULL
+    );
+
+UPDATE
+    component_length_h3
+SET
+    component_per_dens_3 = comp_3_count / lts_1_3_dens
+WHERE
+    (
+        lts_1_3_length > 0
+        AND lts_1_3_length IS NOT NULL
+    );
+
+UPDATE
+    component_length_h3
+SET
+    component_per_dens_4 = comp_4_count / lts_1_4_dens
+WHERE
+    (
+        lts_1_4_length > 0
+        AND lts_1_4_length IS NOT NULL
+    );
+
+UPDATE
+    component_length_h3
+SET
+    component_per_dens_car = comp_car_count / total_car_dens
+WHERE
+    (
+        total_car_length > 0
+        AND total_car_length IS NOT NULL
+    );
