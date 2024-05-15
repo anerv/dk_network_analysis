447fc3b2ced1d5e392d304484b57c1f0da0f6f8a
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-05-15 17:10:47 +0200

    moved files

---
+++ scripts/archive/export_andreas.sql
@@ -0,0 +1,64 @@
+DROP TABLE IF EXISTS andreas_export;
+
+DROP TABLE IF EXISTS andreas_export_nodes;
+
+CREATE TABLE andreas_export AS (
+    SELECT
+        *
+    FROM
+        edges
+    WHERE
+        municipality IN (
+            'Albertslund',
+            'Allerød',
+            'Ballerup',
+            'Brøndby',
+            'Dragør',
+            'Egedal',
+            'Fredensborg',
+            'Frederiksberg',
+            'Frederikssund',
+            'Furesø',
+            'Gentofte',
+            'Gladsaxe',
+            'Glostrup',
+            'Greve',
+            'Gribskov',
+            'Halsnæs',
+            'Helsingør',
+            'Herlev',
+            'Hillerød',
+            'Hvidovre',
+            'Høje-Taastrup',
+            'Hørsholm',
+            'Ishøj',
+            'København',
+            'Køge',
+            'Lyngby-Taarbæk',
+            'Roskilde',
+            'Rudersdal',
+            'Rødovre',
+            'Solrød',
+            'Tårnby',
+            'Vallensbæk'
+        )
+);
+
+CREATE TABLE andreas_export_nodes AS (
+    SELECT
+        *
+    FROM
+        nodes
+    WHERE
+        id IN (
+            SELECT
+                source AS node
+            FROM
+                andreas_export
+            UNION
+            SELECT
+                target AS node
+            FROM
+                andreas_export
+        )
+);
