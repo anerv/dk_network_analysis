d236022617b80a2b94c535a85f730490f651cc6c
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-05-20 12:54:23 +0200

    debugging

--- scripts/sql/05c_compute_reach_difference.sql
+++ scripts/sql/05c_compute_reach_difference.sql
@@ -1,15 +1,85 @@
 -- ***** JOIN REACH TO ORIGINAL HEX GEOMETRIES *****
+ALTER TABLE
+    reach.lts_1_reach
+ADD
+    COLUMN hex_id VARCHAR;
+
+ALTER TABLE
+    reach.lts_2_reach
+ADD
+    COLUMN hex_id VARCHAR;
+
+ALTER TABLE
+    reach.lts_3_reach
+ADD
+    COLUMN hex_id VARCHAR;
+
+ALTER TABLE
+    reach.lts_4_reach
+ADD
+    COLUMN hex_id VARCHAR;
+
+ALTER TABLE
+    reach.car_reach
+ADD
+    COLUMN hex_id VARCHAR;
+
+UPDATE
+    reach.lts_1_reach
+SET
+    hex_id = h.hex_id
+FROM
+    reach.hex_lts_1 h
+WHERE
+    reach.lts_1_reach.start_node = h.node_id;
+
+UPDATE
+    reach.lts_2_reach
+SET
+    hex_id = h.hex_id
+FROM
+    reach.hex_lts_2 h
+WHERE
+    reach.lts_2_reach.start_node = h.node_id;
+
+UPDATE
+    reach.lts_3_reach
+SET
+    hex_id = h.hex_id
+FROM
+    reach.hex_lts_3 h
+WHERE
+    reach.lts_3_reach.start_node = h.node_id;
+
+UPDATE
+    reach.lts_4_reach
+SET
+    hex_id = h.hex_id
+FROM
+    reach.hex_lts_4 h
+WHERE
+    reach.lts_4_reach.start_node = h.node_id;
+
+UPDATE
+    reach.car_reach
+SET
+    hex_id = h.hex_id
+FROM
+    reach.hex_car h
+WHERE
+    reach.car_reach.start_node = h.node_id;
+
 CREATE INDEX IF NOT EXISTS h3_grid_ix ON h3_grid (hex_id);
 
-CREATE INDEX IF NOT EXISTS lts_1_reach_ix ON lts_1_reach (hex_id);
+CREATE INDEX IF NOT EXISTS lts_1_reach_ix ON reach.lts_1_reach (hex_id);
 
-CREATE INDEX IF NOT EXISTS lts_2_reach_ix ON lts_2_reach (hex_id);
+CREATE INDEX IF NOT EXISTS lts_2_reach_ix ON reach.lts_2_reach (hex_id);
 
-CREATE INDEX IF NOT EXISTS lts_3_reach_ix ON lts_3_reach (hex_id);
+CREATE INDEX IF NOT EXISTS lts_3_reach_ix ON reach.lts_3_reach (hex_id);
 
-CREATE INDEX IF NOT EXISTS lts_4_reach_ix ON lts_4_reach (hex_id);
+CREATE INDEX IF NOT EXISTS lts_4_reach_ix ON reach.lts_4_reach (hex_id);
 
-CREATE INDEX IF NOT EXISTS car_reach_ix ON car_reach (hex_id);
+CREATE INDEX IF NOT EXISTS car_reach_ix ON reach.car_reach (hex_id);
 
 CREATE TABLE reach.hex_reach AS
 SELECT
@@ -19,36 +89,14 @@ SELECT
     l2.edge_length AS l2_len,
     l3.edge_length AS l3_len,
     l4.edge_length AS l4_len,
-    ca.edge_length AS car_len,
+    ca.edge_length AS car_len
 FROM
     h3_grid h
-    LEFT JOIN lts_1_reach l1 ON h.hex_id = l1.hex_id
-    LEFT JOIN lts_2_reach l2 ON h.hex_id = l2.hex_id
-    LEFT JOIN lts_3_reach l3 ON h.hex_id = l3.hex_id
-    LEFT JOIN lts_4_reach l4 ON h.hex_id = l4.hex_id
-    LEFT JOIN car_reach ca ON h.hex_id = ca.hex_id;
-
-SELECT
-    lts_1.municipality,
-    lts_1.lts_1_length,
-    lts_2.lts_2_length,
-    lts_3.lts_3_length,
-    lts_4.lts_4_length,
-    lts_5.lts_5_length,
-    lts_6.lts_6_length,
-    lts_7.lts_7_length,
-    total_car.total_car_length,
-    adm_boundaries.geometry
-FROM
-    adm_boundaries
-    LEFT JOIN lts_1 ON adm_boundaries.navn = lts_1.municipality
-    LEFT JOIN lts_2 ON adm_boundaries.navn = lts_2.municipality
-    LEFT JOIN lts_3 ON adm_boundaries.navn = lts_3.municipality
-    LEFT JOIN lts_4 ON adm_boundaries.navn = lts_4.municipality
-    LEFT JOIN lts_5 ON adm_boundaries.navn = lts_5.municipality
-    LEFT JOIN lts_6 ON adm_boundaries.navn = lts_6.municipality
-    LEFT JOIN lts_7 ON adm_boundaries.navn = lts_7.municipality
-    LEFT JOIN total_car ON adm_boundaries.navn = total_car.municipality;
+    LEFT JOIN reach.lts_1_reach l1 ON h.hex_id = l1.hex_id
+    LEFT JOIN reach.lts_2_reach l2 ON h.hex_id = l2.hex_id
+    LEFT JOIN reach.lts_3_reach l3 ON h.hex_id = l3.hex_id
+    LEFT JOIN reach.lts_4_reach l4 ON h.hex_id = l4.hex_id
+    LEFT JOIN reach.car_reach ca ON h.hex_id = ca.hex_id;
 
 DO $$
 DECLARE
