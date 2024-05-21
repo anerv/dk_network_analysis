25a5f7221e638732272898b31383323c6a795968
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-05-21 11:24:03 +0200

    working on plots

--- scripts/09a_plot_reach.py
+++ scripts/09a_plot_reach.py
@@ -21,14 +21,87 @@ connection = dbf.connect_pg(db_name, db_user, db_password, db_port=db_port)
 hex_reach = gpd.read_postgis("SELECT * FROM reach.hex_reach", connection)
 
 # %%
-# MAPS:
-# Reach length
+####### MAPS ##############################
+###########################################
 
-# Reach diffs
+# Absolute reach length
 
-# Reach len as share of car length
+# Use norm/same color scale for all maps?
 
+all_plot_titles = []
+all_file_paths = []
+plot_columns = []
 
+plot_titles = []
+
+for i, p in enumerate(plot_columns):
+
+    plot_func.plot_classified_poly(
+        gdf=hex_reach,
+        plot_col=p,
+        scheme=scheme,
+        k=k,
+        cx_tile=cx_tile_2,
+        plot_na=True,
+        cmap=pdict["pos"],
+        edgecolor="none",
+        title=plot_titles[i],
+        fp=filepaths[i],
+    )
+
+# %%
+# Absolute reach differences
+
+# Use diverging color map
+# Use norm/same color scale for all maps?
+
+all_plot_titles = []
+all_file_paths = []
+plot_columns = []
+
+plot_titles = []
+
+for i, p in enumerate(plot_columns):
+
+    plot_func.plot_classified_poly(
+        gdf=hex_reach,
+        plot_col=p,
+        scheme=scheme,
+        k=k,
+        cx_tile=cx_tile_2,
+        plot_na=True,
+        cmap=pdict["pos"],
+        edgecolor="none",
+        title=plot_titles[i],
+        fp=filepaths[i],
+    )
+
+# %%
+# Pct differences between LTS and car reach
+
+# Use diverging color map
+# Use norm/same color scale for all maps?
+
+all_plot_titles = []
+all_file_paths = []
+plot_columns = []
+
+plot_titles = []
+
+for i, p in enumerate(plot_columns):
+
+    plot_func.plot_classified_poly(
+        gdf=hex_reach,
+        plot_col=p,
+        scheme=scheme,
+        k=k,
+        cx_tile=cx_tile_2,
+        plot_na=True,
+        cmap=pdict["pos"],
+        edgecolor="none",
+        title=plot_titles[i],
+        fp=filepaths[i],
+    )
 # %%
 # Corr plots
 
@@ -40,4 +113,34 @@ hex_reach = gpd.read_postgis("SELECT * FROM reach.hex_reach", connection)
 # %%
 # Distribution plots of reach length and diffs?
 
-# KDE etc.
+
+# %%
+# ***** Histograms *****
+
+
+# %%
+# ***** KDE PLOTS *****
+
+# TODO: Make stacked df?
+
+for label, df in stacked_dfs.items():
+
+    df.rename(columns={"lts": "Network level"}, inplace=True)
+
+    fig = sns.kdeplot(
+        data=df,
+        x="length",
+        hue="Network level",
+        # multiple="stack",
+        # fill=True,
+        log_scale=True,
+        palette=lts_color_dict.values(),
+    )
+
+    fig.set_xlabel("Length (km)")
+    fig.set_title(f"Network length KDE at the {label.lower()} level")
+    plt.savefig(filepaths_kde_length[list(stacked_dfs.keys()).index(label)])
+
+    plt.show()
+
+    plt.close()
