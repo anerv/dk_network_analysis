3a7eee1b6c215abc588a389ab500da38ab551fc5
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-05-21 20:30:17 +0200

    sum stats done

--- scripts/09b_summary_stats_reach.py
+++ scripts/09b_summary_stats_reach.py
@@ -18,3 +18,48 @@ connection = dbf.connect_pg(db_name, db_user, db_password, db_port=db_port)
 # %%
 # Print absolute and relative differences for both reach len and area
 # Min, max, average etc.
+
+# Read data
+
+hex_reach = gpd.read_postgis(
+    "SELECT * FROM reach.hex_reach", connection, geom_col="geometry"
+)
+
+for p in reach_columns:
+    hex_reach[p] = hex_reach[p] / 1000  # Convert to km
+
+for p in reach_diff_columns:
+    hex_reach[p] = hex_reach[p] / 1000  # Convert to km
+
+hex_reach.replace(0, np.nan, inplace=True)
+
+# %%
+labels = [
+    "LTS 1",
+    "LTS 1-2",
+    "LTS 1-3",
+    "LTS 1-4",
+    "car",
+]
+
+for i, r in enumerate(reach_columns):
+
+    print(
+        f"The minimum network reach for the {labels[i]} network is: {hex_reach[r].min():_.3f} km."
+    )
+    print(
+        f"The maximum network reach for the {labels[i]} network is: {hex_reach[r].max():_.3f} km."
+    )
+    print(
+        f"The average network reach for the {labels[i]} network is: {hex_reach[r].mean():_.3f} km."
+    )
+    print(
+        f"The median network reach for the {labels[i]} network is: {hex_reach[r].median():_.3f} km."
+    )
+    print(
+        f"The standard deviation in network reach network for: {labels[i]} is {hex_reach[r].std():_.2f} km."
+    )
+
+    print("\n")
+
+# %%
