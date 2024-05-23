982dbfb858687b0b1bc997aa9fa8acdf54c378a6
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-05-23 10:50:07 +0200

    moved filepaths

---
+++ settings/filepaths.py
@@ -0,0 +1,129 @@
+# Density plots
+
+filepaths_kde_length = [
+    "../results/densit_distributions/administrative/lts_kde_length.jpg",
+    "../results/density_distributions/socio/lts_kde_length.jpg",
+    "../results/density_distributions/hexgrid/lts_kde_length.jpg",
+]
+
+filepaths_kde_density = [
+    "../results/density_distributions/administrative/lts_kde_density.jpg",
+    "../results/density_distributions/socio/lts_kde_density.jpg",
+    "../results/density_distributions/hexgrid/lts_kde_density.jpg",
+]
+
+filepaths_bar_length = [
+    "../results/density_distributions/administrative/lts_stacked_bar_len.jpg",
+    "../results/density_distributions/socio/lts_stacked_bar_len.jpg",
+    "../results/density_distributions/hexgrid/lts_stacked_bar_len.jpg",
+]
+
+filepaths_bar_density = [
+    "../results/density_distributions/administrative/lts_stacked_bar_dens.jpg",
+    "../results/density_distributions/socio/lts_stacked_bar_dens.jpg",
+    "../results/density_distributions/hexgrid/lts_stacked_bar_dens.jpg",
+]
+
+filepaths_violin_density = [
+    "../results/density_distributions/administrative/violin_",
+    "../results/density_distributions/socio/violin_",
+    "../results/density_distributions/hexgrid/violin_",
+]
+
+all_filepaths_map_density = [
+    "../results/density_maps/administrative/",
+    "../results/density_maps/socio/",
+    "../results/density_maps/hexgrid/",
+]
+
+# Reach plots
+
+filepaths_reach = [
+    "../results/reach_maps/lts1_reach_len",
+    "../results/reach_maps/lts2_reach_len",
+    "../results/reach_maps/lts3_reach_len",
+    "../results/reach_maps/lts4_reach_len",
+    "../results/reach_maps/car_reach_len",
+]
+
+filepaths_reach_diff = [
+    "../results/reach_maps/lts1_reach_len_diff",
+    "../results/reach_maps/lts2_reach_len_diff",
+    "../results/reach_maps/lts3_reach_len_diff",
+    "../results/reach_maps/lts4_reach_len_diff",
+]
+
+filepaths_reach_diff_pct = [
+    "../results/reach_maps/lts1_reach_pct_diff",
+    "../results/reach_maps/lts2_reach_pct_diff",
+    "../results/reach_maps/lts3_reach_pct_diff",
+    "../results/reach_maps/lts4_reach_pct_diff",
+]
+
+fp_network_reach_kde = "../results/reach_distributions/kde_network_reach.png"
+
+fp_network_reach_diff_kde = "../results/reach_distributions/kde_network_reach_diff.png"
+
+filepaths_violin_reach = [
+    "../results/reach_distributions/violin_lts1_reach_len.jpg",
+    "../results/reach_distributions/violin_lts2_reach_len.jpg",
+    "../results/reach_distributions/violin_lts3_reach_len.jpg",
+    "../results/reach_distributions/violin_lts4_reach_len.jpg",
+    "../results/reach_distributions/violin_car_reach_len.jpg",
+]
+
+filepaths_violin_reach_diff = [
+    "../results/reach_distributions/violin_lts1_reach_diff_len.jpg",
+    "../results/reach_distributions/violin_lts2_reach_diff_len.jpg",
+    "../results/reach_distributions/violin_lts3_reach_diff_len.jpg",
+    "../results/reach_distributions/violin_lts4_reach_diff_len.jpg",
+]
+
+filepaths_violin_reach_diff_pct = [
+    "../results/reach_distributions/violin_lts1_reach_diff_pct.jpg",
+    "../results/reach_distributions/violin_lts2_reach_diff_pct.jpg",
+    "../results/reach_distributions/violin_lts3_reach_diff_pct.jpg",
+    "../results/reach_distributions/violin_lts4_reach_diff_pct.jpg",
+]
+
+fp_reach_density_corr = "../results/reach_density_correlation/corr_reach_density_"
+
+
+# Fragmentation plots
+
+fp_zipf_lts = "../results/component_size_distribution/zipf_"
+fp_zipf_combined = "../results/component_size_distribution/combined_zipf.png"
+fp_zipf_muni = "../results/component_size_distribution/administrative/combined_zipf_"
+
+filepaths_local_component_count = [
+    "../results/component_count_maps/administrative/",
+    "../results/component_count_maps/socio/",
+    "../results/component_count_maps/hexgrid/",
+]
+
+filepaths_component_density_correlation = [
+    "../results/component_density_correlation/administrative/component_count_infra_density_",
+    "../results/component_density_correlation/socio/component_count_infra_density_",
+    "../results/component_density_correlation/hexgrid/component_count_infra_density_",
+]
+
+filepaths_components_rug = [
+    "../results/component_size_distribution/administrative/component_distribution_muni.jpeg",
+    "../results/component_size_distribution/socio/component_distribution_socio.jpeg",
+    "../results/component_size_distribution/hexgrid/component_distribution_h3.jpeg",
+]
+
+filepaths_components_scatter = [
+    "../results/component_density_correlation/administrative/component_count_infra_density_all_areas.jpeg",
+    "../results/component_density_correlation/socio/component_count_infra_density_all_areas.jpeg",
+    "../results/component_density_correlation/hexgrid/component_count_infra_density_all_areas.jpeg",
+]
+
+filepaths_components_scatter_lts = [
+    "../results/component_density_correlation/hexgrid/component_count_infra_density_all_areas_1.jpeg",
+    "../results/component_density_correlation/hexgrid/component_count_infra_density_all_areas_2.jpeg",
+    "../results/component_density_correlation/hexgrid/component_count_infra_density_all_areas_3.jpeg",
+    "../results/component_density_correlation/hexgrid/component_count_infra_density_all_areas_4.jpeg",
+    "../results/component_density_correlation/hexgrid/component_count_infra_density_all_areas_car.jpeg",
+    "../results/component_density_correlation/hexgrid/component_count_infra_density_all_areas_all.jpeg",
+]
