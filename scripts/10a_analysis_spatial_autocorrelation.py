8a5eb1ebd99acc0d1dfe71c017ab949131ebc4d4
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-05-24 15:12:37 +0200

    first draft?

--- scripts/10a_analysis_spatial_autocorrelation.py
+++ scripts/10a_analysis_spatial_autocorrelation.py
@@ -6,8 +6,7 @@ import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
-# import plotly_express as px
+import json
 
 exec(open("../settings/yaml_variables.py").read())
 exec(open("../settings/plotting.py").read())
@@ -17,28 +16,9 @@ engine = dbf.connect_alc(db_name, db_user, db_password, db_port=db_port)
 
 connection = dbf.connect_pg(db_name, db_user, db_password, db_port=db_port)
 
+
 # %%
 
-# ANALYSE CLUSTERING OFF:
-
-# Global Moran's I
-
-### DENSITY ###
-
-## AT EACH AGGREGATION LEVEL:
-# Density of each LTS type #OK
-# Density of LTS stepwise # OK
-# Share of each LTS type
-# Share of LTS stepwise
-
-
-# TODO: Store results: Moran's I for each type of spatial weight and result
-# TODO: For LISA, join the value to the spatial unit, p-value, spatial weight type, and cluster type
-# TODO: Sensitivity analysis: Test with different types of spatial weights to see if results change
-
-
-# VIZ + EXPORT VALUES
-# %%
 ### READ DATA ###
 density_muni = gpd.GeoDataFrame.from_postgis(
     "SELECT * FROM density.density_municipality;",
@@ -46,7 +26,8 @@ density_muni = gpd.GeoDataFrame.from_postgis(
     crs=crs,
     geom_col="geometry",
 )
-# density_muni.replace(0, np.nan, inplace=True)
+
+density_muni.replace(np.nan, 0, inplace=True)
 
 density_socio = gpd.GeoDataFrame.from_postgis(
     "SELECT * FROM density.density_socio;",
@@ -55,7 +36,7 @@ density_socio = gpd.GeoDataFrame.from_postgis(
     geom_col="geometry",
 )
 
-# density_socio.replace(0, np.nan, inplace=True)
+density_socio.replace(np.nan, 0, inplace=True)
 
 density_hex = gpd.GeoDataFrame.from_postgis(
     "SELECT * FROM density.density_hex;",
@@ -64,7 +45,7 @@ density_hex = gpd.GeoDataFrame.from_postgis(
     geom_col="geometry",
 )
 
-# density_hex.replace(0, np.nan, inplace=True)
+density_hex.replace(np.nan, 0, inplace=True)
 
 gdfs = [density_muni, density_socio, density_hex]
 
@@ -81,10 +62,11 @@ for gdf in gdfs:
 k_muni = 3
 k_socio = 8
 k_hex = 18
-p = 0.05
+p = 0.05  # p-value for LISA
 
+# Define spatial weights
 w_muni = analysis_func.compute_spatial_weights(
-    density_muni, "municipality", w_type="knn", dist=k_muni  # "knn", k=k_muni
+    density_muni, "municipality", w_type="knn", dist=k_muni
 )
 
 w_socio = analysis_func.compute_spatial_weights(
@@ -92,123 +74,66 @@ w_socio = analysis_func.compute_spatial_weights(
 )
 
 w_grid = analysis_func.compute_spatial_weights(
-    density_hex, "id", w_type="knn", k=k_socio
+    density_hex, "hex_id", w_type="knn", k=k_hex
 )
 
+spatial_weights = [w_muni, w_socio, w_grid]
+k_values = [k_muni, k_socio, k_hex]
 
 # %%
-# TODO: do for all aggregation levels
+all_density_columns = [
+    density_columns,
+    density_steps_columns[1:-2],
+    length_relative_columns,
+    length_relative_steps_columns[1:-1],
+]
+
+aggregation_level = ["administrative", "socio", "hexgrid"]
+id_cols = ["municipality", "id", "hex_id"]
 
 for i, gdf in enumerate(gdfs):
 
+    print(f"At aggregation level: {aggregation_level[i]}:")
+
     global_morans_results = {}
 
-    columns = density_columns
+    for columns in all_density_columns:
 
-    # variable_names = [
-    #     "LTS 1 density",
-    #     "LTS 2 density",
-    #     "LTS 3 density",
-    #     "LTS 4 density",
-    #     "car density",
-    #     "total network density",
-    # ]
+        filepaths = [
+            f"../results/spatial_autocorrelation/density/{aggregation_level[i]}/{c}.png".replace(
+                " ", "_"
+            )
+            for c in columns
+        ]
 
-    filepaths = [
-        f"../results/spatial_autocorrelation/density/{v}.png".replace(" ", "_")
-        for v in variable_names
-    ]
+        morans_results = analysis_func.compute_spatial_autocorrelation(
+            columns, columns, gdf, spatial_weights[i], filepaths, show_plot=False
+        )
 
-    morans_results = analysis_func.compute_spatial_autocorrelation(
-        columns, columns, density_muni, w_muni, filepaths
+        lisa_results = analysis_func.compute_lisa(
+            columns,
+            columns,
+            gdf,
+            spatial_weights[i],
+            filepaths,
+            show_plot=False,
+        )
+
+        for key, value in morans_results.items():
+            global_morans_results[key] = value.I
+
+    with open(
+        f"../results/spatial_autocorrelation/density/{aggregation_level[i]}/global_moransi_k{k_values[i]}.json",
+        "w",
+    ) as outfile:
+        json.dump(global_morans_results, outfile)
+
+    q_columns = [c for c in gdf.columns if c.endswith("_q")]
+    q_columns.extend(["geometry", id_cols[i]])
+
+    gdf[q_columns].to_parquet(
+        f"../results/spatial_autocorrelation/density/{aggregation_level[i]}/lisas.parquet"
     )
-
-    lisa_results = analysis_func.compute_lisa(
-        columns, columns, density_muni, w_muni, filepaths
-    )
-
-    for key, value in morans_results.items():
-        global_morans_results[key] = value.I
-
-# %%
-columns = density_steps_columns[1:-2]
-
-variable_names = [
-    # "LTS 1 density",
-    "LTS 1-2 density",
-    "LTS 1-3 density",
-    "LTS 1-4 density",
-    # "car density",
-    # "total network density",
-]
-
-filepaths = [
-    f"../results/spatial_autocorrelation/density/{v}.png".replace(" ", "_")
-    for v in variable_names
-]
-
-# TODO: Store results, convert to table
-
-morans_density_steps_muni = analysis_func.compute_spatial_autocorrelation(
-    columns, variable_names, density_muni, w_muni, filepaths
-)
-
-# TODO: Store results, convert to table
-
-columns = length_relative_columns
-
-variable_names = [
-    "LTS 1%",
-    "LTS 2%",
-    "LTS 3%",
-    "LTS 4%",
-    "car%",
-]
-
-filepaths = [
-    f"../results/spatial_autocorrelation/density/{v}.png".replace(" ", "_")
-    for v in variable_names
-]
-
-morans_density_muni = analysis_func.compute_spatial_autocorrelation(
-    columns, variable_names, density_muni, w_muni, filepaths
-)
-
-
-columns = length_relative_steps_columns[1:-1]
-variable_names = ["LTS 1-2%", "LTS 1-3%", "LTS 1-4%"]
-
-filepaths = [
-    f"../results/spatial_autocorrelation/density/{v}.png".replace(" ", "_")
-    for v in variable_names
-]
-
-morans_density_muni = analysis_func.compute_spatial_autocorrelation(
-    columns, variable_names, density_muni, w_muni, filepaths
-)
-
-# TODO: Store results, convert to table
-
-# TODO: INCORPORATE LISA
-# %%
-
-filepaths = [
-    f"../results/spatial_autocorrelation/density/{v}.png".replace(" ", "_")
-    for v in variable_names
-]
-
-variable_names = [
-    "LTS 1 density",
-    "LTS 2 density",
-    "LTS 3 density",
-    "LTS 4 density",
-    "car density",
-    "total network density",
-]
-
-lisas_density = analysis_func.compute_lisa(
-    density_columns, variable_names, density_muni, w_muni, filepaths, p=p
-)
 
 # %%
 ### FRAGMENTATION ###
@@ -218,8 +143,188 @@ lisas_density = analysis_func.compute_lisa(
 # Comp per km
 # Comp per sqkm
 
+# READ DATA
+
+muni_components = gpd.GeoDataFrame.from_postgis(
+    "SELECT * FROM fragmentation.component_length_muni;",
+    engine,
+    crs=crs,
+    geom_col="geometry",
+)
+
+muni_components.replace(np.nan, 0, inplace=True)
+
+socio_components = gpd.GeoDataFrame.from_postgis(
+    "SELECT * FROM fragmentation.component_length_socio;",
+    engine,
+    crs=crs,
+    geom_col="geometry",
+)
+
+socio_components.replace(np.nan, 0, inplace=True)
+
+hex_components = gpd.GeoDataFrame.from_postgis(
+    "SELECT * FROM fragmentation.component_length_hex;",
+    engine,
+    crs=crs,
+    geom_col="geometry",
+)
+
+hex_components.replace(np.nan, 0, inplace=True)
+
+gdfs = [muni_components, socio_components, hex_components]
+
+# Define K values for spatial weights
+k_muni = 3
+k_socio = 8
+k_hex = 18
+p = 0.05  # p-value for LISA
+
+# Define spatial weights
+w_muni = analysis_func.compute_spatial_weights(
+    muni_components, "municipality", w_type="knn", dist=k_muni
+)
+
+w_socio = analysis_func.compute_spatial_weights(
+    socio_components, "id", w_type="knn", k=k_socio
+)
+
+w_grid = analysis_func.compute_spatial_weights(
+    hex_components, "hex_id", w_type="knn", k=k_hex
+)
+
+spatial_weights = [w_muni, w_socio, w_grid]
+k_values = [k_muni, k_socio, k_hex]
+
+all_fragmentation_columns = [
+    component_count_columns,
+    component_per_km_cols,
+    component_per_km_sqkm_cols,
+]
+
+aggregation_level = ["administrative", "socio", "hexgrid"]
+id_cols = ["municipality", "id", "hex_id"]
+
+for i, gdf in enumerate(gdfs):
+
+    print(f"At aggregation level: {aggregation_level[i]}:")
+
+    global_morans_results = {}
+
+    for columns in all_fragmentation_columns:
+
+        filepaths = [
+            f"../results/spatial_autocorrelation/fragmentation/{aggregation_level[i]}/{c}.png".replace(
+                " ", "_"
+            )
+            for c in columns
+        ]
+
+        morans_results = analysis_func.compute_spatial_autocorrelation(
+            columns, columns, gdf, spatial_weights[i], filepaths, show_plot=False
+        )
+
+        lisa_results = analysis_func.compute_lisa(
+            columns,
+            columns,
+            gdf,
+            spatial_weights[i],
+            filepaths,
+            show_plot=False,
+        )
+
+        for key, value in morans_results.items():
+            global_morans_results[key] = value.I
+
+    with open(
+        f"../results/spatial_autocorrelation/fragmentation/{aggregation_level[i]}/global_moransi_k{k_values[i]}.json",
+        "w",
+    ) as outfile:
+        json.dump(global_morans_results, outfile)
+
+    q_columns = [c for c in gdf.columns if c.endswith("_q")]
+    q_columns.extend(["geometry", id_cols[i]])
+
+    gdf[q_columns].to_parquet(
+        f"../results/spatial_autocorrelation/fragmentation/{aggregation_level[i]}/lisas.parquet"
+    )
+
+
+# %%
 ### REACH #####
 
-## Only at hex level
-# Reach of each LTS step
-# Diff in reach
+hex_reach = gpd.read_postgis(
+    "SELECT * FROM reach.hex_reach", engine, geom_col="geometry"
+)
+
+for p in reach_columns:
+    hex_reach[p] = hex_reach[p] / 1000  # Convert to km
+
+for p in reach_diff_columns:
+    hex_reach[p] = hex_reach[p] / 1000  # Convert to km
+
+hex_reach.replace(np.nan, 0, inplace=True)
+
+gdfs = [hex_reach]
+
+# Define K values for spatial weights
+k_hex = 18
+p = 0.05  # p-value for LISA
+
+# Define spatial weights
+w_grid = analysis_func.compute_spatial_weights(
+    density_hex, "hex_id", w_type="knn", k=k_hex
+)
+
+spatial_weights = [w_muni, w_socio, w_grid]
+k_values = [k_muni, k_socio, k_hex]
+
+all_reach_columns = [reach_columns, reach_diff_columns, reach_diff_pct_columns]
+
+aggregation_level = ["hexgrid"]
+id_cols = ["hex_id"]
+
+for i, gdf in enumerate(gdfs):
+
+    print(f"At aggregation level: {aggregation_level[i]}:")
+
+    global_morans_results = {}
+
+    for columns in all_fragmentation_columns:
+
+        filepaths = [
+            f"../results/spatial_autocorrelation/reach/{aggregation_level[i]}/{c}.png".replace(
+                " ", "_"
+            )
+            for c in columns
+        ]
+
+        morans_results = analysis_func.compute_spatial_autocorrelation(
+            columns, columns, gdf, spatial_weights[i], filepaths, show_plot=False
+        )
+
+        lisa_results = analysis_func.compute_lisa(
+            columns,
+            columns,
+            gdf,
+            spatial_weights[i],
+            filepaths,
+            show_plot=False,
+        )
+
+        for key, value in morans_results.items():
+            global_morans_results[key] = value.I
+
+    with open(
+        f"../results/spatial_autocorrelation/reach/{aggregation_level[i]}/global_moransi_k{k_values[i]}.json",
+        "w",
+    ) as outfile:
+        json.dump(global_morans_results, outfile)
+
+    q_columns = [c for c in gdf.columns if c.endswith("_q")]
+    q_columns.extend(["geometry", id_cols[i]])
+
+    gdf[q_columns].to_parquet(
+        f"../results/spatial_autocorrelation/reach/{aggregation_level[i]}/lisas.parquet"
+    )
+# %%
