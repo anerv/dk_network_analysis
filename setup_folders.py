359b426f7cc8a93b2295a2979ef5cf8e4411539f
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-05-25 09:40:46 +0200

    added new folders

--- setup_folders.py
+++ setup_folders.py
@@ -44,3 +44,43 @@ for f in results_subfolders[:5]:
             os.mkdir("results/" + f + "/" + a)
 
             print("Successfully created folder: results/" + f + "/" + a)
+
+analysis_results_subfolders = ["spatial_autocorrelation", "clustering"]
+
+result_type_subfolders = ["density", "fragmentation", "reach"]
+
+for f in analysis_results_subfolders:
+    if not os.path.exists("results/" + f):
+        os.mkdir("results/" + f)
+
+        print("Successfully created folder: results/" + f)
+
+        for r in result_type_subfolders:
+            if not os.path.exists("results/" + f + "/" + r):
+                os.mkdir("results/" + f + "/" + r)
+
+                print("Successfully created folder: results/" + f + "/" + r)
+
+                if r == "fragmentation" or r == "density ":  # no area folders in reach
+                    for a in area_folders:
+                        if not os.path.exists("results/" + f + "/" + r + "/" + a):
+                            os.mkdir("results/" + f + "/" + r + "/" + a)
+                            print(
+                                "Successfully created folder: results/"
+                                + f
+                                + "/"
+                                + r
+                                + "/"
+                                + a
+                            )
+                elif r == "reach":
+                    if not os.path.exists("results/" + f + "/" + r + "/" + "hexgrid"):
+                        os.mkdir("results/" + f + "/" + r + "/" + "hexgrid")
+                        print(
+                            "Successfully created folder: results/"
+                            + f
+                            + "/"
+                            + r
+                            + "/"
+                            + "hexgrid"
+                        )
