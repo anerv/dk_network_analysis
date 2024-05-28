6c739586f058a07e52b65232ccbc554ea5acb251
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-05-28 15:18:28 +0200

    added new cols

--- scripts/sql/10a_preprocess_socio.sql
+++ scripts/sql/10a_preprocess_socio.sql
@@ -19,7 +19,11 @@ ADD
 ADD
     COLUMN IF NOT EXISTS households_income_400_500k_share DECIMAL,
 ADD
-    COLUMN IF NOT EXISTS households_income_500_750k_share DECIMAL;
+    COLUMN IF NOT EXISTS households_income_500_750k_share DECIMAL,
+ADD
+    COLUMN IF NOT EXISTS households_with_car INT,
+ADD
+    COLUMN IF NOT EXISTS households_with_car_share DECIMAL;
 
 UPDATE
     socio
@@ -33,4 +37,6 @@ SET
     households_income_200_300k_share = households_income_200k_300k / households,
     households_income_300_400k_share = households_income_300k_400k / households,
     households_income_400_500k_share = households_income_400k_500k / households,
-    households_income_500_750k_share = households_income_500k_750k / households;
+    households_income_500_750k_share = households_income_500k_750k / households,
+    households_with_car = households_with_1_car + households_with_2_cars,
+    households_with_car_share = (households_with_1_car + households_with_2_cars) / households;
