fea9521b52cc13ceeb1c9ce4f6e6143a9cdfe6c3
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-05-28 11:13:16 +0200

    cleanup

--- scripts/10a_analysis_spatial_autocorrelation.py
+++ scripts/10a_analysis_spatial_autocorrelation.py
@@ -59,40 +59,24 @@ for gdf in gdfs:
         gdf[p] = gdf[p] * 100
 
 # %%
-
-# Define spatial weights
-w_muni_queen = analysis_func.compute_spatial_weights(
-    density_muni, "municipality", w_type="queen"
-)
-
-w_socio_queen = analysis_func.compute_spatial_weights(
-    density_socio, "id", w_type="queen"
-)
-
-w_grid_queen = analysis_func.compute_spatial_weights(
-    density_hex, "hex_id", w_type="queen"
-)
-
-w_muni_knn = analysis_func.compute_spatial_weights(
-    density_muni, "municipality", w_type="knn", k=k_muni
-)
-
-w_socio_knn = analysis_func.compute_spatial_weights(
-    density_socio, "id", w_type="knn", k=k_socio
-)
-
-w_grid_knn = analysis_func.compute_spatial_weights(
-    density_hex, "hex_id", w_type="knn", k=k_hex
-)
-
-w_muni = weights.set_operations.w_union(w_muni_queen, w_muni_knn)
-w_socio = weights.set_operations.w_union(w_socio_queen, w_socio_knn)
-w_grid = weights.set_operations.w_union(w_grid_queen, w_grid_knn)
-
-
-spatial_weights = [w_muni, w_socio, w_grid]
+id_cols = ["municipality", "id", "hex_id"]
 k_values = [k_muni, k_socio, k_hex]
-spatial_weights_values = [f"queen_{k_muni}", f"queen_{k_socio}", f"queen_{k_hex}"]
+spatial_weights_values = [f"queen_{k}" for k in k_values]
+
+spatial_weights = []
+
+for i, gdf in enumerate(gdfs):
+
+    w_queen = analysis_func.compute_spatial_weights(gdf, id_cols[i], w_type="queen")
+
+    w_knn = analysis_func.compute_spatial_weights(
+        gdf, id_cols[i], w_type="knn", k=k_values[i]
+    )
+    w = weights.set_operations.w_union(w_queen, w_knn)
+
+    assert len(w.islands) == 0
+
+    spatial_weights.append(w)
 
 # %%
 all_density_columns = [
@@ -181,42 +165,26 @@ hex_components = gpd.GeoDataFrame.from_postgis(
 
 hex_components.replace(np.nan, 0, inplace=True)
 
+gdfs = [muni_components, socio_components, hex_components]
+
 # %%
 # Define spatial weights
-w_muni_queen = analysis_func.compute_spatial_weights(
-    muni_components, "municipality", w_type="queen"
-)
+spatial_weights = []
 
-w_socio_queen = analysis_func.compute_spatial_weights(
-    socio_components, "id", w_type="queen"
-)
+for i, gdf in enumerate(gdfs):
 
-w_grid_queen = analysis_func.compute_spatial_weights(
-    hex_components, "hex_id", w_type="queen"
-)
+    w_queen = analysis_func.compute_spatial_weights(gdf, id_cols[i], w_type="queen")
 
-w_muni_knn = analysis_func.compute_spatial_weights(
-    muni_components, "municipality", w_type="knn", k=k_muni
-)
+    w_knn = analysis_func.compute_spatial_weights(
+        gdf, id_cols[i], w_type="knn", k=k_values[i]
+    )
+    w = weights.set_operations.w_union(w_queen, w_knn)
 
-w_socio_knn = analysis_func.compute_spatial_weights(
-    socio_components, "id", w_type="knn", k=k_socio
-)
+    assert len(w.islands) == 0
 
-w_grid_knn = analysis_func.compute_spatial_weights(
-    hex_components, "hex_id", w_type="knn", k=k_hex
-)
-
-w_muni = weights.set_operations.w_union(w_muni_queen, w_muni_knn)
-w_socio = weights.set_operations.w_union(w_socio_queen, w_socio_knn)
-w_grid = weights.set_operations.w_union(w_grid_queen, w_grid_knn)
+    spatial_weights.append(w)
 
 # %%
-spatial_weights = [w_muni, w_socio, w_grid]
-k_values = [k_muni, k_socio, k_hex]
-spatial_weights_values = [f"queen_{k_muni}", f"queen_{k_socio}", f"queen_{k_hex}"]
-
-gdfs = [muni_components, socio_components, hex_components]
 
 all_fragmentation_columns = [
     component_count_columns,
@@ -289,21 +257,29 @@ hex_reach.replace(np.nan, 0, inplace=True)
 
 gdfs = [hex_reach]
 
-w_grid_queen = analysis_func.compute_spatial_weights(
-    hex_components, "hex_id", w_type="queen"
-)
-w_grid_knn = analysis_func.compute_spatial_weights(
-    hex_components, "hex_id", w_type="knn", k=k_hex
-)
-w_grid = weights.set_operations.w_union(w_grid_queen, w_grid_knn)
+# Define spatial weights
+id_cols = ["hex_id"]
+k_values = [k_hex]
+spatial_weights_values = [f"queen_{k}" for k in k_values]
 
-spatial_weights = [w_grid]
-spatial_weights_values = [k_hex]
+spatial_weights = []
+
+for i, gdf in enumerate(gdfs):
+
+    w_queen = analysis_func.compute_spatial_weights(gdf, id_cols[i], w_type="queen")
+
+    w_knn = analysis_func.compute_spatial_weights(
+        gdf, id_cols[i], w_type="knn", k=k_values[i]
+    )
+    w = weights.set_operations.w_union(w_queen, w_knn)
+
+    assert len(w.islands) == 0
+
+    spatial_weights.append(w)
 
 all_reach_columns = [reach_columns, reach_diff_columns, reach_diff_pct_columns]
 
 aggregation_level = ["hexgrid"]
-id_cols = ["hex_id"]
 
 for i, gdf in enumerate(gdfs):
 
@@ -351,10 +327,70 @@ for i, gdf in enumerate(gdfs):
 # %%
 ## Confirm spatial clustering of population density and socio-economic variables
 
-# TODO: convert households columns to share
-
+# Convert household counts to shares
+q = "sql/10a_preprocess_socio.sql"
+result = dbf.run_query_pg(q, connection)
+if result == "error":
+    print("Please fix error before rerunning and reconnect to the database")
 
 socio_gdf = gpd.read_postgis("SELECT * FROM socio;", engine, geom_col="geometry")
 
+w_queen = analysis_func.compute_spatial_weights(socio_gdf, "id", w_type="queen")
 
-columns = ["population_density", "car_ownership", "income"]
+w_knn = analysis_func.compute_spatial_weights(socio_gdf, "id", w_type="knn", k=k_socio)
+w = weights.set_operations.w_union(w_queen, w_knn)
+
+assert len(w.islands) == 0
+
+columns = [
+    "population_density",
+    "households_1car_share",
+    "households_2car_share",
+    "hourseholds_nocar_share",
+    "households_income_under_100k_share",
+    "households_income_100_150k_share",
+    "households_income_150_200k_share",
+    "households_income_200_300k_share",
+    "households_income_200_300k_share",
+    "households_income_300_400k_share",
+    "households_income_400_500k_share",
+    "households_income_500_750k_share",
+]
+
+global_morans_results = {}
+
+for columns in all_fragmentation_columns:
+
+    filepaths = [
+        f"../results/spatial_autocorrelation/socio/{aggregation_level[i]}/{c}.png".replace(
+            " ", "_"
+        )
+        for c in columns
+    ]
+
+    morans_results = analysis_func.compute_spatial_autocorrelation(
+        columns, columns, gdf, spatial_weights[i], filepaths, show_plot=False
+    )
+
+    lisa_results = analysis_func.compute_lisa(
+        columns,
+        columns,
+        gdf,
+        spatial_weights[i],
+        filepaths,
+        show_plot=False,
+    )
+
+    for key, value in morans_results.items():
+        global_morans_results[key] = value.I
+
+with open(
+    f"../results/spatial_autocorrelation/socio_pop/global_moransi_k{spatial_weights_values[i]}.json",
+    "w",
+) as outfile:
+    json.dump(global_morans_results, outfile)
+
+q_columns = [c for c in gdf.columns if c.endswith("_q")]
+q_columns.extend(["geometry", id_cols[i]])
+
+gdf[q_columns].to_parquet(f"../results/spatial_autocorrelation/socio_pop/lisas.parquet")
