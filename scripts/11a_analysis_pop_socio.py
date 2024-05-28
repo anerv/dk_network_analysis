5251ab17983a75b0845496c013ecf621eac2c92e
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-05-28 15:18:50 +0200

    started corr analysis

--- scripts/11a_analysis_pop_socio.py
+++ scripts/11a_analysis_pop_socio.py
@@ -1,3 +1,5 @@
+# %%
+
 from src import db_functions as dbf
 from src import plotting_functions as plot_func
 import geopandas as gpd
@@ -16,13 +18,102 @@ engine = dbf.connect_alc(db_name, db_user, db_password, db_port=db_port)
 connection = dbf.connect_pg(db_name, db_user, db_password, db_port=db_port)
 # %%
 
-# LOOK INTO CORR BETWEEN POP DENS, URB CLASS (??), AND SOCIO FACTORS BETWEEN SELECT METRICS
+# Load socio density data
+socio = gpd.read_postgis("SELECT * FROM socio", engine, geom_col="geometry")
+socio_density = gpd.read_postgis(
+    "SELECT * FROM density.density_socio", engine, geom_col="geometry"
+)
+
+socio.dropna(subset=["population_density"], inplace=True)
+socio.drop(columns=["geometry", "area_name"], inplace=True)
+
+socio_density = socio_density.merge(socio, on="id", how="inner")
+
+assert socio_density.shape[0] == socio.shape[0]
 # %%
 
-# FIRST: LOOK INTO SPATIAL PATTERNS IN POP AND SOCIO FACTORS
+###### NETWORK DENSITY AND POP DENSITY ######
 
+all_density_columns = [
+    # length_columns,
+    # length_steps_columns,
+    density_columns,
+    density_steps_columns,
+    length_relative_columns,
+    length_relative_steps_columns,
+]
+
+for columns in all_density_columns:
+
+    fig, axs = plt.subplots(2, 3, figsize=(15, 10))
+
+    axs = axs.flatten()
+
+    if len(axs) > len(columns):
+        axs[-1].set_visible(False)  # to remove last plot
+
+    for ax, col_name in zip(axs, columns):
+
+        ax.scatter(socio_density["population_density"], socio_density[col_name])
+        ax.set_xscale("log")
+        ax.set_yscale("log")
+        ax.set_xlabel("population_density")
+        ax.set_ylabel(col_name)
+
+    fig.suptitle(f"Correlation between population and network density", fontsize=20)
+
+    plt.tight_layout()
 
 # %%
+###### NETWORK DENSITY AND INCOME DENSITY ######
+
+for columns in all_density_columns:
+
+    fig, axs = plt.subplots(2, 3, figsize=(15, 10))
+
+    axs = axs.flatten()
+
+    if len(axs) > len(columns):
+        axs[-1].set_visible(False)  # to remove last plot
+
+    for ax, col_name in zip(axs, columns):
+
+        ax.scatter(socio_density["population_density"], socio_density[col_name])
+        ax.set_xscale("log")
+        ax.set_yscale("log")
+        ax.set_xlabel("population_density")
+        ax.set_ylabel(col_name)
+
+    fig.suptitle(f"Correlation between population and network density", fontsize=20)
+
+    plt.tight_layout()
+
+# %%
+###### NETWORK DENSITY AND CAR OWNERSHIP ######
+for columns in all_density_columns:
+
+    fig, axs = plt.subplots(2, 3, figsize=(15, 10))
+
+    axs = axs.flatten()
+
+    if len(axs) > len(columns):
+        axs[-1].set_visible(False)  # to remove last plot
+
+    for ax, col_name in zip(axs, columns):
+
+        ax.scatter(socio_density["households_with_car_share"], socio_density[col_name])
+        # ax.set_xscale("log")
+        ax.set_yscale("log")
+        ax.set_xlabel("share of households with car")
+        ax.set_ylabel(col_name)
+
+    fig.suptitle(f"Correlation between population and car ownership", fontsize=20)
+
+    plt.tight_layout()
+
+# %%
+
+
 ## CORR BETWEEN DENSITY AND POP
 
 # Make small multiple plot with corr between pop density and all different density metrics (length ind an step, density, relative length etc.)
