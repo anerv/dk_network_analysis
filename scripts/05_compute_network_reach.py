c0306d3dcb5b8447e691a15b0f3a45a5a39180c1
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-05-30 11:14:16 +0200

    computing for all dist

--- scripts/05_compute_network_reach.py
+++ scripts/05_compute_network_reach.py
@@ -32,217 +32,454 @@ for i, q in enumerate(queries):
 
 
 # %%
-# REACH PARAMETERS
-table_names = [
-    "reach.lts_1_reach",
-    "reach.lts_2_reach",
-    "reach.lts_3_reach",
-    "reach.lts_4_reach",
-    "reach.car_reach",
-]
+distances = [5, 10, 15]  # max distance in km
 
-edge_tables = ["lts_1_edges", "lts_2_edges", "lts_3_edges", "lts_4_edges", "car_edges"]
+for dist in distances:
 
-hex_tables = [
-    "reach.hex_lts_1",
-    "reach.hex_lts_2",
-    "reach.hex_lts_3",
-    "reach.hex_lts_4",
-    "reach.hex_car",
-]
+    print(f"Computing reach for distance: {dist} km")
 
-dist = 5  # max distance in km
-buffer_dist = 10  # buffer distance in meters
+    # REACH PARAMETERS
+    table_names = [
+        f"reach.lts_1_reach_{dist}",
+        f"reach.lts_2_reach_{dist}",
+        f"reach.lts_3_reach_{dist}",
+        f"reach.lts_4_reach_{dist}",
+        f"reach.car_reach_{dist}",
+    ]
 
+    edge_tables = [
+        "lts_1_edges",
+        "lts_2_edges",
+        "lts_3_edges",
+        "lts_4_edges",
+        "car_edges",
+    ]
 
-# %%
+    hex_tables = [
+        "reach.hex_lts_1",
+        "reach.hex_lts_2",
+        "reach.hex_lts_3",
+        "reach.hex_lts_4",
+        "reach.hex_car",
+    ]
 
-# **** COMPUT REACHABLE NODES ****
+    network_types = ["lts1", "lts2", "lts3", "lts4", "car"]
 
-# CREATE TABLES
-for t in table_names:
+    # **** COMPUT REACHABLE NODES ****
 
-    q = f"DROP TABLE IF EXISTS {t};"
+    # CREATE TABLES
+    for t in table_names:
 
-    result = dbf.run_query_pg(q, connection)
-    if result == "error":
-        print("Please fix error before rerunning and reconnect to the database")
-        break
+        q = f"DROP TABLE IF EXISTS {t};"
 
-    q = f"""CREATE TABLE {t} (
-        start_node INTEGER DEFAULT NULL,
-        reachable_nodes VARCHAR DEFAULT NULL
-    );"""
+        result = dbf.run_query_pg(q, connection)
+        if result == "error":
+            print("Please fix error before rerunning and reconnect to the database")
+            break
 
-    result = dbf.run_query_pg(q, connection)
-    if result == "error":
-        print("Please fix error before rerunning and reconnect to the database")
-        break
+        q = f"""CREATE TABLE {t} (
+            start_node INTEGER DEFAULT NULL,
+            reachable_nodes VARCHAR DEFAULT NULL
+        );"""
 
+        result = dbf.run_query_pg(q, connection)
+        if result == "error":
+            print("Please fix error before rerunning and reconnect to the database")
+            break
 
-# %%
-# COMPUTE REACHABLE NODES USING CHUNKS
+    # COMPUTE REACHABLE NODES USING CHUNKS
 
-for i, t in enumerate(table_names):
+    for i, t in enumerate(table_names):
 
-    print(f"At round: {i}")
+        print(f"At round: {i}")
 
-    # GET ALL IDS
-    df = pd.read_sql_query(f"SELECT node_id FROM {hex_tables[i]}", con=engine)
-    node_ids = df.node_id.to_list()
+        # GET ALL IDS
+        df = pd.read_sql_query(f"SELECT node_id FROM {hex_tables[i]}", con=engine)
+        node_ids = df.node_id.to_list()
 
-    for ids_chunk in dbf.get_chunks(sequence=node_ids, chunk_size=1000):
+        for ids_chunk in dbf.get_chunks(sequence=node_ids, chunk_size=1000):
 
-        # for ids in ids_chunk:
+            # for ids in ids_chunk:
 
-        q = f"""
-        INSERT INTO
-            {t} (
-                SELECT
-                    from_v AS start_node,   
-                    ARRAY_AGG(
-                        node
-                        ORDER BY
+            q = f"""
+            INSERT INTO
+                {t} (
+                    SELECT
+                        from_v AS start_node,   
+                        ARRAY_AGG(
                             node
-                    ) AS reachable_nodes
-                FROM
-                    pgr_drivingDistance(
-                        'SELECT id, source, target, km AS cost FROM {edge_tables[i]}',
-                        (
-                           array{ids_chunk}
-                                
+                            ORDER BY
+                                node
+                        ) AS reachable_nodes
+                    FROM
+                        pgr_drivingDistance(
+                            'SELECT id, source, target, km AS cost FROM {edge_tables[i]}',
+                            (
+                            array{ids_chunk}
+                                    
+                            )
+                            ,
+                            {dist},
+                            FALSE
                         )
-                        ,
-                        {dist},
-                        FALSE
-                    )
-                GROUP BY
-                    from_v
-            );"""
+                    GROUP BY
+                        from_v
+                );"""
+
+            result = dbf.run_query_pg(q, connection)
+            if result == "error":
+                print("Please fix error before rerunning and reconnect to the database")
+                break
+
+    # CREATE INDICES
+
+    result = dbf.run_query_pg(
+        "CREATE INDEX IF NOT EXISTS idx_nodes_id ON nodes(id);", connection
+    )
+
+    if result == "error":
+        print("Please fix error before rerunning and reconnect to the database")
+
+    for i, t in enumerate(table_names):
+
+        l = i + 1
+        q = f"CREATE INDEX IF NOT EXISTS idx_reach_{l}_start_node ON {t}(start_node);"
 
         result = dbf.run_query_pg(q, connection)
+
         if result == "error":
             print("Please fix error before rerunning and reconnect to the database")
             break
 
-# %%
-# CREATE INDICES
+    # **** COMPUT REACHABLE EDGES ****
 
-result = dbf.run_query_pg(
-    "CREATE INDEX IF NOT EXISTS idx_nodes_id ON nodes(id);", connection
-)
-
-if result == "error":
-    print("Please fix error before rerunning and reconnect to the database")
-
-for i, t in enumerate(table_names):
-
-    l = i + 1
-    q = f"CREATE INDEX IF NOT EXISTS idx_reach_{l}_start_node ON {t}(start_node);"
-
-    result = dbf.run_query_pg(q, connection)
-
-    if result == "error":
-        print("Please fix error before rerunning and reconnect to the database")
-        break
-
-
-# %%
-# **** COMPUT REACHABLE EDGES ****
-
-# Add new columns
-
-for t in table_names:
-
-    q = f"""
-        ALTER TABLE
-            {t}
-        ADD
-            COLUMN IF NOT EXISTS reachable_edges VARCHAR DEFAULT NULL,
-        ADD
-            COLUMN IF NOT EXISTS edge_length NUMERIC DEFAULT NULL;
-        """
-
-    result = dbf.run_query_pg(q, connection)
-    if result == "error":
-        print("Please fix error before rerunning and reconnect to the database")
-        break
-
-
-# %%
-# compute reachable edges
-for i, t in enumerate(table_names):
-
-    print(f"At round: {i}")
-
-    # GET ALL IDS
-    df = pd.read_sql_query(f"SELECT node_id FROM {hex_tables[i]}", con=engine)
-    node_ids = tuple(df.node_id.to_list())
-
-    for ids_chunk in dbf.get_chunks(sequence=node_ids, chunk_size=1000):
+    # Add new columns
+    for t in table_names:
 
         q = f"""
-            WITH filtered_nodes AS (
-                SELECT *
-                FROM {t}
-                WHERE start_node IN {ids_chunk}
-            ),
-            filtered_edges AS (
-                SELECT e.id, e.source, e.target, e.geometry, ST_Length(e.geometry) AS length
-                FROM {edge_tables[i]} e
-                WHERE e.source IN (
-                    SELECT unnest(reachable_nodes::int[]) FROM filtered_nodes
-                ) OR e.target IN (
-                    SELECT unnest(reachable_nodes::int[]) FROM filtered_nodes
-                )
-            ),
-            joined_edges AS (
-                SELECT
-                    fn.start_node,
-                    array_agg(fe.id) AS reachable_edges,
-                    SUM(fe.length) AS total_length
-                FROM filtered_nodes fn
-                JOIN filtered_edges fe
-                ON fe.source = ANY(fn.reachable_nodes::int[])
-                AND fe.target = ANY(fn.reachable_nodes::int[])
-                GROUP BY fn.start_node
-            )
-            UPDATE {t} rc
-            SET
-                reachable_edges = j.reachable_edges,
-                edge_length = j.total_length
-            FROM joined_edges j
-            WHERE rc.start_node = j.start_node;
-        
-        """
+            ALTER TABLE
+                {t}
+            ADD
+                COLUMN IF NOT EXISTS reachable_edges VARCHAR DEFAULT NULL,
+            ADD
+                COLUMN IF NOT EXISTS edge_length NUMERIC DEFAULT NULL;
+            """
 
         result = dbf.run_query_pg(q, connection)
         if result == "error":
             print("Please fix error before rerunning and reconnect to the database")
             break
 
-# %%
-queries = [
-    "sql/05c_compute_reach_difference.sql",
-    "sql/05d_reach_length_component_comparison.sql",
-]
+    # compute reachable edges
+    for i, t in enumerate(table_names):
 
-for i, q in enumerate(queries):
-    print(f"Running step {i+1}...")
-    result = dbf.run_query_pg(q, connection)
+        print(f"At round: {i}")
+
+        # GET ALL IDS
+        df = pd.read_sql_query(f"SELECT node_id FROM {hex_tables[i]}", con=engine)
+        node_ids = tuple(df.node_id.to_list())
+
+        for ids_chunk in dbf.get_chunks(sequence=node_ids, chunk_size=1000):
+
+            q = f"""
+                WITH filtered_nodes AS (
+                    SELECT *
+                    FROM {t}
+                    WHERE start_node IN {ids_chunk}
+                ),
+                filtered_edges AS (
+                    SELECT e.id, e.source, e.target, e.geometry, ST_Length(e.geometry) AS length
+                    FROM {edge_tables[i]} e
+                    WHERE e.source IN (
+                        SELECT unnest(reachable_nodes::int[]) FROM filtered_nodes
+                    ) OR e.target IN (
+                        SELECT unnest(reachable_nodes::int[]) FROM filtered_nodes
+                    )
+                ),
+                joined_edges AS (
+                    SELECT
+                        fn.start_node,
+                        array_agg(fe.id) AS reachable_edges,
+                        SUM(fe.length) AS total_length
+                    FROM filtered_nodes fn
+                    JOIN filtered_edges fe
+                    ON fe.source = ANY(fn.reachable_nodes::int[])
+                    AND fe.target = ANY(fn.reachable_nodes::int[])
+                    GROUP BY fn.start_node
+                )
+                UPDATE {t} rc
+                SET
+                    reachable_edges = j.reachable_edges,
+                    edge_length = j.total_length
+                FROM joined_edges j
+                WHERE rc.start_node = j.start_node;
+            
+            """
+
+            result = dbf.run_query_pg(q, connection)
+            if result == "error":
+                print("Please fix error before rerunning and reconnect to the database")
+                break
+
+    # Join hex_id to reach tables
+    for i, t in enumerate(table_names):
+        q1 = f"ALTER TABLE {t} ADD COLUMN IF NOT EXISTS hex_id VARCHAR;"
+        q2 = f"UPDATE {t} SET hex_id = h.hex_id FROM {hex_tables[i]} h WHERE {t}.start_node = h.node_id;"
+        q3 = f"CREATE INDEX IF NOT EXISTS {network_types[i]}_reach_{dist}_ix ON {t} (hex_id);"
+
+        for q in [q1, q2, q3]:
+            result = dbf.run_query_pg(q, connection)
+            if result == "error":
+                print("Please fix error before rerunning and reconnect to the database")
+                break
+
+    # prepare joined table
+    prepare_hex_reach = [
+        "CREATE INDEX IF NOT EXISTS hex_grid_ix ON hex_grid (hex_id);"
+        f"DROP TABLE IF EXISTS reach.hex_reach_{dist};"
+    ]
+
+    for q in prepare_hex_reach:
+        result = dbf.run_query_pg(q, connection)
+        if result == "error":
+            print("Please fix error before rerunning and reconnect to the database")
+            break
+
+    # create joined table
+    q_hex_reach = f"""
+    CREATE TABLE reach.hex_reach_{dist} AS
+    SELECT
+        h.hex_id,
+        h.geometry,
+        l1.edge_length AS lts1_reach,
+        l2.edge_length AS lts2_reach,
+        l3.edge_length AS lts3_reach,
+        l4.edge_length AS lts4_reach,
+        ca.edge_length AS car_reach
+    FROM
+        hex_grid h
+        LEFT JOIN {table_names[0]} l1 ON h.hex_id = l1.hex_id
+        LEFT JOIN  {table_names[1]} l2 ON h.hex_id = l2.hex_id
+        LEFT JOIN  {table_names[2]} l3 ON h.hex_id = l3.hex_id
+        LEFT JOIN  {table_names[3]} l4 ON h.hex_id = l4.hex_id
+        LEFT JOIN  {table_names[4]} ca ON h.hex_id = ca.hex_id;
+    """
+
+    result = dbf.run_query_pg(q_hex_reach, connection)
     if result == "error":
         print("Please fix error before rerunning and reconnect to the database")
-        break
 
-    print(f"Step {i+1} done!")
+    # Check if all hexagons are present
+    q_check = f"""
+    DO $$
+    DECLARE
+        hex_reach_len INT;
 
+    DECLARE
+        hex_grid_len INT;
 
-connection.close()
+    BEGIN
+        SELECT
+            COUNT(*) INTO hex_reach_len
+        FROM
+            reach.hex_reach_{dist};
+
+    SELECT
+        COUNT(*) INTO hex_grid_len
+    FROM
+        hex_grid;
+
+    ASSERT hex_grid_len = hex_reach_len,
+    'Missing hex cells!';
+
+    END $$;
+    """
+    result = dbf.run_query_pg(q_check, connection)
+    if result == "error":
+        print("Please fix error before rerunning and reconnect to the database")
+
+    # compute difference between lts and car reach
+    q_alter = f"""
+    ALTER TABLE
+        reach.hex_reach_{dist}
+    ADD
+        COLUMN car_lts1_diff DECIMAL,
+    ADD
+        COLUMN car_lts2_diff DECIMAL,
+    ADD
+        COLUMN car_lts3_diff DECIMAL,
+    ADD
+        COLUMN car_lts4_diff DECIMAL;"""
+
+    q_update = f"""UPDATE
+        reach.hex_reach_{dist}
+    SET
+        car_lts1_diff = car_reach - lts1_reach,
+        car_lts2_diff = car_reach - lts2_reach,
+        car_lts3_diff = car_reach - lts3_reach,
+        car_lts4_diff = car_reach - lts4_reach;
+
+    """
+
+    for q in [q_alter, q_update]:
+        result = dbf.run_query_pg(q, connection)
+        if result == "error":
+            print("Please fix error before rerunning and reconnect to the database")
+
+    for n in network_types:
+        q = f"""
+        UPDATE
+            reach.hex_reach_{dist}
+        SET
+            {n}_reach = 0
+        WHERE
+            {n}_reach IS NULL;
+
+        """
+
+        result = dbf.run_query_pg(q, connection)
+        if result == "error":
+            print("Please fix error before rerunning and reconnect to the database")
+
+    # compute percentage difference between lts and car reach
+    q_alter = f"""
+    ALTER TABLE
+        reach.hex_reach_{dist}
+    ADD
+        COLUMN car_lts1_diff_pct DECIMAL,
+    ADD
+        COLUMN car_lts2_diff_pct DECIMAL,
+    ADD
+        COLUMN car_lts3_diff_pct DECIMAL,
+    ADD
+        COLUMN car_lts4_diff_pct DECIMAL;
+    """
+
+    q_update = f"""UPDATE
+        reach.hex_reach_{dist}
+    SET
+        car_lts1_diff_pct = (lts1_reach / car_reach) * 100,
+        car_lts2_diff_pct = (lts2_reach / car_reach) * 100,
+        car_lts3_diff_pct = (lts3_reach / car_reach) * 100,
+        car_lts4_diff_pct = (lts4_reach / car_reach) * 100
+    WHERE
+        car_reach > 0;"""
+
+    for q in [q_alter, q_update]:
+        result = dbf.run_query_pg(q, connection)
+        if result == "error":
+            print("Please fix error before rerunning and reconnect to the database")
+            break
+
+    for n in network_types[-1]:
+        f"""
+        UPDATE
+            reach.hex_reach_{dist}
+        SET
+            car_{n}_diff_pct = 100
+        WHERE
+            car_reach = 0
+            AND {n}_reach > 0;
+        """
+        result = dbf.run_query_pg(q, connection)
+        if result == "error":
+            print("Please fix error before rerunning and reconnect to the database")
+            break
+
+    # combine results and join to hex grid
+    q_drop = f"DROP TABLE IF EXISTS reach.reach_{dist}_component_length_hex;"
+
+    q_create = f"""
+    CREATE TABLE reach.reach_{dist}_component_length_hex AS (
+        SELECT
+            dens.hex_id,
+            dens.lts_1_length,
+            dens.lts_1_2_length,
+            dens.lts_1_3_length,
+            dens.lts_1_4_length,
+            dens.total_car_length,
+            dens.lts_1_dens,
+            dens.lts_1_2_dens,
+            dens.lts_1_3_dens,
+            dens.lts_1_4_dens,
+            dens.total_car_dens,
+            dens.geometry,
+            comp.comp_1_count,
+            comp.comp_2_count,
+            comp.comp_3_count,
+            comp.comp_4_count,
+            comp.comp_car_count,
+            reach.lts1_reach,
+            reach.lts2_reach,
+            reach.lts3_reach,
+            reach.lts4_reach,
+            reach.car_reach
+        FROM
+            density.density_hex dens
+            LEFT JOIN fragmentation.comp_count_hex comp ON dens.hex_id = comp.hex_id
+            LEFT JOIN reach.hex_reach_{dist} reach ON dens.hex_id = reach.hex_id
+    );
+    """
+
+    q_update = f"""
+    UPDATE
+        reach.reach_{dist}_component_length_hex
+    SET
+        lts1_reach = lts1_reach / 1000,
+        lts2_reach = lts2_reach / 1000,
+        lts3_reach = lts3_reach / 1000,
+        lts4_reach = lts4_reach / 1000,
+        car_reach = car_reach / 1000;
+    """
+
+    for q in [q_drop, q_create, q_update]:
+        result = dbf.run_query_pg(q, connection)
+        if result == "error":
+            print("Please fix error before rerunning and reconnect to the database")
+            break
+
+    # drop tables
+    for t in table_names:
+        q = f"DROP TABLE IF EXISTS {t};"
+        result = dbf.run_query_pg(q, connection)
+        if result == "error":
+            print("Please fix error before rerunning and reconnect to the database")
+            break
+
+    connection.close()
 
 # %%
-
 with open("vacuum_analyze.py") as f:
     exec(f.read())
+
+# %%
+
+start = "CREATE TABLE reach.compare_reach AS (SELECT"
+end = ");"
+
+select_cols = ""
+
+for d in distances:
+    s = f"""
+    r{d}.lts1_reach_{d},
+    r{d}.lts2_reach_{d},
+    r{d}.lts3_reach_{d},
+    r{d}.lts4_reach_{d},
+    r{d}.car_reach_{d},
+    """
+    select_cols += s
+
+from_q = f"FROM reach.hex_reach_{distances[0]} r{distances[0]}"
+
+for d in distances[1:]:
+    from_q += f" JOIN reach.hex_reach_{d} r{d} ON r{d}.hex_id = r{distances[0]}.hex_id"
+
+final_query = start + select_cols + from_q + end
+
+print(final_query)
+
+# %%
+
 
 print("Script 05 complete!")
 # %%
