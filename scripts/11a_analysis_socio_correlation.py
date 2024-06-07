c187231689dcc526517db9e0e478b5e9df5f24bd
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-06-07 12:55:31 +0200

    moved data read

--- scripts/11a_analysis_socio_correlation.py
+++ scripts/11a_analysis_socio_correlation.py
@@ -23,45 +23,7 @@ connection = dbf.connect_pg(db_name, db_user, db_password, db_port=db_port)
 ###### SOCIO-ECO VARIABLES #####
 ################################
 
-socio = gpd.read_postgis("SELECT * FROM socio", engine, geom_col="geometry")
-
-rename_dict = {
-    "households_income_under_100k_share": "Income under 100k",
-    "households_income_100_150k_share": "Income 100-150k",
-    "households_income_150_200k_share": "Income 150-200k",
-    "households_income_200_300k_share": "Income 200-300k",
-    "households_income_300_400k_share": "Income 300-400k",
-    "households_income_400_500k_share": "Income 400-500k",
-    "households_income_500_750k_share": "Income 500-750k",
-    "households_income_750k_share": "Income 750k",
-    "households_with_car_share": "Households w car",
-    "households_1car_share": "Households 1 car",
-    "households_2cars_share": "Households 2 cars",
-    "households_nocar_share": "Households no car",
-}
-
-socio.rename(columns=rename_dict, inplace=True)
-
-socio_corr_variables = [
-    "Income under 100k",
-    "Income 100-150k",
-    "Income 150-200k",
-    "Income 200-300k",
-    "Income 300-400k",
-    "Income 400-500k",
-    "Income 500-750k",
-    "Income 750k",
-    "Households w car",
-    "Households 1 car",
-    "Households 2 cars",
-    "Households no car",
-    "population_density",
-    "urban_pct",
-]
-
-keep_columns = socio_corr_variables + ["id"]
-
-socio = socio[keep_columns]
+exec(open("../settings/read_socio_pop").read())
 
 plot_func.plot_correlation(
     socio,
@@ -82,7 +44,6 @@ socio_density = gpd.read_postgis(
 )
 
 socio.dropna(subset=["population_density"], inplace=True)
-# socio.drop(columns=["geometry", "area_name"], inplace=True)
 
 socio_density = socio_density.merge(socio, on="id", how="inner")
 
@@ -150,7 +111,7 @@ socio_components = socio_components[
     ]
 ].merge(socio, on="id", how="inner")
 
-keep_columns = socio_largest_component_columns + ["id"]
+keep_columns = socio_largest_component_columns_ave + ["id"]
 socio_components = socio_components.merge(
     socio_largest_components[keep_columns], on="id", how="inner"
 )
@@ -159,7 +120,7 @@ socio_components = socio_components.merge(
 all_fragmentation_columns = [
     component_count_columns,
     component_per_km_columns,
-    socio_largest_component_columns,
+    socio_largest_component_columns_ave,
 ]
 
 labels = [
