ca9744d4df30b25816a19885f6aefccc8ba9551a
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-06-10 13:54:59 +0200

    keep nan

--- settings/read_largest_components.py
+++ settings/read_largest_components.py
@@ -1,6 +1,5 @@
 from src import db_functions as dbf
-import pandas as pd
-import numpy as np
+import geopandas as gpd
 
 exec(open("../settings/yaml_variables.py").read())
 exec(open("../settings/plotting.py").read())
@@ -18,13 +17,13 @@ hex_largest_components = gpd.GeoDataFrame.from_postgis(
     geom_col="geometry",
 )
 
-hex_largest_components[largest_local_component_len_columns] = hex_largest_components[
-    largest_local_component_len_columns
-].replace(np.nan, 0)
+# hex_largest_components[largest_local_component_len_columns] = hex_largest_components[
+#     largest_local_component_len_columns
+# ].replace(np.nan, 0)
 
-hex_largest_components[largest_local_component_area_columns] = hex_largest_components[
-    largest_local_component_area_columns
-].replace(np.nan, 0)
+# hex_largest_components[largest_local_component_area_columns] = hex_largest_components[
+#     largest_local_component_area_columns
+# ].replace(np.nan, 0)
 
 
 print("Loaded largest components per hexagon!")
