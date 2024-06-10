3a79f901ad7bb914b5ac11a0a511f73b51d2b847
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-06-10 13:54:50 +0200

    changed pd to gdf

--- settings/read_component_length_agg.py
+++ settings/read_component_length_agg.py
@@ -1,5 +1,6 @@
 from src import db_functions as dbf
 import pandas as pd
+import geopandas as gpd
 
 exec(open("../settings/yaml_variables.py").read())
 exec(open("../settings/plotting.py").read())
@@ -10,14 +11,14 @@ engine = dbf.connect_alc(db_name, db_user, db_password, db_port=db_port)
 connection = dbf.connect_pg(db_name, db_user, db_password, db_port=db_port)
 
 
-component_length_muni = pd.read_sql(
-    "SELECT * FROM fragmentation.component_length_muni;", engine
+component_length_muni = gpd.read_postgis(
+    "SELECT * FROM fragmentation.component_length_muni;", engine, geom_col="geometry"
 )
-component_length_socio = pd.read_sql(
-    "SELECT * FROM fragmentation.component_length_socio;", engine
+component_length_socio = gpd.read_postgis(
+    "SELECT * FROM fragmentation.component_length_socio;", engine, geom_col="geometry"
 )
-component_length_hex = pd.read_sql(
-    "SELECT * FROM fragmentation.component_length_hex;", engine
+component_length_hex = gpd.read_postgis(
+    "SELECT * FROM fragmentation.component_length_hex;", engine, geom_col="geometry"
 )
 
 print("Component length per aggregation level loaded!")
