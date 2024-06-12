f1f39151dc991907c771debe70cbf38039f318b3
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-06-12 15:01:44 +0200

    debug

--- settings/prepare_socio_cluster_data.py
+++ settings/prepare_socio_cluster_data.py
@@ -13,23 +13,29 @@ exec(open("../settings/plotting.py").read())
 # Read socio pop
 exec(open("../settings/read_socio_pop.py").read())
 
-socio.dropna(subset=["population_density"], inplace=True)
-
 exec(open("../settings/read_socio_results.py").read())
 
+socio_density = socio_density[socio_density.total_network_length > 0]
+
 # Merge all
-socio_gdf = socio.merge(socio_density, on="id", how="inner")
-socio_gdf = socio.merge(socio_components, on="id", how="inner")
-socio_gdf = socio.merge(socio_largest_components, on="id", how="inner")
-socio_gdf = socio.merge(socio_reach, on="id", how="inner")
-socio_gdf = socio.merge(socio_reach_comparison, on="id", how="inner")
+socio_cluster_gdf = socio.merge(socio_density, on="id", how="inner")
+socio_cluster_gdf = socio_cluster_gdf.merge(socio_components, on="id", how="inner")
+socio_cluster_gdf = socio_cluster_gdf.merge(
+    socio_largest_components, on="id", how="inner"
+)
+socio_cluster_gdf = socio_cluster_gdf.merge(socio_reach, on="id", how="inner")
+socio_cluster_gdf = socio_cluster_gdf.merge(
+    socio_reach_comparison, on="id", how="inner"
+)
 
-assert len(socio) == len(socio_gdf)
+assert len(socio_density) == len(socio_cluster_gdf)
 
-duplicates = [c for c in socio_gdf.columns if c.endswith("_x") or c.endswith("_y")]
+duplicates = [
+    c for c in socio_cluster_gdf.columns if c.endswith("_x") or c.endswith("_y")
+]
 assert len(duplicates) == 0
 
-socio_gdf.replace(np.nan, 0, inplace=True)
+socio_cluster_gdf.replace(np.nan, 0, inplace=True)
 
 
 # %%
