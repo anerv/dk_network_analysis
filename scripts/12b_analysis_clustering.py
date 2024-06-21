17201fdb5bcdc99ac070b4e592eb31cb63c4f01a
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-06-21 11:04:53 +0200

    removed cluster intersection and cluster of inter

--- scripts/12b_analysis_clustering.py
+++ scripts/12b_analysis_clustering.py
@@ -117,26 +117,28 @@ plot_func.plot_rank(socio_cluster_gdf, "network_rank")
 
 
 # %%
-
+plot_func.set_renderer("svg")
 # input = [v for v in lts_color_dict.values()]
-# test_colors = plot_func.color_list_to_cmap(input)
+input_colors = plot_func.get_hex_colors_from_colormap("viridis", k)
+input_colors.reverse()
+test_colors = plot_func.color_list_to_cmap(input_colors)
 
 
-# def plot_rank(gdf, label_col, cmap="viridis"):
-#     fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-#     ax.set_axis_off()
-#     gdf.plot(
-#         column=label_col,
-#         legend=True,
-#         ax=ax,
-#         cmap=cmap,
-#         linewidth=0.1,
-#         categorical=True,
-#     )
-#     plt.tight_layout()
+def plot_rank(gdf, label_col, cmap="viridis"):
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    ax.set_axis_off()
+    gdf.plot(
+        column=label_col,
+        legend=True,
+        ax=ax,
+        cmap=cmap,
+        linewidth=0.1,
+        categorical=True,
+    )
+    plt.tight_layout()
 
 
-# plot_rank(socio_cluster_gdf, "network_rank", cmap=test_colors)
+plot_rank(socio_cluster_gdf, "network_rank", cmap=test_colors)
 
 # %%
 # SOCIO CLUSTERING: Socio-economic variables
@@ -280,7 +282,9 @@ correlation = (
 )
 correlation = correlation.div(correlation.sum(axis=1), axis=0)
 plt.figure(figsize=(15, 15))
-correlation.plot(kind="bar", stacked=True, color=lts_color_dict.values())
+correlation.plot(
+    kind="bar", stacked=True, color=input_colors
+)  # lts_color_dict.values()
 plt.ylabel("Proportion", fontsize=12)
 plt.title("Correlation between Socio Label and Network Rank", fontsize=14)
 plt.legend(bbox_to_anchor=(1, 1), fontsize=12, title="Network Rank", title_fontsize=10)
@@ -291,87 +295,6 @@ plt.xlabel("", fontsize=12)
 plt.show()
 
 # %%
-# Compute cluster intersections
 
-# socio_cluster_gdf["network_cluster"] = socio_cluster_gdf[
-#     "network_rank"
-# ]  # socio_cluster_gdf[kmeans_col_net]
-# socio_cluster_gdf["socio_cluster"] = socio_cluster_gdf[
-#     "socio_label"
-# ]  # socio_cluster_gdf[kmeans_col_soc]
-
-# intersections = gpd.overlay(
-#     socio_cluster_gdf[["network_cluster", "geometry"]],
-#     socio_cluster_gdf[["socio_cluster", "geometry"]],
-#     how="intersection",
-#     keep_geom_type=True,
-# )
-
-# intersections["cluster_combinations"] = (
-#     intersections.network_cluster.astype(str)
-#     + "_"
-#     + intersections.socio_cluster.astype(str)
-# )
-
-# inter_dissolved = intersections.dissolve(by="cluster_combinations")
-
-# inter_dissolved.reset_index(inplace=True)
-
-# inter_dissolved["area_sqkm"] = inter_dissolved.geometry.area / 1000000
-
-# intersecting_clusters = inter_dissolved[inter_dissolved.area_sqkm > 0.1]
-
-# display(
-#     intersecting_clusters[["cluster_combinations", "area_sqkm"]]
-#     .sort_values(by="area_sqkm", ascending=False)
-#     .style.pipe(format_style_index)
-# )
-
-# intersecting_clusters.to_file(fp_cluster_intersections, driver="GPKG")
-
-# %%
-
-# all_socio_variables = socio_network_cluster_variables + socio_soc_cluster_variables
-
-# # Use robust_scale to norm cluster variables
-# socio_all_scaled = robust_scale(socio_cluster_gdf[all_socio_variables])
-
-# # Find appropriate number of clusters
-# m1, m2 = analysis_func.find_k_elbow_method(socio_all_scaled, min_k=1, max_k=20)
-
-# for key, val in m1.items():
-#     print(f"{key} : {val:.2f}")
-
-
-# # Define K!
-# k = 10
-
-# ##### K-Means #######
-
-# kmeans_col_all = f"kmeans_all_{k}"
-
-# k_labels = analysis_func.run_kmeans(k, socio_all_scaled)
-
-# socio_cluster_gdf[kmeans_col_all] = k_labels
-
-# fp_map = fp_socio_socio_cluster_base + f"__map_{kmeans_col_all}.png"
-# fp_size = fp_socio_socio_cluster_base + f"_size_{kmeans_col_all}.png"
-# fp_kde = fp_socio_socio_cluster_base + f"_kde_{kmeans_col_all}.png"
-
-# colors = plot_func.get_hex_colors_from_colormap("Set2", k)
-# cmap = plot_func.color_list_to_cmap(colors)
-
-# cluster_means_all = analysis_func.examine_cluster_results(
-#     socio_cluster_gdf,
-#     kmeans_col_all,
-#     all_socio_variables,
-#     fp_map,
-#     fp_size,
-#     fp_kde,
-#     cmap=cmap,
-#     palette=colors,
-# )
-
-# plot_func.style_cluster_means(cluster_means_all)
 
 # %%
