c7cf9dc0b6dc8cc816ef640c63659254ad9720da
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-07-01 14:17:15 +0200

    moved socio pop rename dict

--- settings/plotting.py
+++ settings/plotting.py
@@ -255,19 +255,35 @@ socio_reach_max_columns = [
     "car_reach_max",
 ]
 
+population_rename_dict = {
+    "households_income_under_100k_share": "Income under 100k (share)",
+    "households_income_100_150k_share": "Income 100-150k (share)",
+    "households_income_150_200k_share": "Income 150-200k (share)",
+    "households_income_200_300k_share": "Income 200-300k (share)",
+    "households_income_300_400k_share": "Income 300-400k (share)",
+    "households_income_400_500k_share": "Income 400-500k (share)",
+    "households_income_500_750k_share": "Income 500-750k (share)",
+    "households_income_750k_share": "Income 750k (share)",
+    "households_with_car_share": "Households w car (share)",
+    "households_1car_share": "Households 1 car (share)",
+    "households_2cars_share": "Households 2 cars (share)",
+    "households_nocar_share": "Households no car (share)",
+}
+
+
 socio_corr_variables = [
-    "Income under 100k",
-    "Income 100-150k",
-    "Income 150-200k",
-    "Income 200-300k",
-    "Income 300-400k",
-    "Income 400-500k",
-    "Income 500-750k",
-    "Income 750k",
-    "Households w car",
-    "Households 1 car",
-    "Households 2 cars",
-    "Households no car",
+    "Income under 100k (share)",
+    "Income 100-150k (share)",
+    "Income 150-200k (share)",
+    "Income 200-300k (share)",
+    "Income 300-400k (share)",
+    "Income 400-500k (share)",
+    "Income 500-750k (share)",
+    "Income 750k (share)",
+    "Households w car (share)",
+    "Households 1 car (share)",
+    "Households 2 cars (share)",
+    "Households no car (share)",
     "population_density",
     "urban_pct",
 ]
--- settings/read_socio_pop.py
+++ settings/read_socio_pop.py
@@ -8,22 +8,8 @@ engine = dbf.connect_alc(db_name, db_user, db_password, db_port=db_port)
 
 socio = gpd.read_postgis("SELECT * FROM socio", engine, geom_col="geometry")
 
-pop_rename_dict = {
-    "households_income_under_100k_share": "Income under 100k",
-    "households_income_100_150k_share": "Income 100-150k",
-    "households_income_150_200k_share": "Income 150-200k",
-    "households_income_200_300k_share": "Income 200-300k",
-    "households_income_300_400k_share": "Income 300-400k",
-    "households_income_400_500k_share": "Income 400-500k",
-    "households_income_500_750k_share": "Income 500-750k",
-    "households_income_750k_share": "Income 750k",
-    "households_with_car_share": "Households w car",
-    "households_1car_share": "Households 1 car",
-    "households_2cars_share": "Households 2 cars",
-    "households_nocar_share": "Households no car",
-}
 
-socio.rename(columns=pop_rename_dict, inplace=True)
+socio.rename(columns=population_rename_dict, inplace=True)
 
 keep_columns = socio_corr_variables + ["id", "area_name", "geometry"]
 
