47876ad0535283fcb17af8d8aea02b322075c7b4
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-07-11 11:52:40 +0200

    debug - network length

--- scripts/sql/04d_component_length_comparison.sql
+++ scripts/sql/04d_component_length_comparison.sql
@@ -204,25 +204,143 @@ SET
     component_per_dens_1_4 = component_1_4_count / lts_1_4_dens,
     component_per_dens_car = component_car_count / total_car_dens;
 
+-- UPDATE
+--     fragmentation.component_length_socio
+-- SET
+--     component_per_length_all = component_all_count / total_network_length,
+--     component_per_length_1 = component_1_count / lts_1_length,
+--     component_per_length_1_2 = component_1_2_count / lts_1_2_length,
+--     component_per_length_1_3 = component_1_3_count / lts_1_3_length,
+--     component_per_length_1_4 = component_1_4_count / lts_1_4_length,
+--     component_per_length_car = component_car_count / total_car_length;
+-- UPDATE
+--     fragmentation.component_length_socio
+-- SET
+--     component_per_dens_all = component_all_count / total_network_dens,
+--     component_per_dens_1 = component_1_count / lts_1_dens,
+--     component_per_dens_1_2 = component_1_2_count / lts_1_2_dens,
+--     component_per_dens_1_3 = component_1_3_count / lts_1_3_dens,
+--     component_per_dens_1_4 = component_1_4_count / lts_1_4_dens,
+--     component_per_dens_car = component_car_count / total_car_dens;
 UPDATE
     fragmentation.component_length_socio
 SET
-    component_per_length_all = component_all_count / total_network_length,
-    component_per_length_1 = component_1_count / lts_1_length,
-    component_per_length_1_2 = component_1_2_count / lts_1_2_length,
-    component_per_length_1_3 = component_1_3_count / lts_1_3_length,
-    component_per_length_1_4 = component_1_4_count / lts_1_4_length,
-    component_per_length_car = component_car_count / total_car_length;
+    component_per_length_all = component_all_count / total_network_length
+WHERE
+    (
+        total_network_length > 0
+        AND total_network_length IS NOT NULL
+    );
 
 UPDATE
     fragmentation.component_length_socio
 SET
-    component_per_dens_all = component_all_count / total_network_dens,
-    component_per_dens_1 = component_1_count / lts_1_dens,
-    component_per_dens_1_2 = component_1_2_count / lts_1_2_dens,
-    component_per_dens_1_3 = component_1_3_count / lts_1_3_dens,
-    component_per_dens_1_4 = component_1_4_count / lts_1_4_dens,
-    component_per_dens_car = component_car_count / total_car_dens;
+    component_per_length_1 = component_1_count / lts_1_length
+WHERE
+    (
+        lts_1_length > 0
+        AND lts_1_length IS NOT NULL
+    );
+
+UPDATE
+    fragmentation.component_length_socio
+SET
+    component_per_length_1_2 = component_1_2_count / lts_1_2_length
+WHERE
+    (
+        lts_1_2_length > 0
+        AND lts_1_2_length IS NOT NULL
+    );
+
+UPDATE
+    fragmentation.component_length_socio
+SET
+    component_per_length_1_3 = component_1_3_count / lts_1_3_length
+WHERE
+    (
+        lts_1_3_length > 0
+        AND lts_1_3_length IS NOT NULL
+    );
+
+UPDATE
+    fragmentation.component_length_socio
+SET
+    component_per_length_1_4 = component_1_4_count / lts_1_4_length
+WHERE
+    (
+        lts_1_4_length > 0
+        AND lts_1_4_length IS NOT NULL
+    );
+
+UPDATE
+    fragmentation.component_length_socio
+SET
+    component_per_length_car = component_car_count / total_car_length
+WHERE
+    (
+        total_car_length > 0
+        AND total_car_length IS NOT NULL
+    );
+
+UPDATE
+    fragmentation.component_length_socio
+SET
+    component_per_dens_all = component_all_count / total_network_dens
+WHERE
+    (
+        total_network_length > 0
+        AND total_network_length IS NOT NULL
+    );
+
+UPDATE
+    fragmentation.component_length_socio
+SET
+    component_per_dens_1 = component_1_count / lts_1_dens
+WHERE
+    (
+        lts_1_length > 0
+        AND lts_1_length IS NOT NULL
+    );
+
+UPDATE
+    fragmentation.component_length_socio
+SET
+    component_per_dens_1_2 = component_1_2_count / lts_1_2_dens
+WHERE
+    (
+        lts_1_2_length > 0
+        AND lts_1_2_length IS NOT NULL
+    );
+
+UPDATE
+    fragmentation.component_length_socio
+SET
+    component_per_dens_1_3 = component_1_3_count / lts_1_3_dens
+WHERE
+    (
+        lts_1_3_length > 0
+        AND lts_1_3_length IS NOT NULL
+    );
+
+UPDATE
+    fragmentation.component_length_socio
+SET
+    component_per_dens_1_4 = component_1_4_count / lts_1_4_dens
+WHERE
+    (
+        lts_1_4_length > 0
+        AND lts_1_4_length IS NOT NULL
+    );
+
+UPDATE
+    fragmentation.component_length_socio
+SET
+    component_per_dens_car = component_car_count / total_car_dens
+WHERE
+    (
+        total_car_length > 0
+        AND total_car_length IS NOT NULL
+    );
 
 UPDATE
     fragmentation.component_length_hex
