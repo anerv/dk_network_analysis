743aaf3e38405003da8b9bfa544a61be54570b62
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-07-16 12:23:53 +0200

    tidy

--- scripts/08c_summary_stats_reach.py
+++ scripts/08c_summary_stats_reach.py
@@ -1,11 +1,7 @@
 # %%
-from src import db_functions as dbf
-from src import plotting_functions as plot_func
-import geopandas as gpd
+
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-import plotly_express as px
 import pandas as pd
 from IPython.display import display
 
@@ -51,6 +47,7 @@ df = pd.DataFrame(
 
 df.to_csv(filepath_summary_stats_reach, index=True)
 
+print(f"Reach summary stats for reach at distance {reach_dist} km:")
 display(df.style.pipe(format_style_index))
 
 # %%
@@ -81,6 +78,9 @@ df = pd.DataFrame(
     },
 )
 
+print(
+    f"Summary stats for reach differences between bike and car reach at distance {reach_dist} km:"
+)
 df.to_csv(filepath_summary_stats_reach_diff, index=True)
 
 display(df.style.pipe(format_style_index))
@@ -114,6 +114,10 @@ df = pd.DataFrame(
 
 df.to_csv(filepath_summary_stats_reach_diff_pct, index=True)
 
+print(
+    f"Summary stats for reach differences between bike and car reach at distance {reach_dist} km:"
+)
+
 display(df.style.pipe(format_style_index))
 # %%
 # Increases in reach at different distance thresholds
@@ -122,53 +126,34 @@ exec(open("../helper_scripts/read_reach_comparison.py").read())
 
 columns = [c for c in hex_reach_comparison.columns if "pct_diff" in c]
 
-display(hex_reach_comparison[columns].describe().style.pipe(format_style_index))
+display(hex_reach_comparison[columns].describe().T.style.pipe(format_style_index))
 
-lts_1_d5_10 = len(
-    hex_reach_comparison[hex_reach_comparison["lts_1_pct_diff_5_10"] == 0]
-)
-lts_1_d10_15 = len(
-    hex_reach_comparison[hex_reach_comparison["lts_1_pct_diff_10_15"] == 0]
-)
-lts_1_2_d5_10 = len(
-    hex_reach_comparison[hex_reach_comparison["lts_1_2_pct_diff_5_10"] == 0]
-)
-lts_1_2_d10_15 = len(
-    hex_reach_comparison[hex_reach_comparison["lts_1_2_pct_diff_10_15"] == 0]
-)
-lts_1_3_d5_10 = len(
-    hex_reach_comparison[hex_reach_comparison["lts_1_3_pct_diff_5_10"] == 0]
-)
-lts_1_3_d10_15 = len(
-    hex_reach_comparison[hex_reach_comparison["lts_1_3_pct_diff_10_15"] == 0]
-)
-lts_1_4_d5_10 = len(
-    hex_reach_comparison[hex_reach_comparison["lts_1_4_pct_diff_5_10"] == 0]
-)
-lts_1_4_d10_15 = len(
-    hex_reach_comparison[hex_reach_comparison["lts_1_4_pct_diff_10_15"] == 0]
-)
-car_d5_10 = len(hex_reach_comparison[hex_reach_comparison["car_pct_diff_5_10"] == 0])
-car_d10_15 = len(hex_reach_comparison[hex_reach_comparison["car_pct_diff_10_15"] == 0])
+# %%
+selected_columns = [c for c in columns if "2_5" in c or "5_10" in c]
 
-values = [
-    (lts_1_d5_10, lts_1_d10_15),
-    (lts_1_2_d10_15, lts_1_2_d10_15),
-    (lts_1_3_d5_10, lts_1_3_d10_15),
-    (lts_1_4_d5_10, lts_1_4_d10_15),
-    (car_d5_10, car_d10_15),
-]
+display(
+    hex_reach_comparison[selected_columns].describe().T.style.pipe(format_style_index)
+)
+# %%
 
-for l, n, v in zip(labels_step, network_levels, values):
+combis = [(1, 5), (5, 10), (10, 15)]
 
-    total_count = len(hex_reach_comparison[hex_reach_comparison[f"{n}_reach_5"] > 0])
-    print(
-        f"{v[0]/total_count*100:.2f}% of {l} cells have no improvements in reach when increasing distance from 5 to 10 km"
-    )
-    print(
-        f"{v[1]/total_count*100:.2f}% of {l} cells have no improvements in reach when increasing distance from 10 to 15 km"
-    )
+for c in combis:
+
+    for i, n in enumerate(network_levels_step):
+        no_impro = len(
+            hex_reach_comparison[
+                hex_reach_comparison[f"{n}_pct_diff_{c[0]}_{c[1]}"] == 0
+            ]
+        )
+
+        total_count = len(
+            hex_reach_comparison[hex_reach_comparison[f"{n}_reach_{c[0]}"] > 0]
+        )
+
+        print(
+            f"{no_impro:,.0f} ({no_impro/total_count*100:.2f}%) of {labels_step[i]} cells have no improvements in reach when increasing distance from {c[0]} to {c[1]} km"
+        )
 
     print("\n")
-
 # %%
