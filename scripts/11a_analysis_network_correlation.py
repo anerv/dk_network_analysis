99dfa8ea4457615adb7bff81ce921d2900a82a30
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-07-19 10:27:45 +0200

    polished plot

--- scripts/11a_analysis_network_correlation.py
+++ scripts/11a_analysis_network_correlation.py
@@ -50,7 +50,7 @@ plot_func.plot_correlation(
     heatmap_fp=fp_hex_network_heatmap,
     pairplot_fp=fp_hex_network_pairplot,
     corner=False,
-    pairplot=True,
+    pairplot=False,
 )
 # %%
 ##### SOCIO #####
@@ -59,7 +59,7 @@ exec(open("../helper_scripts/prepare_socio_network_corr_data.py").read())
 
 # generate socio reach comparison columns
 exec(open("../helper_scripts/generate_socio_reach_columns.py").read())
-
+# %%
 socio_corr_variables = (
     density_columns
     + density_steps_columns[1:4]
@@ -68,28 +68,57 @@ socio_corr_variables = (
     + component_per_km_columns
     + socio_largest_component_columns_median
     + socio_reach_median_columns
-    + socio_reach_max_columns
+    # + socio_reach_max_columns
     + socio_reach_compare_columns
     + ["urban_pct"]
 )
 
-display(
-    socio_gdf[socio_corr_variables]
-    .corr(method="spearman")
-    .style.background_gradient(cmap="coolwarm")
-)
-display(socio_gdf[socio_corr_variables].describe().style.pipe(format_style_index))
+socio_gdf = socio_gdf[socio_corr_variables]
+
+socio_gdf.rename(rename_index_dict_density, inplace=True, axis=1)
+socio_gdf.rename(rename_index_dict_fragmentation, inplace=True, axis=1)
+socio_gdf.rename(rename_index_dict_largest_comp, inplace=True, axis=1)
+socio_gdf.rename(rename_index_dict_reach, inplace=True, axis=1)
+
+rename_dict = {
+    "lts_1_largest_component_median": "LTS 1 largest component (median)",
+    "lts_1_2_largest_component_median": "LTS 1-2 largest component (median)",
+    "lts_1_3_largest_component_median": "LTS 1-3 largest component (median)",
+    "lts_1_4_largest_component_median": "LTS 1-4 largest component (median)",
+    "car_largest_component_median": "Car largest component (median)",
+    "lts_1_reach_median": "LTS 1 reach (median)",
+    "lts_1_2_reach_median": "LTS 1-2 reach (median)",
+    "lts_1_3_reach_median": "LTS 1-3 reach (median)",
+    "lts_1_4_reach_median": "LTS 1-4 reach (median)",
+    "car_reach_median": "Car reach (median)",
+    "lts_1_pct_diff_1_5_median": "LTS 1 % difference 1-5 km reach (median)",
+    "lts_1_pct_diff_5_10_median": "LTS 1 % difference 5-10 km reach (median)",
+    "lts_1_2_pct_diff_1_5_median": "LTS 1-2 % difference 1-5 km reach (median)",
+    "lts_1_2_pct_diff_5_10_median": "LTS 1-2 % difference 5-10 km reach (median)",
+    "lts_1_3_pct_diff_1_5_median": "LTS 1-3 % difference 1-5 km reach (median)",
+    "lts_1_3_pct_diff_5_10_median": "LTS 1-3 % difference 5-10 km reach (median)",
+    "lts_1_4_pct_diff_1_5_median": "LTS 1-4 % difference 1-5 km reach (median)",
+    "lts_1_4_pct_diff_5_10_median": "LTS 1-4 % difference 5-10 km reach (median)",
+    "car_pct_diff_1_5_median": "Car % difference 1-5 km reach (median)",
+    "car_pct_diff_5_10_median": "Car % difference 5-10 km reach (median)",
+    "urban_pct": "Urban %",
+}
+
+socio_gdf.rename(rename_dict, inplace=True, axis=1)
+# %%
+display(socio_gdf.corr(method="spearman").style.background_gradient(cmap="coolwarm"))
+display(socio_gdf.describe().style.pipe(format_style_index))
 
 # %%
 
 # SOCIO PLOT
 plot_func.plot_correlation(
     socio_gdf,
-    socio_corr_variables,
+    socio_gdf.columns,
     heatmap_fp=fp_socio_network_heatmap,
     pairplot_fp=fp_socio_network_pairplot,
     corner=False,
-    pairplot=True,
+    pairplot=False,
 )
 
 # %%
