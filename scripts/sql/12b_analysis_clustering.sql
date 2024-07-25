481b9ba2bcb29c0aad36084df737b2a047740c86
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-07-25 17:26:32 +0200

    adding pop density to hex clusters

--- scripts/sql/12b_analysis_clustering.sql
+++ scripts/sql/12b_analysis_clustering.sql
@@ -159,3 +159,23 @@ DROP TABLE IF EXISTS clustering.dissolved_hex_clusters;
 DROP TABLE IF EXISTS clustering.clipped_socio_cluster_results;
 
 DROP TABLE IF EXISTS clustering.grouped_intersection;
+
+ALTER TABLE
+    clustering.hex_clusters
+ADD
+    column IF NOT EXISTS population FLOAT,
+ADD
+    COLUMN IF NOT EXISTS population_density FLOAT,
+ADD
+    COLUMN IF NOT EXISTS urban_pct FLOAT;
+
+UPDATE
+    clustering.hex_clusters hc
+SET
+    population = hg.population,
+    population_density = hg.population_density,
+    urban_pct = hg.urban_pct
+FROM
+    hex_grid hg
+WHERE
+    hc.hex_id = hg.hex_id;
