a8c7d9c170881925b81eca12685c9829846fc14b
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-07-26 17:09:08 +0200

    added overview map

---
+++ scripts/xx_make_overview_plot.py
@@ -0,0 +1,61 @@
+# %%
+from src import db_functions as dbf
+from src import plotting_functions as plot_func
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import contextily as cx
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import geopandas as gpd
+from matplotlib_scalebar.scalebar import ScaleBar
+
+exec(open("../settings/yaml_variables.py").read())
+exec(open("../settings/plotting.py").read())
+exec(open("../settings/filepaths.py").read())
+plot_func.set_renderer("png")
+
+engine = dbf.connect_alc(db_name, db_user, db_password, db_port=db_port)
+
+connection = dbf.connect_pg(db_name, db_user, db_password, db_port=db_port)
+
+# %%
+# Get full network
+network = gpd.read_postgis(
+    "SELECT id, geometry FROM edges WHERE lts_access IN (1,2,3,4,7)",
+    engine,
+    geom_col="geometry",
+)
+# %%
+plot_res = "high"
+
+filepath = "../results/overview_map"
+
+fig, ax = plt.subplots(figsize=pdict["fsmap"])
+
+network.plot(ax=ax, color="black", linewidth=0.3)
+
+ax.set_axis_off()
+
+ax.add_artist(
+    ScaleBar(
+        dx=1,
+        units="m",
+        dimension="si-length",
+        length_fraction=0.15,
+        width_fraction=0.002,
+        location="lower left",
+        box_alpha=0,
+    )
+)
+cx.add_attribution(ax=ax, text="(C) " + pdict["map_attr"])
+txt = ax.texts[-1]
+txt.set_position([0.99, 0.01])
+txt.set_ha("right")
+txt.set_va("bottom")
+
+if plot_res == "high":
+    fig.savefig(filepath + ".svg", dpi=pdict["dpi"])
+else:
+    fig.savefig(filepath + ".png", dpi=pdict["dpi"])
+
+# %%
