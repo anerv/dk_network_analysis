1a31d9abefdab5a01870993e0e839e8619cad2b2
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-08-01 20:17:43 +0200

    added new maps

--- illustrations/make_overview_plot.py
+++ illustrations/make_overview_plot.py
@@ -32,9 +32,10 @@ filepath = "../illustrations/overview_map"
 
 fig, ax = plt.subplots(figsize=pdict["fsmap"])
 
-network.plot(ax=ax, color="black", linewidth=0.3)
+network.plot(ax=ax, color="black", linewidth=0.2)
 
 ax.set_axis_off()
+ax.set_title("Road and path network")
 
 ax.add_artist(
     ScaleBar(
@@ -48,6 +49,103 @@ ax.add_artist(
     )
 )
 cx.add_attribution(ax=ax, text="(C) " + pdict["map_attr"])
+txt = ax.texts[-1]
+txt.set_position([0.99, 0.01])
+txt.set_ha("right")
+txt.set_va("bottom")
+
+if plot_res == "high":
+    fig.savefig(filepath + ".svg", dpi=pdict["dpi"])
+else:
+    fig.savefig(filepath + ".png", dpi=pdict["dpi"])
+
+# %%
+population = gpd.read_postgis(
+    "SELECT population, population_density, urban_pct, geometry FROM hex_grid",
+    engine,
+    geom_col="geometry",
+)
+# %%
+plot_res = "low"
+
+filepath = "../illustrations/population_map"
+
+fig, ax = plt.subplots(figsize=pdict["fsmap"])
+
+population.plot(
+    # cax=cax,
+    ax=ax,
+    scheme="natural_breaks",
+    k=5,
+    column="population_density",
+    cmap="viridis",
+    linewidth=0.0,
+    edgecolor="none",
+    legend=True,
+    legend_kwds={"fmt": "{:.0f}", "frameon": False},
+)
+
+ax.set_axis_off()
+ax.set_title("Population density (people/km²)")
+
+ax.add_artist(
+    ScaleBar(
+        dx=1,
+        units="m",
+        dimension="si-length",
+        length_fraction=0.15,
+        width_fraction=0.002,
+        location="lower left",
+        box_alpha=0,
+    )
+)
+cx.add_attribution(ax=ax, text="(C) " + "GHSL")
+txt = ax.texts[-1]
+txt.set_position([0.99, 0.01])
+txt.set_ha("right")
+txt.set_va("bottom")
+
+if plot_res == "high":
+    fig.savefig(filepath + ".svg", dpi=pdict["dpi"])
+else:
+    fig.savefig(filepath + ".png", dpi=pdict["dpi"])
+
+# %%
+
+filepath = "../illustrations/urban_areas"
+
+fig, ax = plt.subplots(figsize=pdict["fsmap"])
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="3.5%", pad="1%")
+
+population.plot(
+    cax=cax,
+    ax=ax,
+    # scheme="fisher_jenks",
+    # k=5,
+    column="urban_pct",
+    cmap="PuRd",
+    linewidth=0.0,
+    edgecolor="none",
+    legend=True,
+    legend_kwds={"fmt": "{:.0f}"},
+)
+
+ax.set_axis_off()
+ax.set_title("Urban areas (%)")
+
+ax.add_artist(
+    ScaleBar(
+        dx=1,
+        units="m",
+        dimension="si-length",
+        length_fraction=0.15,
+        width_fraction=0.002,
+        location="lower left",
+        box_alpha=0,
+    )
+)
+cx.add_attribution(ax=ax, text="(C) " + "SDFI")
 txt = ax.texts[-1]
 txt.set_position([0.99, 0.01])
 txt.set_ha("right")
