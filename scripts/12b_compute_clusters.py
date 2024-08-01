9a87e2f94b67347ca2699401daf3b74c321477b6
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-08-01 08:56:52 +0200

    cleanup

--- scripts/12b_compute_clusters.py
+++ scripts/12b_compute_clusters.py
@@ -166,48 +166,6 @@ socio_soc_gdf[
 
 exec(open("../helper_scripts/prepare_hex_cluster_data.py").read())
 
-# exec(open("../helper_scripts/read_hex_results.py").read())
-
-# exec(open("../helper_scripts/read_reach_comparison.py").read())
-# reach_compare_columns = [c for c in hex_reach_comparison.columns if "pct_diff" in c]
-
-# reach_compare_columns = [
-#     c for c in reach_compare_columns if any(r in c for r in reach_comparisons)
-# ]
-
-# hex_gdf.replace(np.nan, 0, inplace=True)
-
-# selected_reach_comp_columns = [r for r in reach_compare_columns if "5_10" not in r]
-
-# # Define cluster variables
-# hex_network_cluster_variables_org = (
-#     density_columns
-#     # + density_steps_columns[1:4]
-#     + length_relative_columns
-#     # + component_count_columns
-#     # component_per_km_columns
-#     + largest_local_component_len_columns
-#     + reach_columns
-#     + reach_compare_columns
-#     # + ["urban_pct"]
-# )
-
-# rename_hex_dict = (
-#     rename_index_dict_density
-#     | rename_index_dict_fragmentation
-#     | rename_index_dict_largest_comp
-#     | rename_index_dict_reach
-#     | rename_hex_reach_dict
-# )
-# rename_hex_dict["urban_pct"] = "Urban %"
-# hex_gdf.rename(rename_hex_dict, inplace=True, axis=1)
-
-# hex_network_cluster_variables = []
-# for key, value in rename_hex_dict.items():
-#     if key in hex_network_cluster_variables_org:
-#         hex_network_cluster_variables.append(value)
-
-# assert len(hex_network_cluster_variables) == len(hex_network_cluster_variables_org)
 # %%
 # Use robust_scale to norm cluster variables
 hex_network_scaled = robust_scale(hex_gdf[hex_network_cluster_variables])
@@ -258,11 +216,11 @@ hex_gdf["cluster_label"] = None
 
 
 label_dict = {
-    0: "Highest stress - lowest density - lowest reach",
-    1: "Low stress - medium density - medium reach",
-    2: "High stress - medium density - low reach - local connectivity",
-    3: "High stress - medium density - low reach - regional connectivity",
-    4: "Lowest Stress - highest density - highest reach",
+    0: "0: Highest stress - lowest density - lowest reach",
+    1: "1: Low stress - medium density - medium reach",
+    2: "2: High stress - medium density - low reach - local connectivity",
+    3: "3: High stress - medium density - low reach - regional connectivity",
+    4: "4: Lowest Stress - highest density - highest reach",
 }
 
 assert len(label_dict) == k
