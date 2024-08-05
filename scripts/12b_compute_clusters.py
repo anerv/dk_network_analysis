0554604a50914a723fb7fa4fdbe5bbd5c67422c3
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-08-05 14:32:41 +0200

    formatting

--- scripts/12b_compute_clusters.py
+++ scripts/12b_compute_clusters.py
@@ -214,8 +214,8 @@ cluster_means_hex.to_csv(fp_hex_network_cluster_means, index=True)
 fp = fp_cluster_maps_base + "hex_cluster_map.png"
 plot_func.plot_hex_clusters(hex_gdf, "kmeans_net_5", cmap, fp)
 
+# %%
 # Make zoomed cluster map
-
 fp = fp_cluster_maps_base + "hex_cluster_map_zoom.png"
 
 xmin, ymin = (689922.425333, 6161099.004817)
