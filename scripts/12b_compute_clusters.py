8ec4e4486e5d1e2c0157f2b527bbe0124aa872cd
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-10-29 14:55:27 -0400

    rmv socio network cluster

--- scripts/12b_compute_clusters.py
+++ scripts/12b_compute_clusters.py
@@ -23,75 +23,6 @@ connection = dbf.connect_pg(db_name, db_user, db_password, db_port=db_port)
 exec(open("../helper_scripts/prepare_socio_cluster_data.py").read())
 
 # %%
-# SOCIO CLUSTERING: Network variables
-
-# # Use robust_scale to norm cluster variables
-# socio_network_scaled = robust_scale(socio_cluster_gdf[socio_network_cluster_variables])
-
-# # Find appropriate number of clusters
-# m1, m2 = analysis_func.find_k_elbow_method(socio_network_scaled, min_k=1, max_k=20)
-
-# for key, val in m1.items():
-#     print(f"{key} : {val:.2f}")
-
-# # %%
-# # Define K!
-# k = 8
-# ##### K-Means #######
-
-# kmeans_col_net_socio = f"kmeans_net_{k}"
-
-# k_labels = analysis_func.run_kmeans(k, socio_network_scaled)
-
-# socio_cluster_gdf[kmeans_col_net_socio] = k_labels
-
-# fp_map = fp_cluster_maps_base + f"socio_net_map_{kmeans_col_net_socio}.png"
-# fp_size = fp_cluster_plots_base + f"socio_net_size_{kmeans_col_net_socio}.png"
-# fp_kde = fp_cluster_plots_base + f"socio_net_kde_{kmeans_col_net_socio}.png"
-
-# colors = plot_func.get_hex_colors_from_colormap(pdict["cat"], k)
-# cmap = plot_func.color_list_to_cmap(colors)
-
-# cluster_means_socio_net = analysis_func.examine_cluster_results(
-#     socio_cluster_gdf,
-#     kmeans_col_net_socio,
-#     socio_network_cluster_variables,
-#     fp_map,
-#     fp_size,
-#     fp_kde,
-#     cmap=cmap,
-#     palette=colors,
-# )
-
-# plot_func.style_cluster_means(cluster_means_socio_net)
-
-# cluster_means_socio_net.to_csv(fp_socio_net_cluster_means, index=True)
-
-# %%
-# Label clusters after bikeability rank
-
-# socio_cluster_gdf["network_rank"] = None
-
-# rank = [1, 6, 7, 0, 5, 3, 4, 2]
-# assert len(rank) == k
-
-# for i, r in enumerate(rank):
-#     socio_cluster_gdf.loc[
-#         socio_cluster_gdf[kmeans_col_net_socio] == r,
-#         "network_rank",
-#     ] = (
-#         i + 1
-#     )
-
-# socio_cluster_gdf.network_rank = socio_cluster_gdf.network_rank.astype(int)
-# socio_cluster_gdf[
-#     [kmeans_col_net_socio] + ["geometry", id_columns[1], "network_rank"]
-# ].to_file(fp_socio_network_clusters, driver="GPKG")
-
-
-# plot_func.plot_rank(socio_cluster_gdf, "network_rank")
-
-# %%
 # SOCIO CLUSTERING: Socio-economic variables
 
 socio_socio_cluster_variables = socio_socio_cluster_variables[6:-2]
@@ -270,13 +201,7 @@ for q in schema_queries:
         print("Please fix error before rerunning and reconnect to the database")
 
 # %%
-# socio_cluster_gdf[["id", kmeans_col_net_socio, "geometry"]].to_postgis(
-#     "socio_network_clusters",
-#     engine,
-#     schema="clustering",
-#     if_exists="replace",
-#     index=False,
-# )
+
 socio_socio_gdf[["id", "socio_label", kmeans_col_socio_soc, "geometry"]].to_postgis(
     "socio_socio_clusters",
     engine,
