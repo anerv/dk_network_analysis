92bb9c670a1e87fe1658dc3b40825428f38268dc
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-11-08 18:58:04 -0500

    computing ave bikeability rank

--- scripts/sql/12d_process_socio_clusters.sql
+++ scripts/sql/12d_process_socio_clusters.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS clustering.grouped_intersection;
 
 CREATE INDEX IF NOT EXISTS socio_cluster_geom_ix ON clustering.socio_socio_clusters USING GIST (geometry);
 
-CREATE TABLE clustering.dissolved_hex_clusters AS WITH union_geoms AS (
+CREATE TEMP TABLE clustering.dissolved_hex_clusters AS WITH union_geoms AS (
     SELECT
         ST_Union(geometry) AS geometry,
         kmeans_net AS bikeability_cluster
@@ -23,7 +23,7 @@ FROM
 
 CREATE INDEX IF NOT EXISTS dissolved_hex_cluster_geom_ix ON clustering.dissolved_hex_clusters USING GIST (geometry);
 
-CREATE TABLE clustering.clipped_socio_clusters AS
+CREATE TEMP TABLE clustering.clipped_socio_clusters AS
 SELECT
     sc.id,
     dhc.bikeability_cluster,
@@ -32,7 +32,7 @@ FROM
     clustering.socio_socio_clusters sc
     INNER JOIN clustering.dissolved_hex_clusters dhc ON ST_Intersects(sc.geometry, dhc.geometry);
 
-CREATE TABLE clustering.grouped_intersection AS
+CREATE TEMP TABLE clustering.grouped_intersection AS
 SELECT
     id,
     bikeability_cluster,
@@ -167,8 +167,68 @@ WHERE
     cg.id = sc.id
     AND cg.bikeability_cluster = 5;
 
-DROP TABLE IF EXISTS clustering.dissolved_hex_clusters;
+CREATE TABLE hex_socio_intersection AS
+SELECT
+    socio_socio_clusters.id AS socio_id,
+    hex_clusters.hex_id,
+    ST_Area(
+        ST_Intersection(
+            socio_socio_clusters.geometry,
+            hex_clusters.geometry
+        )
+    ) AS intersect_area,
+    hex_clusters.kmeans_net AS bikeability_rank
+FROM
+    clustering.socio_socio_clusters
+    JOIN clustering.hex_clusters ON ST_Intersects(
+        socio_socio_clusters.geometry,
+        hex_clusters.geometry
+    );
 
-DROP TABLE IF EXISTS clustering.clipped_socio_clusters;
+CREATE TEMP TABLE socio_areas_total AS
+SELECT
+    id AS socio_id,
+    ST_Area(geometry) AS total_area
+FROM
+    clustering.socio_socio_clusters;
 
-DROP TABLE IF EXISTS clustering.grouped_intersection;
+CREATE TABLE weighted_bikeability AS
+SELECT
+    inter.socio_id,
+    inter.hex_id,
+    inter.bikeability_rank,
+    (
+        inter.bikeability_rank * (inter.intersect_area / t.total_area)
+    ) AS weighted_bikeability_rank
+FROM
+    hex_socio_intersection inter
+    JOIN socio_areas_total t ON inter.socio_id = t.socio_id;
+
+ALTER TABLE
+    clustering.socio_socio_clusters
+ADD
+    COLUMN IF NOT EXISTS average_bikeability_rank DOUBLE PRECISION;
+
+WITH weighted_bikeability AS (
+    SELECT
+        socio_id,
+        SUM(weighted_bikeability_rank) AS average_bikeability_rank
+    FROM
+        weighted_bikeability
+    GROUP BY
+        socio_id
+    ORDER BY
+        socio_id
+)
+UPDATE
+    clustering.socio_socio_clusters
+SET
+    average_bikeability_rank = wb.average_bikeability_rank
+FROM
+    weighted_bikeability wb
+WHERE
+    socio_socio_clusters.id = wb.socio_id;
+
+-- DROP TABLE IF EXISTS clustering.dissolved_hex_clusters;
+-- DROP TABLE IF EXISTS clustering.clipped_socio_clusters;
+-- DROP TABLE IF EXISTS clustering.grouped_intersection;
