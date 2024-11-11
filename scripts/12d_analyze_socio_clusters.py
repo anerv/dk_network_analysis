9d119bba803dfe535ec17108e8b6072394326f73
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-11-11 15:36:59 -0500

    added plots

--- scripts/12d_analyze_socio_clusters.py
+++ scripts/12d_analyze_socio_clusters.py
@@ -1,4 +1,5 @@
 # %%
+
 from src import db_functions as dbf
 from src import plotting_functions as plot_func
 import geopandas as gpd
@@ -8,6 +9,7 @@ import pandas as pd
 from IPython.display import display
 import seaborn as sns
 import matplotlib.patches as mpatches
+import matplotlib as mpl
 
 exec(open("../settings/yaml_variables.py").read())
 exec(open("../settings/plotting.py").read())
@@ -21,7 +23,7 @@ engine = dbf.connect_alc(db_name, db_user, db_password, db_port=db_port)
 connection = dbf.connect_pg(db_name, db_user, db_password, db_port=db_port)
 
 # %%
-preprocess = True
+preprocess = False
 
 if preprocess:
 
@@ -37,13 +39,10 @@ socio_hex_cluster = gpd.read_postgis(
 )
 socio_hex_cluster.fillna(0, inplace=True)
 
-# %%
-
 area_columns = [c for c in socio_hex_cluster.columns if "area" in c]
 area_columns.append("socio_label")
 grouped_socio_clusters = socio_hex_cluster[area_columns].groupby("socio_label").sum()
 
-# %%
 # Normalize the data to get proportions
 hex_type_proportions = grouped_socio_clusters.div(
     grouped_socio_clusters.sum(axis=1), axis=0
@@ -84,8 +83,191 @@ plt.savefig(fp_cluster_plots_base + "socio_clusters_proportions.png", dpi=pdict[
 plt.show()
 # %%
 
-# TODO: Plot average bikeability rank for socio clusters
+# Average bikeability rank per socio clsuters
+ave_socio_bike = pd.read_sql("SELECT * FROM clustering.socio_socio_clusters", engine)
+# Order rows by socio_label and average_bikeability_rank
+ave_socio_bike.sort_values(by=["socio_label", "average_bikeability_rank"], inplace=True)
+ave_socio_bike.reset_index(drop=True, inplace=True)
 
-# Analyze *share* of population and pct urban areas in each socio cluster
+# %%
+# For each socio area
 
-# look into student distribution
+plot_func.make_stripplot(
+    ave_socio_bike,
+    "average_bikeability_rank",
+    "socio_label",
+    "socio_label",
+    list(socio_cluster_colors_dict.values()),
+    xlabel="Average bikeability rank",
+    xticks=[i for i in range(1, 6)],
+    fp=fp_cluster_plots_base + "average_bikeability_rank_by_socio_label.png",
+)
+
+plot_func.make_barplot(
+    ave_socio_bike,
+    "socio_label",
+    "average_bikeability_rank",
+    "socio_label",
+    list(socio_cluster_colors_dict.values()),
+    xlabel="",
+    fp=fp_cluster_plots_base + "average_bikeability_rank_by_socio_label_grouped.png",
+)
+
+# %%
+# Correlation between socio variables and bikeability rank
+
+socio_bike = pd.read_sql("SELECT * FROM clustering.socio_socio_clusters", engine)
+
+exec(open("../helper_scripts/read_socio_pop.py").read())
+
+socio.drop(columns=["geometry"], inplace=True)
+
+socio_socio_bike = socio_bike.merge(socio, on="id")
+
+# %%
+# Correlation between socio variables and average bikeability rank
+
+for c in socio_corr_variables:
+
+    plot_func.sns_scatter(
+        socio_socio_bike,
+        c,
+        "average_bikeability_rank",
+        "socio_label",
+        list(socio_cluster_colors_dict.values()),
+        yticks=[i for i in range(1, 6)],
+    )
+
+# %%
+# Share of students
+
+plot_func.make_barplot(
+    socio_socio_bike,
+    "socio_label",
+    "Students (share)",
+    "socio_label",
+    list(socio_cluster_colors_dict.values()),
+    xlabel="",
+)
+
+plot_func.make_stripplot(
+    socio_socio_bike,
+    "Students (share)",
+    "socio_label",
+    "socio_label",
+    list(socio_cluster_colors_dict.values()),
+    xlabel="Share of students",
+)
+
+
+# %%
+# Share of population and pct urban areas in each socio cluster
+
+socio_bike = gpd.read_postgis(
+    "SELECT * FROM clustering.socio_socio_clusters", engine, geom_col="geometry"
+)
+
+socio = pd.read_sql("SELECT id, population, households, urban_pct FROM socio", engine)
+
+socio_socio_bike = socio_bike.merge(socio, on="id")
+
+# %%
+# Share of population
+
+fig, ax = plt.subplots(figsize=pdict["fsbar"])
+
+sns.barplot(
+    data=socio_socio_bike,
+    x="socio_label",
+    y="population",
+    hue="socio_label",
+    estimator=sum,
+    palette=list(socio_cluster_colors_dict.values()),
+    width=0.4,
+    ax=ax,
+    errorbar=None,
+)
+
+ax.set_xlabel("")
+ax.set_ylabel("Population (mill.)")
+plt.xticks(rotation=45, ha="right")
+plt.yticks([0, 1200000])
+labels = ["0", "1.2"]
+ax.set_yticklabels(labels)
+ax.tick_params(axis="both", which="major", labelsize=pdict["fontsize"])
+
+sns.despine(left=True, bottom=True)
+
+# %%
+
+socio_socio_bike["area_sqkm"] = socio_socio_bike.geometry.area / 10**6
+
+socio_socio_bike["urban_area"] = (socio_socio_bike.area_sqkm) * (
+    socio_socio_bike["urban_pct"] / 100
+)
+
+socio_socio_bike["non_urban_area"] = (
+    socio_socio_bike.area_sqkm - socio_socio_bike["urban_area"]
+)
+
+socio_grouped = (
+    socio_socio_bike[["socio_label", "urban_area", "non_urban_area"]]
+    .groupby("socio_label")
+    .sum()
+)
+
+# %%
+colors = list(socio_cluster_colors_dict.values())
+
+fig, ax = plt.subplots(figsize=pdict["fsbar"])
+
+bar_width = 0.4
+x = range(len(socio_grouped))
+
+ax.bar(
+    x,
+    socio_grouped["urban_area"],
+    color=colors,
+    width=bar_width,
+    label="Urban area",
+)
+
+# Plot non-urban population with hatch
+ax.bar(
+    x,
+    socio_grouped["non_urban_area"],
+    bottom=socio_grouped["urban_area"],
+    color=colors,
+    width=bar_width,
+    hatch="//",
+    label="Non-urban area",
+)
+ax.set_xticks(x)
+ax.set_xticklabels(socio_grouped.index)
+plt.xticks(rotation=45, ha="right")
+ax.set_xlabel("")
+ax.set_ylabel("Area (km²)")
+ax.tick_params(axis="both", which="major")
+ax.set_yticks([0, 15000])
+ax.get_yaxis().set_major_formatter(
+    mpl.ticker.FuncFormatter(lambda x, p: format(int(x), ","))
+)
+sns.despine(left=True, bottom=True)
+
+no_urban_patch = mpatches.Patch(
+    facecolor="none",
+    edgecolor="grey",
+    linewidth=0.5,
+    label="Non-urban",
+    hatch="///",
+)
+
+# Add legend
+ax.legend(
+    handles=[no_urban_patch],
+    loc="upper right",
+    frameon=False,
+    fontsize=pdict["fs_subplot"],
+)
+
+# %%
