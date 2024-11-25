6286f69afdb09288fe2e4f1074a9a013c8e9e01e
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-11-25 16:34:05 -0500

    moved func

--- scripts/13a_equity.py
+++ scripts/13a_equity.py
@@ -151,28 +151,9 @@ display(inequalities_socio)
 
 # %%
 
-# TODO: refine
-
-
-def classify_urban_rural(x):
-
-    if x > 20:
-        return "slightly urban"
-
-    if x > 50:
-        return "mostly urban"
-
-    if x > 80:
-        return "very urban"
-
-    if x == 100:
-        return "completely urban"
-
-    else:
-        return "rural"
-
-
-socio_gdf["urban_rural"] = socio_gdf["Urban area (%)"].apply(classify_urban_rural)
+socio_gdf["urban_rural"] = socio_gdf["Urban area (%)"].apply(
+    analysis_func.classify_urban_rural
+)
 
 # %%
 for c, l in zip(
@@ -292,114 +273,4 @@ theil_ds = inequality.theil.TheilDSim(
     hexjoin[inequality_columns_hex].values, hexjoin.socio_id, 999
 )
 
-# %%
-## ************************** OLD CODE ************************** ##
-
-
-# %%
-####### BIVARIATE MORAN's I #######
-
-from esda.moran import Moran_BV, Moran_Local_BV
-from splot.esda import plot_local_autocorrelation, moran_scatterplot
-from splot.esda import plot_moran_bv
-from esda.moran import Moran_Local_BV
-from esda.moran import Moran_Local
-from esda.moran import Moran
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import geopandas as gpd
-
-
-def compute_bivariate_moransi(x, y, w):
-
-    # Compute bivariate Moran's I
-    moran_bv = Moran_BV(y, x, w)
-    lisas_bv = Moran_Local_BV(y, x, w)
-
-    return moran_bv, lisas_bv
-
-
-def compare_bivariate_moransi(all_morans):
-
-    _, axs = plt.subplots(
-        1, len(all_morans), figsize=(15, 10), subplot_kw={"aspect": "equal"}
-    )
-
-    axs = axs.flatten()
-
-    for i, mo in enumerate(all_morans):
-
-        moran_scatterplot(mo, p=0.05, ax=axs[i])
-        plt.show()
-
-
-def confirm_spatial_auto(x, y, gdf, w):
-
-    # Confirm spatial autocorrelation of individual variables
-    moran_x = Moran(x, w)
-    moran_y = Moran(y, w)
-
-    print(
-        f"With a p_sim value of {moran_x.p_sim}, the Moran's I value for variable x is {moran_x.I}"
-    )
-    print(
-        f"With a p_sim value of {moran_y.p_sim}, the Moran's I value for variable y is {moran_y.I}"
-    )
-
-    lisas_x = Moran_Local(x, w)
-    lisas_y = Moran_Local(y, w)
-
-    return lisas_x, lisas_y
-
-
-# %%
-w = analysis_func.spatial_weights_combined(
-    socio_gdf, id_columns[1], k_socio, silence_warnings=True
-)
-
-x_col = "Household income 50th percentile"
-y_col = "Low Stress Density"
-
-x = socio_gdf[x_col].values
-y = socio_gdf[y_col].values
-
-lisas_x, lisas_y = confirm_spatial_auto(x, y, socio_gdf, w)
-
-plot_local_autocorrelation(lisas_x, socio_gdf, x_col)
-plt.show()
-
-plot_local_autocorrelation(lisas_y, socio_gdf, y_col)
-plt.show()
-
-moran_bv, lisas_bv = compute_bivariate_moransi(x, y, w)
-
-plot_moran_bv(moran_bv)
-plt.show()
-
-all_morans = [lisas_x, lisas_y, lisas_bv]
-
-compare_bivariate_moransi(all_morans)
-
-
-# Bivariate Moran Statistics describe the correlation between one variable
-# and the spatial lag of another variable.
-# Therefore, we have to be careful interpreting our results.
-# Bivariate Moran Statistics do not take the inherent correlation
-# between the two variables at the same location into account.
-# They much more offer a tool to measure the degree one polygon with a
-# specific attribute is correlated with its neighboring polygons
-# with a different attribute.
-# %%
-### NEEDS GAP
-
-# TODO: join hex pop to density, reach?
-# Define threshold for density, reach, etc.
-
-# write code for counting areas and people in areas with high and low values
-# %%
-
-# makes sense for density - zero sum game
-
-# what about reach? not a zero sum game, but can still show the inequality - a bit like accessiblity
 # %%
