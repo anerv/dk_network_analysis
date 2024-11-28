6468ff5c0f774a9d9712a199d79bb0b5469f11c5
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-11-28 16:53:56 -0500

    added summary/describe of socio vars

---
+++ scripts/08d_summary_stats_socio.py
@@ -0,0 +1,31 @@
+# %%
+
+import pandas as pd
+import numpy as np
+import pandas as pd
+from IPython.display import display
+from src import db_functions as dbf
+
+exec(open("../settings/yaml_variables.py").read())
+exec(open("../settings/plotting.py").read())
+exec(open("../settings/filepaths.py").read())
+exec(open("../settings/df_styler.py").read())
+
+# %%
+
+
+engine = dbf.connect_alc(db_name, db_user, db_password, db_port=db_port)
+
+connection = dbf.connect_pg(db_name, db_user, db_password, db_port=db_port)
+
+socio = pd.read_sql("SELECT * FROM socio", con=engine)
+
+socio.rename(population_rename_dict, inplace=True, axis=1)
+
+socio["Income under 150k (share)"] = (
+    socio["Income under 100k (share)"] + socio["Income 100-150k (share)"]
+)
+
+# %%
+display(socio[socio_corr_variables].describe())
+# %%
