6f703adde2ee9457e90ddb54a54f4e374d97db56
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-12-11 13:43:27 -0500

    working on outlier analysis

--- scripts/12f_analyze_socio_bikeability.py
+++ scripts/12f_analyze_socio_bikeability.py
@@ -1,6 +1,7 @@
 # %%
 from src import db_functions as dbf
 from src import plotting_functions as plot_func
+from src import analysis_functions as analysis_func
 import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -28,18 +29,29 @@ socio_hex_cluster.fillna(0, inplace=True)
 
 exec(open("../helper_scripts/read_socio_pop.py").read())
 
-# %%
+socio_hex_cluster.drop(columns=["geometry"], inplace=True)
+
 socio_socio = socio.merge(socio_hex_cluster, on="id")
 
-socio_socio = socio_socio.drop(columns=["geometry_x", "geometry_y"])
+# socio_socio = socio_socio.drop(columns=["geometry_x", "geometry_y"])
 
 assert len(socio_socio) == len(socio) == len(socio_hex_cluster)
 # %%
 
+bikeability_rename_dict = {
+    "share_hex_cluster_1": "Share bikeability 1",
+    "share_hex_cluster_2": "Share bikeability 2",
+    "share_hex_cluster_3": "Share bikeability 3",
+    "share_hex_cluster_4": "Share bikeability 4",
+    "share_hex_cluster_5": "Share bikeability 5",
+}
+socio_socio.rename(columns=bikeability_rename_dict, inplace=True)
+
+
 socio_bikeability_cols = [c for c in socio_socio.columns if "share_hex_cluster" in c]
 
 corr_columns = socio_corr_variables[7:-2] + socio_bikeability_cols
-
+# %%
 plot_func.plot_correlation(
     socio_socio,
     corr_columns,
@@ -54,7 +66,7 @@ display(socio_socio[corr_columns])
 
 # %%
 
-preprocess = True
+preprocess = False
 
 if preprocess:
 
@@ -84,6 +96,7 @@ hex_bike_socio["households_income_under_150k_pct"] = (
 )
 
 hex_bike_socio.rename(columns=population_rename_dict, inplace=True)
+
 
 # %%
 
@@ -117,5 +130,170 @@ for s in socio_corr_variables[7:-2]:
     )
 
     plt.show()
+
+# %%
+
+# Compute mean, median, and standard deviation for 'average_bikeability_rank' grouped by 'socio_cluster'
+bikeability_stats = socio_socio.groupby("socio_label")["average_bikeability_rank"].agg(
+    ["mean", "median", "std"]
+)
+
+display(bikeability_stats)
+
+for socio_cluster in socio_socio.socio_label.unique():
+
+    areas_below_median = socio_socio[socio_socio["socio_label"] == socio_cluster].loc[
+        socio_socio["average_bikeability_rank"]
+        < bikeability_stats.loc[socio_cluster, "median"]
+    ]
+
+    total_socio_label = socio_socio[socio_socio["socio_label"] == socio_cluster]
+
+    print(
+        f"{len(areas_below_median)} out of {len(total_socio_label)} areas are below the median for socio cluster {socio_cluster}"
+    )
+
+    areas_below_mean = socio_socio[socio_socio["socio_label"] == socio_cluster].loc[
+        socio_socio["average_bikeability_rank"]
+        < bikeability_stats.loc[socio_cluster, "mean"]
+    ]
+    print(
+        f"{len(areas_below_mean)} out of {len(total_socio_label)} areas are below the mean for socio cluster {socio_cluster}"
+    )
+
+    areas_1std_below = socio_socio[socio_socio["socio_label"] == socio_cluster].loc[
+        socio_socio["average_bikeability_rank"]
+        < bikeability_stats.loc[socio_cluster, "mean"]
+        - bikeability_stats.loc[socio_cluster, "std"]
+    ]
+    print(
+        f"{len(areas_1std_below)} out of {len(total_socio_label)} areas are 1 std dev below the mean for socio cluster {socio_cluster}"
+    )
+
+    areas_1std_above = socio_socio[socio_socio["socio_label"] == socio_cluster].loc[
+        socio_socio["average_bikeability_rank"]
+        > bikeability_stats.loc[socio_cluster, "mean"]
+        + bikeability_stats.loc[socio_cluster, "std"]
+    ]
+
+    print(
+        f"{len(areas_1std_above)} out of {len(total_socio_label)} areas are 1 std dev above the mean for socio cluster {socio_cluster}"
+    )
+
+    print("\n")
+
+
+# %%
+
+socio_cluster_values = socio_socio.socio_label.unique()
+socio_cluster_values.sort()
+
+for socio_label in socio_cluster_values:
+
+    socio_label_fp = analysis_func.clean_labels(socio_label)
+
+    plot_func.plot_above_below_mean(
+        socio_socio,
+        socio_label=socio_label,
+    )
+
+    analysis_func.export_outliers(
+        socio_socio,
+        socio_label=socio_label,
+        fp_above="../results/equity/data/outliers_above_mean_"
+        + f"{socio_label_fp}.gpkg",
+        fp_below="../results/equity/data/outliers_below_mean_"
+        + f"{socio_label_fp}.gpkg",
+    )
+
+plot_func.make_combined_outlier_plot(
+    socio_socio,
+    "socio_label",
+    "average_bikeability_rank",
+    socio_cluster_colors_dict,
+    "../results/equity/maps/outliers_combined.png",
+)
+
+# %%
+# TODO: For each socio cluster, make a scatter plot plotting the value of all members of that cluster for all socio variables.
+# Plot non-outlier members in one color, and outliers in another color.
+
+# %%
+
+
+socio_socio = analysis_func.label_above_below_mean(
+    socio_socio, "socio_label", "average_bikeability_rank"
+)
+
+socio_cluster_values = socio_socio.socio_label.unique()
+socio_cluster_values.sort()
+
+for socio_label in socio_cluster_values:
+
+    this_cluster = socio_socio[
+        (socio_socio["socio_label"] == socio_label)
+        & (socio_socio.above_mean == False)
+        & (socio_socio.below_mean == False)
+    ]
+
+    this_cluster_above = socio_socio[
+        (socio_socio["socio_label"] == socio_label) & (socio_socio.above_mean == True)
+    ]
+
+    this_cluster_below = socio_socio[
+        (socio_socio["socio_label"] == socio_label) & (socio_socio.below_mean == True)
+    ]
+
+    # Define cluster variables
+    socio_socio_cluster_variables = [
+        c for c in socio_corr_variables if "w car" not in c
+    ]
+    socio_socio_cluster_variables = socio_socio_cluster_variables[8:-2]
+    socio_socio_cluster_variables.remove(
+        "Household income 50th percentile",
+    )
+    socio_socio_cluster_variables.remove(
+        "Household income 80th percentile",
+    )
+
+    fig, ax = plt.subplots(figsize=pdict["fsbar"])
+    sns.stripplot(
+        data=this_cluster[socio_socio_cluster_variables],
+        size=5,
+        jitter=True,
+        ax=ax,
+        color="#BBBBBB",
+        label="Non-outlier",
+    )
+    sns.stripplot(
+        data=this_cluster_above[socio_socio_cluster_variables],
+        size=5,
+        jitter=True,
+        ax=ax,
+        color="#882255",
+        alpha=0.5,
+        label="Outlier above mean",
+    )
+    sns.stripplot(
+        data=this_cluster_below[socio_socio_cluster_variables],
+        size=5,
+        jitter=True,
+        ax=ax,
+        color="#009988",
+        label="Outlier below mean",
+    )
+    plt.xticks(rotation=90)
+    plt.title(f"{socio_label}")
+    plt.xlabel("")
+    plt.ylabel("")
+
+    sns.despine(left=True)
+    handles, labels = ax.get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    plt.legend(by_label.values(), by_label.keys(), frameon=False)
+    plt.show()
+
+# %%
+
 
 # %%
